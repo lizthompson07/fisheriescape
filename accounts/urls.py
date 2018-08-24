fcf2ce545ace4938378cf251cf833bdc60f16ee5
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-08-24 16:06:49 -0300

    reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -23,8 +23,9 @@ urlpatterns = [
             'message': "You must be logged in to access this page",
             }
         }),
-    # path('password-reset/', views.UserPassWordResetView.as_view(), name='password_reset'),
+    path('password-reset/', views.UserPassWordResetView.as_view(), name='password_reset'),
+    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name="registration/passwords_reset_form.html"), name='password_reset'),
 
-
+# PasswordResetView
 
 ]
