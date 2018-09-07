4ce30ea6c5a2502eff75021753691e4469fda11f
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-09-07 11:44:52 -0300

    sample module hammered out!

--- herring/forms.py
+++ herring/forms.py
@@ -8,7 +8,7 @@ class PortSampleForm(forms.ModelForm):
     class Meta:
         model = models.Sample
         fields = "__all__"
-        exclude = ['old_id', 'season']
+        exclude = ['old_id', 'season', "tests", "length_frequencies"]
         labels={
             'district':mark_safe("District (<a href='#' >search</a>)"),
             'vessel':mark_safe("Vessel CFVN (<a href='#' >add</a>)"),
@@ -25,3 +25,24 @@ class PortSampleForm(forms.ModelForm):
             'longitude_w':forms.NumberInput(attrs={'placeholder':"dd.ddddd"}),
 
         }
+
+class LengthFrquencyWizardSetupForm(forms.Form):
+    minimum_length = forms.FloatField(required=True,min_value=0, max_value=50, widget=forms.NumberInput(attrs={'placeholder': 'mulitple of 0.5 cm'}))
+    maximum_length = forms.FloatField(required=True,min_value=0, max_value=50, widget=forms.NumberInput(attrs={'placeholder': 'mulitple of 0.5 cm'}))
+
+    def clean_minimum_length(self):
+        minimum_length = self.cleaned_data['minimum_length']
+        if minimum_length % 0.5 > 0: #if length is not divisible by 0.5
+            raise forms.ValidationError("Number must be a multiple of 0.5!")
+        return minimum_length
+
+    def clean_maximum_length(self):
+        maximum_length = self.cleaned_data['maximum_length']
+        if maximum_length % 0.5 > 0: #if length is not divisible by 0.5
+            raise forms.ValidationError("Number must be a multiple of 0.5!")
+        elif maximum_length < self.cleaned_data['minimum_length']:
+            raise forms.ValidationError("Maxium length must be greater than or equal to minimum length!")
+        return maximum_length
+
+class LengthFrquencyWizardForm(forms.Form):
+    count = forms.IntegerField(required=True, min_value=0, max_value=1000, widget=forms.NumberInput(attrs={'placeholder': 'Number of fish observed'}))
