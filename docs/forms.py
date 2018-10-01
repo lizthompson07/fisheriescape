55eb2171adc8025a8b4974a228727a23a01325c9
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-10-01 15:18:55 -0300

    updated oceanography doc library so that records can be created and edited and can now have file attachments

---
+++ docs/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.core import validators
+from . import models
+from django.utils.safestring import mark_safe
+
+
+class DocForm(forms.ModelForm):
+    class Meta:
+        model = models.Doc
+        fields = "__all__"
+        labels={
+            'district':mark_safe("District (<a href='#' >search</a>)"),
+            'vessel':mark_safe("Vessel CFVN (<a href='#' >add</a>)"),
+        }
+        widgets = {
+            'description':forms.Textarea(attrs={'rows': '5'}),
+            'source':forms.Textarea(attrs={'rows': '5'}),
+
+        }
--- docs/models.py
+++ docs/models.py
@@ -1,22 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+def img_file_name(instance, filename):
+    img_name = 'oceanography/{}'.format(filename)
+    return img_name
 
-class doc(models.Model):
-
-    item_name = models.CharField(max_length=255)
-    section = models.ForeignKey('section', on_delete=models.DO_NOTHING)
+class Doc(models.Model):
+    title = models.CharField(max_length=255)
     description = models.TextField(null=True, blank=True)
     source = models.TextField(null=True, blank=True)
     url = models.TextField(null=True, blank=True)
+    file = models.FileField(null=True, blank=True, upload_to=img_file_name)
+    date_modified = models.DateTimeField(default = timezone.now )
+
+    def save(self,*args,**kwargs):
+        self.date_modified  = timezone.now()
+        return super().save(*args,**kwargs)
+
 
     def __str__(self):
         return self.item_name
-
-class section(models.Model):
-
-    section_name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.section_name
