72fb579d589d2aa17bb64ec02ef8d50a7822a6d0
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-10-03 15:44:36 -0300

    building oceanography

--- oceanography/forms.py
+++ oceanography/forms.py
@@ -21,13 +21,25 @@ class DocForm(forms.ModelForm):
 class MissionForm(forms.ModelForm):
     class Meta:
         model = models.Mission
-        fields = "__all__"
+        exclude = ["season",]
         # labels={
         #     'district':mark_safe("District (<a href='#' >search</a>)"),
         #     'vessel':mark_safe("Vessel CFVN (<a href='#' >add</a>)"),
         # }
-        # widgets = {
-        #     'description':forms.Textarea(attrs={'rows': '5'}),
-        #     'source':forms.Textarea(attrs={'rows': '5'}),
-        #
+        widgets = {
+            'start_date':forms.DateInput(attrs={'type': 'date'}),
+            'end_date':forms.DateInput(attrs={'type': 'date'}),
+        }
+
+class BottleForm(forms.ModelForm):
+    class Meta:
+        model = models.Bottle
+        exclude = ["mission",]
+        # labels={
+        #     'district':mark_safe("District (<a href='#' >search</a>)"),
+        #     'vessel':mark_safe("Vessel CFVN (<a href='#' >add</a>)"),
         # }
+        widgets = {
+            'start_date':forms.DateInput(attrs={'type': 'date'}),
+            'end_date':forms.DateInput(attrs={'type': 'date'}),
+        }
