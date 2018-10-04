556bfd0bfffdf3c68d75f39891c99618d947dbe6
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-10-04 16:27:01 -0300

    file delete recievers added to dm_ticket app and oceanography app

--- oceanography/models.py
+++ oceanography/models.py
@@ -1,6 +1,7 @@
+import os
+from django.dispatch import receiver
 from django.db import models
 from django.utils import timezone
-
 # Create your models here.
 
 def img_file_name(instance, filename):
@@ -100,4 +101,33 @@ class File(models.Model):
     def __str__(self):
         return self.caption
 
-    
+
+@receiver(models.signals.post_delete, sender=File)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+
+@receiver(models.signals.pre_save, sender=File)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    """
+    Deletes old file from filesystem
+    when corresponding `MediaFile` object is updated
+    with new file.
+    """
+    if not instance.pk:
+        return False
+
+    try:
+        old_file = File.objects.get(pk=instance.pk).file
+    except File.DoesNotExist:
+        return False
+
+    new_file = instance.file
+    if not old_file == new_file:
+        if os.path.isfile(old_file.path):
+            os.remove(old_file.path)
