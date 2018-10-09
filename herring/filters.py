7cfac17c816cc336cae2d0c294f3952b6ddc31ea
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-10-08 22:05:04 -0300

    fixed bug in grais with template tag missing; minor aesthetic change to herring filter page"

--- herring/filters.py
+++ herring/filters.py
@@ -3,9 +3,9 @@ from . import models
 from django import forms
 
 class SampleFilter(django_filters.FilterSet):
-    SeasonSince = django_filters.NumberFilter(field_name='season', label="Since year", lookup_expr='gte', widget= forms.NumberInput(attrs={'style':"width: 4em"}))
-    SeasonExact = django_filters.NumberFilter(field_name='season', label="From year", lookup_expr='exact', widget= forms.NumberInput(attrs={'style':"width: 4em"}))
-    SamplerRefNumber = django_filters.NumberFilter(field_name='sampler_ref_number', label="Sampler's reference no.", lookup_expr='exact', widget= forms.NumberInput(attrs={'style':"width: 4em"}))
+    SeasonSince = django_filters.NumberFilter(field_name='season', label="Since year", lookup_expr='gte', widget= forms.NumberInput())
+    SeasonExact = django_filters.NumberFilter(field_name='season', label="From year", lookup_expr='exact', widget= forms.NumberInput())
+    SamplerRefNumber = django_filters.NumberFilter(field_name='sampler_ref_number', label="Sampler's reference no.", lookup_expr='exact', widget= forms.NumberInput())
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -18,9 +18,3 @@ class SampleFilter(django_filters.FilterSet):
             # 'sampler_ref_number':['exact'],
             'sampling_protocol':['exact'],
         }
-
-
-        # def __init__(self, *args, **kwargs):
-        #     super(SampleFilter, self).__init__(*args, **kwargs)
-        #     self.filters['season'].extra.update(
-        #         {'empty_label': 'All Manufacturers'})
