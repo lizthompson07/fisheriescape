d46483bd00f24de928041bccfc0cfb40b3020536
davjfish <FISHMAND@dfo-mpo.ca>
2018-10-12 16:14:08 -0300

    inventory: added flag for deletion view, template and email

--- inventory/emails.py
+++ inventory/emails.py
@@ -34,7 +34,7 @@ class FlagForDeletionEmail:
         self.to_list = [admin_email,]
 
     def load_html_template(self):
-        t = loader.get_template('inventory/email_certification_request.html')
+        t = loader.get_template('inventory/email_flaged_for_deletion.html')
         context ={
             'object': object,
         }
