f4b5b979a60f43e415ad940baf3903242fdaf754
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-11-16 11:33:59 -0400

    herring and grais updates

---
+++ grais/todo.py
@@ -0,0 +1,17 @@
+# TODO: add sampler button
+# TODO: multieple samplers
+# TODO: ability to add discrete entries to a sample, user who is logged in is the author; default date created
+# TODO: item to do
+
+    # TODO: sampler detail: contact info who they work for; email, phone
+# TODO: logger data and profile datasets are taken at each sample. must decide what to do: add file attachment; integrate data
+
+# TODO: probe measurement, add textbox for weather / notes; add turbidity field
+
+# TODO: for collector number, it should auto populate field when created
+# TODO: collector does not need to be anything but a text field
+# TODO: make one search bar for species "search term"
+# TODO: add delete button for species
+# TODO: species insert list should also have scientific name
+# TODO: line detail heder should be TAG # 820 + surface name PLATE 1
+# TODO: line detail : invasive spp is problematic
