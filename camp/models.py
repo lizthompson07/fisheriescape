aa8b4dc3e3fa12640e338b773380f11f82dc7fd2
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-11-19 16:03:23 -0400

    camp build

--- camp/models.py
+++ camp/models.py
@@ -66,25 +66,86 @@ class Species(models.Model):
 
 
 class Sample(models.Model):
+    # Choices for turbidity
+    TURBID = 1
+    CLEAR = 2
+    TURBIDITY_CHOICES = (
+        (TURBID, "Turbid"),
+        (CLEAR, "Clear"),
+    )
+
+    # choice for tide_state
+    LOW = 'l'
+    MID = 'm'
+    HIGH = 'h'
+    TIDE_STATE_CHOICES = (
+        (HIGH, "High"),
+        (MID, "Mid"),
+        (LOW, "Low"),
+    )
+
+    # choice for tide_direction
+    INCOMING = 'in'
+    OUTGOING = 'out'
+    TIDE_DIR_CHOICES = (
+        (INCOMING, "Incoming"),
+        (OUTGOING, "Outgoing"),
+    )
+
+    # Choices for timezone
+    AST = 'AST'
+    ADT = 'ADT'
+    UTC = 'UTC'
+    TIMEZONE_CHOICES = (
+        (AST, 'AST'),
+        (ADT, 'ADT'),
+        (UTC, 'UTC'),
+    )
+
     station = models.ForeignKey(Station, related_name='samples', on_delete=models.DO_NOTHING)
-    sample_start_date = models.DateTimeField()
-    sample_end_date = models.DateTimeField(blank=True, null=True)
+    timezone = models.CharField(max_length=5, choices=TIMEZONE_CHOICES, blank=True, null=True)
+    start_date = models.DateTimeField(verbose_name="Start date / time (yyyy-mm-dd hh:mm:ss)")
+    end_date = models.DateTimeField(blank=True, null=True, verbose_name="End date / time (yyyy-mm-dd hh:mm:ss)")
+    weather_notes = models.CharField(max_length=1000, blank=True, null=True)
     temperature_c = models.FloatField(null=True,blank=True, verbose_name="Temperature (°C)")
     salinity = models.FloatField(null=True,blank=True, verbose_name="Salinity (ppt)")
-    disolved_o2 = models.FloatField(null=True,blank=True, verbose_name="dissolved oxygen (?mgl?)")
+    dissolved_o2 = models.FloatField(null=True,blank=True, verbose_name="dissolved oxygen (mg/L)")
+    water_turbidity = models.IntegerField(choices=TURBIDITY_CHOICES, blank=True, null=True)
+    tide_state = models.CharField(max_length=5, choices=TIDE_STATE_CHOICES, blank=True, null=True)
+    tide_direction = models.CharField(max_length=5, choices=TIDE_DIR_CHOICES, blank=True, null=True)
+    rain_past_24_hours = models.BooleanField(default=False, verbose_name="Has it rained in the past 24 h?")
+    samplers = models.CharField(max_length=1000, blank=True, null=True)
+
+    percent_sand = models.FloatField(null=True,blank=True, verbose_name="Sand (%)")
+    percent_gravel = models.FloatField(null=True,blank=True, verbose_name="Gravel (%)")
+    percent_rock = models.FloatField(null=True,blank=True, verbose_name="Rock (%)")
+    percent_mud = models.FloatField(null=True,blank=True, verbose_name="Mud (%)")
+
+    visual_sediment_obs = models.CharField(max_length=1000, blank=True, null=True, verbose_name="Visual sediment observations")
+    sav_survey_conducted = models.BooleanField(default=False, verbose_name="Was SAV survey conducted?")
+    excessive_green_algae_water = models.BooleanField(default=False, verbose_name="Excessive green algae in water?")
+    excessive_green_algae_shore = models.BooleanField(default=False, verbose_name="Excessive green algae on shore?")
+    unsampled_vegetation_inside = models.CharField(max_length=1000, blank=True, null=True, verbose_name="Vegetation present inside sample area (underwater) but outside of quadrat")
+    unsampled_vegetation_outside = models.CharField(max_length=1000, blank=True, null=True, verbose_name="Vegetation present outside of sample area (underwater)")
+
+
+
     per_sediment_water_cont = models.FloatField(null=True,blank=True, verbose_name="sediment water content (%)")
     per_sediment_organic_cont = models.FloatField(null=True,blank=True, verbose_name="sediment organic content (%)")
     mean_sediment_grain_size = models.FloatField(null=True,blank=True, verbose_name="Mean sediment grain size (??)") # where 9999 means >2000
+
     silicate = models.FloatField(null=True,blank=True, verbose_name="Silicate (µM)")
     phosphate = models.FloatField(null=True,blank=True, verbose_name="Phosphate (µM)")
     nitrates = models.FloatField(null=True,blank=True, verbose_name="NO3 + NO2(µM)")
     nitrite = models.FloatField(null=True,blank=True, verbose_name="Nitrite (µM)")
     ammonia = models.FloatField(null=True,blank=True, verbose_name="Ammonia (µM)")
     notes = models.TextField(blank=True, null=True)
+
     year = models.IntegerField(null=True, blank=True)
     month = models.IntegerField(null=True, blank=True)
     last_modified = models.DateTimeField(blank=True, null=True)
     species = models.ManyToManyField(Species, through="SpeciesObservations")
+
 
     def save(self, *args, **kwargs):
         self.year = self.sample_start_date.year
@@ -94,8 +155,8 @@ class Sample(models.Model):
         super().save(*args, **kwargs)
 
     class Meta:
-        ordering = ['-sample_start_date', 'station']
-        unique_together = [["sample_start_date","station"],]
+        ordering = ['-start_date', 'station']
+        unique_together = [["start_date","station"],]
 
 
 class SpeciesObservations(models.Model):
