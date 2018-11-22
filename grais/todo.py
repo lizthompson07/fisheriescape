2c1cf218554af7fd3508a3c140c7d41fbc7a0ddd
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-11-22 10:37:43 -0400

    improvement of js on herring.otolith_form.html

--- grais/todo.py
+++ grais/todo.py
@@ -3,3 +3,6 @@
 
 # TODO: french species names.. need to have field and to populate table
 # TODO: report followups
+# TODO: mobile/sessile field for spp
+# TODO: species obs at the line level. need to have a text field for notes.
+# TODO: species obs at the sample level. need to have a text field for notes.
