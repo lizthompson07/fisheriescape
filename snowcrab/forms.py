960d1e50299e2b87c8a52d387591e4ec36bbb8c0
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-12-04 08:47:39 -0400

    snowcrab build

--- snowcrab/forms.py
+++ snowcrab/forms.py
@@ -3,34 +3,20 @@ from django.core import validators
 from . import models
 from django.utils.safestring import mark_safe
 
-#
-# class DocForm(forms.ModelForm):
-#     class Meta:
-#         model = models.Doc
-#         fields = "__all__"
-#         labels={
-#             'district':mark_safe("District (<a href='#' >search</a>)"),
-#             'vessel':mark_safe("Vessel CFVN (<a href='#' >add</a>)"),
-#         }
-#         widgets = {
-#             'description':forms.Textarea(attrs={'rows': '5'}),
-#             'source':forms.Textarea(attrs={'rows': '5'}),
-#
-#         }
-#
-# class MissionForm(forms.ModelForm):
-#     class Meta:
-#         model = models.Mission
-#         exclude = ["season",]
-#         # labels={
-#         #     'district':mark_safe("District (<a href='#' >search</a>)"),
-#         #     'vessel':mark_safe("Vessel CFVN (<a href='#' >add</a>)"),
-#         # }
-#         widgets = {
-#             'start_date':forms.DateInput(attrs={'type': 'date'}),
-#             'end_date':forms.DateInput(attrs={'type': 'date'}),
-#         }
-#
+
+class CruiseForm(forms.ModelForm):
+    class Meta:
+        model = models.Cruise
+        exclude = ["season",]
+        # labels={
+        #     'district':mark_safe("District (<a href='#' >search</a>)"),
+        #     'vessel':mark_safe("Vessel CFVN (<a href='#' >add</a>)"),
+        # }
+        widgets = {
+            'start_date':forms.DateInput(attrs={'type': 'date'}),
+            'end_date':forms.DateInput(attrs={'type': 'date'}),
+        }
+
 # class BottleForm(forms.ModelForm):
 #     class Meta:
 #         model = models.Bottle
--- snowcrab/urls.py
+++ snowcrab/urls.py
@@ -6,9 +6,17 @@ app_name = 'crab'
 urlpatterns = [
     path('', views.IndexTemplateView.as_view(), name='index'),
 
-    # # CRUISE #
+    # CRUISE #
     # ##########
-    # path('missions/', views.MissionYearListView.as_view(), name='mission_year_list'),
+    path('cruises/', views.CruiseListView.as_view(), name='cruise_list'),
+    path('cruise/<int:pk>/view/', views.CruiseDetailView.as_view(), name='cruise_detail'),
+    path('cruise/<int:pk>/edit/', views.CruiseUpdateView.as_view(), name='cruise_edit'),
+
+    # SETS #
+    ########
+    path('set/<int:pk>/view/', views.SetDetailView.as_view(), name='set_detail'),
+    # path('cruise/<int:pk>/edit/', views.CruiseUpdateView.as_view(), name=
+
     # path('missions/<int:year>/list/', views.MissionListView.as_view(), name='mission_list'),
     # path('missions/<int:pk>/view/', views.MissionDetailView.as_view(), name='mission_detail'),
     # path('missions/<int:pk>/edit/', views.MissionUpdateView.as_view(), name='mission_edit'),
