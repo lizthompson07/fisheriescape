c366e0be8d6048c6ab6d0e9ae6620566fef1f053
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-12-14 16:04:42 -0400

    removed unused packages from inventory.models

--- inventory/models.py
+++ inventory/models.py
@@ -1,15 +1,10 @@
 
-from django.core.validators import MaxValueValidator, MinValueValidator
-from django.conf import settings
-from django.contrib import auth
-from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.utils import timezone
-import misaka
 import os
 import uuid
 
