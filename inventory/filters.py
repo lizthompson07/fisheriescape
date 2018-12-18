df331a5d1901212022ba9b1932660d08dd8fe698
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-12-18 16:26:02 -0400

    improve search filter by creating annotated field

--- inventory/filters.py
+++ inventory/filters.py
@@ -4,36 +4,40 @@ from django import forms
 from . import models
 import django_filters
 
+
 class ResourceFilter(django_filters.FilterSet):
     # generate a list of people from inventory.people
     person_list = []
 
     person_list = [p.user_id for p in models.Person.objects.all()]
     PEOPLE_CHOICES = []
-    for u in User.objects.all().order_by("last_name","first_name"):
+    for u in User.objects.all().order_by("last_name", "first_name"):
         if u.id in person_list:
-            PEOPLE_CHOICES.append((u.id,"{}, {}".format(u.last_name,u.first_name)))
+            PEOPLE_CHOICES.append((u.id, "{}, {}".format(u.last_name, u.first_name)))
 
     STATUS_CHOICES = [(s.id, str(s)) for s in models.Status.objects.all()]
     SECTION_CHOICES = [(s.id, str(s)) for s in models.Section.objects.all()]
 
-    search_term = django_filters.CharFilter(field_name='search_term', label="Search term", lookup_expr='icontains', widget= forms.TextInput())
-    section = django_filters.ChoiceFilter(field_name="section", label = "Section", lookup_expr='exact', choices=SECTION_CHOICES)
-    person = django_filters.ChoiceFilter(field_name="people", label = "Person", lookup_expr='exact', choices=PEOPLE_CHOICES)
-    status = django_filters.ChoiceFilter(field_name="status", label = "Status", lookup_expr='exact', choices=STATUS_CHOICES)
+    search_term = django_filters.CharFilter(field_name='search_term', label="Search term", lookup_expr='icontains',
+                                            widget=forms.TextInput())
+    section = django_filters.ChoiceFilter(field_name="section", label="Section", lookup_expr='exact',
+                                          choices=SECTION_CHOICES)
+    person = django_filters.ChoiceFilter(field_name="people", label="Person", lookup_expr='exact',
+                                         choices=PEOPLE_CHOICES)
+    status = django_filters.ChoiceFilter(field_name="status", label="Status", lookup_expr='exact',
+                                         choices=STATUS_CHOICES)
 
 
 class PersonFilter(django_filters.FilterSet):
-
     class Meta:
         model = models.Person
         fields = {
             'full_name': ['icontains'],
         }
 
+
 class KeywordFilter(django_filters.FilterSet):
     search_term = django_filters.CharFilter(label="Keyword Search Term", lookup_expr='icontains')
-
 
 
 class CitationFilter(django_filters.FilterSet):
