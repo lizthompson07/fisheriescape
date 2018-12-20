5bba37a13b923c0e7ce3a617e00bab8bdfca6313
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2018-12-20 16:30:53 -0400

    working version of xlsx report

--- camp/reports.py
+++ camp/reports.py
@@ -1,5 +1,9 @@
+import csv
 import statistics
 
+import pandas
+import xlsxwriter as xlsxwriter
+from django.template.defaultfilters import yesno
 from math import pi
 
 from bokeh.io import show, export_png, export_svgs
@@ -10,6 +14,8 @@ from bokeh.transform import cumsum
 from django.db.models import Sum, Q
 from shutil import rmtree
 from django.conf import settings
+
+from lib.functions.nz import nz
 from . import models
 import numpy as np
 import os
@@ -228,10 +234,12 @@ def generate_species_richness_report(site=None):
 
     save(p)
 
+
 WIDTH = 1200
 HEIGHT = 800
 TITLE_FONT_SIZE = '16pt'
 LEGEND_FONT_SIZE = '12pt'
+
 
 def generate_annual_watershed_report(site, year):
     # start assigning files and by cleaning the temp dir
@@ -521,3 +529,157 @@ def generate_sub_green_crab(site, target_file):
 
     # show(p)
     export_png(p, filename=target_file)
+
+
+def generate_annual_watershed_spreadsheet(site, year):
+    # figure out the filename
+    target_dir = os.path.join(settings.BASE_DIR, 'media', 'camp', 'temp')
+    target_file = "temp_data_export.xlsx"
+    target_file_path = os.path.join(target_dir, target_file)
+    target_url = os.path.join(settings.MEDIA_ROOT, 'camp', 'temp', target_file)
+
+    # create spreadsheet
+    workbook = xlsxwriter.Workbook(target_file_path)
+    worksheet1 = workbook.add_worksheet(name="Fauna")
+    worksheet2 = workbook.add_worksheet(name="Sediment")
+    worksheet3 = workbook.add_worksheet(name="Vegetation")
+
+    # get a list of species obs for which total > 0
+    species_list = models.Species.objects.filter(sav=False).order_by("code")
+    sample_list = models.Sample.objects.filter(year=year).filter(station__site=site).order_by("start_date",
+                                                                                              "station__station_number")
+
+    species_header_list = []
+    for s in species_list:
+        species_header_list.append("{}(A)".format(s.code))
+        species_header_list.append("{}(YOY)".format(s.code))
+        species_header_list.append("{}(TOT)".format(s.code))
+
+    header_eng = [
+        "Site location",
+        "Station #",
+        "Station name",
+        "Date",
+        "Month",
+        "Year",
+        "Time start",
+        "Time finish",
+        "Water Sample #",
+        "Rain 24h Y / N",
+        "Stage of tide",
+        "Samplers name",
+        "Water temp",
+        "Salinity",
+        "Dissolved Oxygen",
+        "Water turbidity",
+        "SP. RICHNESS",
+        "TOTAL",
+    ]
+
+    header_fre = [
+        "Location Site",
+        "# Station",
+        "Nom station",
+        "(dj/mm/ya)",
+        "Mois",
+        "Année",
+        "Heure début",
+        "Heure fin",
+        "# échantillon d'eau",
+        "Pluie 24h O / N",
+        "Stade de marée",
+        "Nom échantillonneurs",
+        "Temp eau",
+        "Salinité",
+        "Oxygène dissout",
+        "Turbidité de l'eau",
+        "SP. RICHNESS",
+        "TOTAL",
+    ]
+
+    header_eng[-2:-2] = species_header_list
+    header_fre[-2:-2] = species_header_list
+    my_df = pandas.DataFrame([header_eng, header_fre], columns=[i for i in range(0, len(header_eng))])
+
+    # write some data
+    for s in sample_list:
+        data_row = [
+            s.station.site.site,
+            s.station.station_number,
+            s.station.name,
+            s.start_date.strftime("%d/%m/%Y"),
+            s.start_date.month,
+            s.start_date.year,
+            s.start_date.strftime("%H:%M"),
+            s.end_date.strftime("%H:%M"),
+            s.nutrient_sample_id,
+            yesno(s.rain_past_24_hours),
+            "{} - {}".format(s.get_tide_state_display(), s.get_tide_direction_display(), ),
+            s.samplers,
+            s.h2o_temperature_c,
+            s.salinity,
+            s.dissolved_o2,
+            s.get_water_turbidity_display(),
+        ]
+
+        # now get species data
+        species_obs_list = []
+
+        for species in species_list:
+            try:
+                species_obs = models.SpeciesObservation.objects.get(sample=s, species=species)
+            except:
+                species_obs_list.append(0)
+                species_obs_list.append(0)
+                species_obs_list.append(0)
+            else:
+                nz(species_obs_list.append(species_obs.adults), 0)
+                nz(species_obs_list.append(species_obs.yoy), 0)
+                species_obs_list.append(species_obs.total_non_sav)
+
+        data_row.extend(species_obs_list)
+
+        # species richness
+        annual_obs = models.SpeciesObservation.objects.filter(sample=s, species__sav=False).values(
+            'species_id',
+        ).distinct()
+        species_set = set([i["species_id"] for i in annual_obs])
+        data_row.append(len(species_set))
+
+        # total count
+        total = models.SpeciesObservation.objects.filter(sample=s).filter(species__sav=False).values(
+            'sample_id'
+        ).distinct().annotate(dsum=Sum('total_non_sav'))
+        data_row.append(total[0]['dsum'])
+        my_df = my_df.append(pandas.DataFrame([data_row, ], columns=[i for i in range(0, len(data_row))]),
+                             ignore_index=True)
+
+    # create formatting
+    header_format = workbook.add_format(
+        {'bold': True, 'border': 1, 'border_color': 'black', 'bg_color': '#8C96A0', "align": 'center'})
+    total_format = workbook.add_format({'bg_color': '#D6D1C0', "align": 'center'})
+    normal_format = workbook.add_format({"align": 'center'})
+
+    # write dataframe to xlsx
+    for i in range(0, my_df.shape[0]):
+        for j in range(0, my_df.shape[1]):
+            # print("{},{}".format(i,j))
+
+            my_val = my_df[j][i]
+            if i <= 1:
+                worksheet1.write(i, j, my_val, header_format)
+            elif "(TOT)" in my_df[j][0]:
+                worksheet1.write(i, j, my_val, total_format)
+            else:
+                worksheet1.write(i, j, my_val, normal_format)
+
+    col_max = [max([len(str(s)) for s in my_df[j].values]) for j in my_df.columns]
+
+    for j in my_df.columns:
+        worksheet1.set_column(j, j, width=col_max[j]*1.1)
+
+
+
+
+    workbook.close()
+    return target_url
