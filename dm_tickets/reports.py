be6b7beb4844212004e06cd3c80b46230eb767bf
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-08 15:51:00 -0400

    new report for finance in dm tickets

---
+++ dm_tickets/reports.py
@@ -0,0 +1,78 @@
+import xlsxwriter as xlsxwriter
+from django.conf import settings
+from django.utils import timezone
+from . import models
+import os
+
+
+def generate_finance_spreadsheet():
+    # figure out the filename
+    target_dir = os.path.join(settings.BASE_DIR, 'media', 'dm_tickets', 'temp')
+    target_file = "temp_data_export.xlsx"
+    target_file_path = os.path.join(target_dir, target_file)
+    target_url = os.path.join(settings.MEDIA_ROOT, 'dm_tickets', 'temp', target_file)
+
+    # get a sample list for the site / year
+    tickets = models.Ticket.objects.filter(financial_follow_up_needed=True).filter(
+        sd_ref_number__isnull=False).order_by("-date_opened")
+
+    # create workbook and worksheets
+    workbook = xlsxwriter.Workbook(target_file_path)
+    worksheet1 = workbook.add_worksheet(name="DM Tickets")
+
+    header_format = workbook.add_format(
+        {'bold': True, 'bg_color': '#8C96A0', "align": 'center'})
+
+    header_eng = [
+        "Fiscal Year",
+        "DM Ticket #",
+        "Service Desk #",
+        "Section",
+        "Primary Client",
+        "Title",
+        "Request Type",
+        "Estimated Cost",
+        "Coding",
+        "Status",
+        "Date Opened",
+        "Date Closed",
+    ]
+
+    worksheet1.write_row(0, 0, header_eng, header_format)
+    i = 1
+    for ticket in tickets:
+        if ticket.date_opened.month >= 4:
+            fiscal_year = "{}-{}".format(ticket.date_opened.year - 1, ticket.date_opened.year)
+        elif timezone.now().year > ticket.date_opened.year:
+            fiscal_year = "{}-{}".format(ticket.date_opened.year - 2, ticket.date_opened.year - 1)
+        else:
+            fiscal_year = "{}-{}".format(ticket.date_opened.year, ticket.date_opened.year + 1)
+
+        if ticket.date_opened:
+            date_opened = ticket.date_opened.strftime('%Y-%m-%d')
+        else:
+            date_opened = None
+
+        if ticket.date_closed:
+            date_closed = ticket.date_closed.strftime('%Y-%m-%d')
+        else:
+            date_closed = None
+
+        row_data = [
+            fiscal_year,
+            ticket.id,
+            ticket.sd_ref_number,
+            ticket.section.section_name,
+            "{} {}".format(ticket.primary_contact.first_name, ticket.primary_contact.last_name),
+            ticket.title,
+            ticket.request_type.request_type,
+            ticket.estimated_cost,
+            ticket.financial_coding,
+            ticket.get_status_display(),
+            date_opened,
+            date_closed,
+        ]
+        worksheet1.write_row(i, 0, row_data)
+        i += 1
+    workbook.close()
+    return target_url
