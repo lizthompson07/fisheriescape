a057b87f821232d42c9fb526dcad7edbd94ba9fd
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-14 16:34:23 -0400

    building projects app

---
+++ projects/todo.py
@@ -0,0 +1,6 @@
+# TODO: on person form, the student dropdown should be disabled unless type = student
+# TODO: delete the project attributes
+# TODO: summary table
+# TODO: when create a new project, it should load with a whack load of categories.
+# TODO: remove ablity to add students and post-doc from staff..? Or we can just leave it.
+# TODO: when there is nothing in a sub table, it should say nothing! and not show an empty table
