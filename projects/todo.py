44bd9ba3960df394ea92452276a429b47da08917
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-14 23:47:17 -0400

    building projects app

--- projects/todo.py
+++ projects/todo.py
@@ -1,9 +1,6 @@
 # TODO: remove ablity to add students and post-doc from staff..? Or we can just leave it.
 # TODO: filter for projects
 # TODO: report for employee by FTE
-# TODO: my section
-# TODO: staff form: disable cost field when FTE
 # TODO: not sure what to do with overtime hours
-# TODO: you should only be able to delete a project if a) you are superuser or b) it is one of your projects
-# TODO: when you click submit project, should take you to a review page
-# TODO: there should be a print report option
+# TODO: print project as a pdf?
+# TODO: index page... with instructions
