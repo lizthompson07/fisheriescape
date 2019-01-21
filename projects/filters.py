48afe5c802b69799cf35e66366f69422279a570f
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-21 15:05:29 -0400

    a few clean up changes around projects app. reports component of app still under dev

--- projects/filters.py
+++ projects/filters.py
@@ -13,18 +13,12 @@ class ProjectFilter(django_filters.FilterSet):
     # SECTION_CHOICES = [(None, "----"),]
     DIVISION_CHOICES = [(d.id, str(d)) for d in models.Division.objects.all()]
     SECTION_CHOICES = [(s.id, str(s)) for s in models.Section.objects.all()]
+    YES_NO_CHOICES = [(True, "Yes"), (False, "No"), ]
 
-    fiscal_year = django_filters.ChoiceFilter(field_name='fiscal_year', lookup_expr='exact', choices=FY_CHOICES)
     project_title = django_filters.CharFilter(field_name='project_title', lookup_expr='icontains')
+    fiscal_year = django_filters.ChoiceFilter(field_name='fiscal_year', lookup_expr='exact', choices=FY_CHOICES)
     division = django_filters.ChoiceFilter(field_name='section__division', lookup_expr='exact', label="Division", choices=DIVISION_CHOICES)
     section = django_filters.ChoiceFilter(field_name='section', lookup_expr='exact', label="Section", choices=SECTION_CHOICES)
+    submitted = django_filters.ChoiceFilter(field_name='submitted', lookup_expr='exact', label="Submitted?", choices=YES_NO_CHOICES)
 
-    # class Meta:
-    #     model = models.Project
-    #     fields = {
-    #         'fiscal_year': ['iexact'],
-    #         'project_title': ['icontains'],
-    #         'division': ['exact'],
-    #         'section': ['exact'],
-    #     }
 
