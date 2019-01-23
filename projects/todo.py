dcd74aa32c816cd74e2a0e3402b09807a59e3449
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-23 06:55:51 -0400

    updates to projects app including development of first xls report

--- projects/todo.py
+++ projects/todo.py
@@ -1,7 +1,4 @@
 # TODO: remove ablity to add students and post-doc from staff..? Or we can just leave it.
-# TODO: filter for projects
-# TODO: report for employee by FTE
 # TODO: not sure what to do with overtime hours
-# TODO: print project as a pdf?
 # TODO: index page... with instructions
 # TODO: add new users...
