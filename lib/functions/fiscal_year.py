edaf4e758a1040b18f594baf3e95a881804588dd
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-23 06:08:50 -0400

    updates to projects app including development of first xls report

--- lib/functions/fiscal_year.py
+++ lib/functions/fiscal_year.py
@@ -1,12 +1,18 @@
 from django.utils import timezone
 
 
-def fiscal_year(date=timezone.now()):
+def fiscal_year(next=False, date=timezone.now()):
     """
     Function will return a fiscal year value. if no arg is given, current date will be used.
     """
 
     if date.month >= 4:
-        return "{}-{}".format(date.year, date.year + 1)
+        if next:
+            return "{}-{}".format(date.year + 1, date.year + 2)
+        else:
+            return "{}-{}".format(date.year, date.year + 1)
     else:
-        return "{}-{}".format(date.year - 1, date.year)
+        if next:
+            return "{}-{}".format(date.year, date.year + 1)
+        else:
+            return "{}-{}".format(date.year - 1, date.year)
