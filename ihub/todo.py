aa7c61df2aa4cc26209dddf61e6ba9ddec7d705b
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-29 11:30:25 -0400

    authentication in grAIS has been updated

--- ihub/todo.py
+++ ihub/todo.py
@@ -1,2 +1,3 @@
 # TODO: sort out the issue with groupings... do they belong with an entry or org? 
 # TODO: email to stacy when a new record is created
+# TODO: set up the capacity report.. this should be a mulitselect field for organization on the report search page
