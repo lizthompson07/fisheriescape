f895cb44830fa96598e9b6e25b7875563b134570
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-29 14:40:36 -0400

    scifi build

--- lib/templatetags/verbose_names.py
+++ lib/templatetags/verbose_names.py
@@ -1,9 +1,111 @@
 from django import template
+from django.template.defaultfilters import yesno
+from django.utils.safestring import SafeString
+
 register = template.Library()
+
 
 @register.simple_tag
 def get_verbose_field_name(instance, field_name):
     """
-    Returns verbose_name for a field.
+    Returns verbose_name for a field. DEPRECATED use get_verbose_label instead
     """
     return instance._meta.get_field(field_name).verbose_name.capitalize()
+
+
+@register.simple_tag
+def get_verbose_label(instance, field_name):
+    """
+    Returns verbose_name for a field.
+    """
+    # check to see if there were any arguments passed in with the field name
+    if len(field_name.split(".")) > 1:
+        arg = field_name.split(".")[1]
+        field_name = field_name.split(".")[0]
+
+    # complicated little dance to make sure the first letter is capitalized.
+    # Do not want to use the capitalize() method since it makes the remaining portion of str lowercase
+    field_instance = instance._meta.get_field(field_name)
+    verbose_name = field_instance.verbose_name
+    first_letter = verbose_name[0].upper()
+    str_list = list(verbose_name)
+    str_list[0] = first_letter
+    verbose_name = "".join(str_list)
+
+    return verbose_name
+
+
+@register.simple_tag
+def get_field_value(instance, field_name, format=None, display_time=False):
+    """
+    Returns verbose_name for a field. To return a field from a foreign key, send in the field name as such: "user.first_name"
+    """
+    # check to see if there were any arguments passed in with the field name
+    if len(field_name.split(".")) > 1:
+        arg = field_name.split(".")[1]
+        field_name = field_name.split(".")[0]
+        try:
+            field_value = getattr(getattr(instance, field_name), arg)
+        except:
+            field_value = "n/a"
+
+    else:
+        field_instance = instance._meta.get_field(field_name)
+
+        # first check if there is a value :
+        if getattr(instance, field_name) is not None:
+
+            # check to see if there are choices
+            if len(field_instance.choices) > 0:
+                field_value = getattr(instance, "get_{}_display".format(field_name))()
+
+            # check to see if it is a datefield
+            elif field_instance.get_internal_type() == 'DateTimeField':
+                datetime_obj = getattr(instance, field_name)
+                if display_time:
+                    field_value = datetime_obj.strftime('%Y-%m-%d %H:%M')
+                else:
+                    field_value = datetime_obj.strftime('%Y-%m-%d')
+
+            # check to see if it is a url
+            elif str(getattr(instance, field_name)).startswith("http"):
+                field_value = '<a href="{url}">{url}</a>'.format(url=getattr(instance, field_name))
+
+            # check to see if it is a BooleanField
+            elif field_instance.get_internal_type() == 'BooleanField' or field_instance.get_internal_type() == 'NullBooleanField':
+                field_value = yesno(getattr(instance, field_name), "Yes,No,Unknown")
+
+            else:
+                field_value = getattr(instance, field_name)
+        else:
+            field_value = "n/a"
+
+    # TODO: specify special formatting
+    if format == "currency":
+        try:
+            field_value = '${:,.2f}'.format(int(field_value))
+        except:
+            print("cannot coerce formatting to field value")
+
+    return field_value
+
+
+@register.simple_tag
+def verbose_field_display(instance, field_name, format=None, display_time=False, url=None):
+    """
+    Returns a standard display block for a field based on the verbose fieldname
+    """
+    field_instance = instance._meta.get_field(field_name)
+
+    # call on the get_verbose_label func to handle label prep
+    verbose_name = get_verbose_label(instance, field_name)
+
+    # call on the get_field_value func to handle field value prep
+    field_value = get_field_value(instance, field_name, format, display_time)
+
+    if url and field_value != "n/a":
+        html_block = '<p><span class="label">{}:</span><br><a href="{}">{}</a></p>'.format(verbose_name, url, field_value)
+    else:
+        html_block = '<p><span class="label">{}:</span><br>{}</p>'.format(verbose_name, field_value)
+
+    return SafeString(html_block)
--- scifi/urls.py
+++ scifi/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path('allotment-code/new/', views.AllotmentCodeCreateView.as_view(), name="allotment_new"),
     path('allotment-code/<int:pk>/edit/', views.AllotmentCodeUpdateView.as_view(), name="allotment_edit"),
     path('allotment-code/<int:pk>/delete/', views.AllotmentCodeDeleteView.as_view(), name="allotment_delete"),
+    path('allotment-code/<int:pk>/view/', views.AllotmentCodeDetailView.as_view(), name="allotment_detail"),
 
     # BUSINESS LINE #
     #################
@@ -20,6 +21,7 @@ urlpatterns = [
     path('business-line/new/', views.BusinessLineCreateView.as_view(), name="business_new"),
     path('business-line/<int:pk>/edit/', views.BusinessLineUpdateView.as_view(), name="business_edit"),
     path('business-line/<int:pk>/delete/', views.BusinessLineDeleteView.as_view(), name="business_delete"),
+    path('business-line/<int:pk>/view/', views.BusinessLineDetailView.as_view(), name="business_detail"),
 
     # LINE OBJECT #
     ###############
@@ -27,6 +29,7 @@ urlpatterns = [
     path('line-object/new/', views.LineObjectCreateView.as_view(), name="lo_new"),
     path('line-object/<int:pk>/edit/', views.LineObjectUpdateView.as_view(), name="lo_edit"),
     path('line-object/<int:pk>/delete/', views.LineObjectDeleteView.as_view(), name="lo_delete"),
+    path('line-object/<int:pk>/view/', views.LineObjectDetailView.as_view(), name="lo_detail"),
 
     # RC #
     ######
@@ -34,6 +37,15 @@ urlpatterns = [
     path('responsibility-centre/new/', views.ResponsibilityCentreCreateView.as_view(), name="rc_new"),
     path('responsibility-centre/<int:pk>/edit/', views.ResponsibilityCentreUpdateView.as_view(), name="rc_edit"),
     path('responsibility-centre/<int:pk>/delete/', views.ResponsibilityCentreDeleteView.as_view(), name="rc_delete"),
+    path('responsibility-centre/<int:pk>/view/', views.ResponsibilityCentreDetailView.as_view(), name="rc_detail"),
+
+    # PROJECTS #
+    ############
+    path('projects/', views.ProjectListView.as_view(), name="project_list"),
+    path('project/new/', views.ProjectCreateView.as_view(), name="project_new"),
+    path('project/<int:pk>/edit/', views.ProjectUpdateView.as_view(), name="project_edit"),
+    path('project/<int:pk>/delete/', views.ProjectDeleteView.as_view(), name="project_delete"),
+    path('project/<int:pk>/view/', views.ProjectDetailView.as_view(), name="project_detail"),
 
     # TRANSACTION #
     ###############
@@ -43,17 +55,4 @@ urlpatterns = [
     path('transaction/<int:pk>/edit/', views.TransactionUpdateView.as_view(), name="trans_edit"),
     path('transaction/<int:pk>/delete/', views.TransactionDeleteView.as_view(), name="trans_delete"),
 
-    # # # Entry #
-    # # #########
-    # path('entries/', views.EntryListView.as_view(), name="entry_list"),
-    # path('entry/new/', views.EntryCreateView.as_view(), name="entry_new"),
-    # path('entry/<int:pk>/view', views.EntryDetailView.as_view(), name="entry_detail"),
-    # path('entry/<int:pk>/edit', views.EntryUpdateView.as_view(), name="entry_edit"),
-    # path('entry/<int:pk>/delete', views.EntryDeleteView.as_view(), name="entry_delete"),
-    #
-    # # NOTES #
-    # #########
-    # path('entry/<int:entry>/note/new/', views.NoteCreateView.as_view(), name="note_new"),
-    # path('note/<int:pk>/edit/', views.NoteUpdateView.as_view(), name="note_edit"),
-    # path('note/<int:pk>/delete/', views.note_delete, name="note_delete"),
 ]
