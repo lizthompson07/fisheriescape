e53cf06cf85390933b8e7a57e9932e8dc90b3d6c
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-01-31 08:40:10 -0400

    sci models adjust

--- scifi/todo.py
+++ scifi/todo.py
@@ -1,5 +1,2 @@
-# TODO: transaction list should be a full table of all fields
-# TODO: we need to create RC manager views
 # TODO: we need to create groups to control access
-# TODO: develop reports
 # TODO: when you select a project code from a new transaction.. it should default to the correct coding
