03421cab53bc5d01991caeb6de4f257e24bf2f8e
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-05 10:38:33 -0400

    scifi report revamp

--- camp/todo.py
+++ camp/todo.py
@@ -5,3 +5,31 @@
 # TODO: export view for sample by species observations (1 spp per row)
 # TODO: export view for sample by species observations (contingency table)
 
+
+# TODO: species count works the first time
+# TODO: delete species richness by year report
+
+
+# TODO: pie chart
+## for 2018, title should be modified to have "in June" at the end
+## for legend, should have both eng and fre names... "eng / fre"
+
+# TODO: species richness
+## titre update
+## insert label on black line at each point indicating number of stations
+## change name of black line in legend
+
+# TODO: DO graph
+## updated title
+## insert label above each point indicating number of samples
+
+
+# TODO: GC graph
+## new title
+## insert label above each point indicating number of samples
+
+# TODO: Excel
+## sort by station number
+
+
+## timeline:
--- herring/todo.py
+++ herring/todo.py
@@ -1,1 +1,2 @@
 # TODO: maybe a new field should be created to parse out the P/A from E in otolith season. It would be a NullBooleanField called something like early_season
+
