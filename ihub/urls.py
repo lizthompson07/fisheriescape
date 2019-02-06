b4831a9d427e0f6b4977ff609dab8561a3cbc236
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-06 12:16:41 -0400

    ihub: build the first report

--- ihub/urls.py
+++ ihub/urls.py
@@ -21,34 +21,19 @@ urlpatterns = [
     path('note/<int:pk>/edit/', views.NoteUpdateView.as_view(), name="note_edit"),
     path('note/<int:pk>/delete/', views.note_delete, name="note_delete"),
 
-    # # Collaborator #
-    # ################
-    # path('project/<int:project>/collaborator/new/', views.CollaboratorCreateView.as_view(), name="collab_new"),
-    # path('collaborator/<int:pk>/edit/', views.CollaboratorUpdateView.as_view(), name="collab_edit"),
-    # path('collaborator/<int:pk>/delete/', views.collaborator_delete, name="collab_delete"),
+    # Reports #
+    ###########
+    path('reports/search/', views.ReportSearchFormView.as_view(), name="report_search"),
+    path('reports/capacity-report/fy/<str:fy>/orgs/<str:orgs>/', views.capacity_export_spreadsheet, name="capacity_xlsx"),
+    path('reports/capacity-report/', views.capacity_export_spreadsheet, name="capacity_xlsx"),
+    path('reports/capacity-report/fy/<str:fy>/', views.capacity_export_spreadsheet, name="capacity_xlsx"),
+    path('reports/capacity-report/orgs/<str:orgs>/', views.capacity_export_spreadsheet, name="capacity_xlsx"),
 
-    # # Collaborative Agreements #
-    # ############################
-    # path('project/<int:project>/agreement/new/', views.AgreementCreateView.as_view(), name="agreement_new"),
-    # path('agreement/<int:pk>/edit/', views.AgreementUpdateView.as_view(), name="agreement_edit"),
-    # path('agreement/<int:pk>/delete/', views.agreement_delete, name="agreement_delete"),
-
-    # # O&M COST #
-    # ############
-    # path('project/<int:project>/om-cost/new/', views.OMCostCreateView.as_view(), name="om_new"),
-    # path('om-cost/<int:pk>/edit/', views.OMCostUpdateView.as_view(), name="om_edit"),
-    # path('om-cost/<int:pk>/delete/', views.om_cost_delete, name="om_delete"),
-
-    # # CAPITAL COST #
-    # ################
-    # path('project/<int:project>/capital-cost/new/', views.CapitalCostCreateView.as_view(), name="capital_new"),
-    # path('capital-cost/<int:pk>/edit/', views.CapitalCostUpdateView.as_view(), name="capital_edit"),
-    # path('capital-cost/<int:pk>/delete/', views.capital_cost_delete, name="capital_delete"),
-
-    # # G&C COST #
-    # ############
-    # path('project/<int:project>/gc-cost/new/', views.GCCostCreateView.as_view(), name="gc_new"),
-    # path('gc-cost/<int:pk>/edit/', views.GCCostUpdateView.as_view(), name="gc_edit"),
-    # path('gc-cost/<int:pk>/delete/', views.gc_cost_delete, name="gc_delete"),
+    # path('reports/<str:species_list>/species-count/', views.report_species_count, name="species_report"),
+    # path('reports/species-richness/', views.report_species_richness, name="species_richness"),
+    # path('reports/species-richness/site/<int:site>/', views.report_species_richness, name="species_richness"),
+    # path('reports/annual-watershed-report/site/<int:site>/year/<int:year>',
+    #      views.AnnualWatershedReportTemplateView.as_view(), name="watershed_report"),
+    # path('reports/fgp-csv-export/', views.fgp_export, name="watershed_csv"),
 
 ]
--- ihub/views.py
+++ ihub/views.py
@@ -1,29 +1,34 @@
+import os
+
 from django.conf import settings
 from django.contrib import messages
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.mail import send_mail
+from django.utils import timezone
 from django.utils.translation import gettext as _
 from django_filters.views import FilterView
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, UpdateView, DeleteView, CreateView, DetailView, FormView, TemplateView
+from django.views.generic import  UpdateView, DeleteView, CreateView, DetailView, FormView, TemplateView
 ###
 from . import models
 from . import forms
 from . import filters
 from . import emails
+from . import reports
 
 
 # Create your views here.
 class CloserTemplateView(TemplateView):
     template_name = 'ihub/close_me.html'
 
+
 def not_in_ihub_group(user):
     if user:
         return user.groups.filter(name='ihub_access').count() != 0
 
+
 class iHubAccessRequiredMixin(LoginRequiredMixin, UserPassesTestMixin):
-    login_url = '/accounts/login_required/'
 
     def test_func(self):
         return not_in_ihub_group(self.request.user)
@@ -100,7 +105,8 @@ class EntryCreateView(iHubAccessRequiredMixin, CreateView):
                       recipient_list=email.to_list, fail_silently=False, )
         else:
             print('not sending email since in dev mode')
-        messages.success(self.request, "The entry has been submitted and an email has been sent to the Indigenous Hub Coordinator!")
+        messages.success(self.request,
+                         "The entry has been submitted and an email has been sent to the Indigenous Hub Coordinator!")
         return HttpResponseRedirect(self.get_success_url())
 
     def get_initial(self):
@@ -162,3 +168,97 @@ def note_delete(request, pk):
     messages.success(request, _("The note has been successfully deleted from the entry."))
     return HttpResponseRedirect(reverse_lazy("ihub:entry_detail", kwargs={"pk": object.entry.id}))
 
+
+# REPORTS #
+###########
+
+class ReportSearchFormView(iHubAccessRequiredMixin, FormView):
+    template_name = 'ihub/report_search.html'
+    form_class = forms.ReportSearchForm
+
+    # def get_initial(self):
+    # default the year to the year of the latest samples
+    # return {"fiscal_year": fiscal_year()}
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def form_valid(self, form):
+        orgs = str(form.cleaned_data["organizations"]).replace("[", "").replace("]", "").replace(" ", "").replace("'", "")
+        fy = str(form.cleaned_data["fiscal_year"])
+        report = int(form.cleaned_data["report"])
+
+        if report == 1:
+            if fy and orgs:
+                return HttpResponseRedirect(reverse("ihub:capacity_xlsx", kwargs={"fy": fy, "orgs": orgs}))
+            elif fy:
+                return HttpResponseRedirect(reverse("ihub:capacity_xlsx", kwargs={"fy": fy,}))
+            elif orgs:
+                return HttpResponseRedirect(reverse("ihub:capacity_xlsx", kwargs={"orgs": orgs,}))
+            else:
+                return HttpResponseRedirect(reverse("ihub:capacity_xlsx"))
+        else:
+            messages.error(self.request, "Report is not available. Please select another report.")
+            return HttpResponseRedirect(reverse("ihub:report_search"))
+
+
+def capacity_export_spreadsheet(request, fy=None, orgs=None):
+    file_url = reports.generate_capacity_spreadsheet(fy, orgs)
+
+    if os.path.exists(file_url):
+        with open(file_url, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename="iHub export {}.xlsx"'.format(timezone.now().strftime("%Y-%m-%d"))
+            return response
+    raise Http404
+
+
+#
+# def report_species_count(request, species_list):
+#     reports.generate_species_count_report(species_list)
+#     # find the name of the file
+#     base_dir = os.path.dirname(os.path.abspath(__file__))
+#     target_dir = os.path.join(base_dir, 'templates', 'camp', 'temp')
+#     for root, dirs, files in os.walk(target_dir):
+#         for file in files:
+#             if "report_temp" in file:
+#                 my_file = "camp/temp/{}".format(file)
+#
+#     return render(request, "camp/report_display.html", {"report_path": my_file})
+#
+#
+# def report_species_richness(request, site=None):
+#     if site:
+#         reports.generate_species_richness_report(site)
+#     else:
+#         reports.generate_species_richness_report()
+#
+#     return render(request, "camp/report_display.html")
+#
+#
+# class AnnualWatershedReportTemplateView(PDFTemplateView):
+#     template_name = 'camp/report_watershed_display.html'
+#
+#     def get_pdf_filename(self):
+#         site = models.Site.objects.get(pk=self.kwargs['site']).site
+#         return "{} Annual Report {}.pdf".format(self.kwargs['year'], site)
+#
+#     def get_context_data(self, **kwargs):
+#         reports.generate_annual_watershed_report(self.kwargs["site"], self.kwargs["year"])
+#         site = models.Site.objects.get(pk=self.kwargs['site']).site
+#         return super().get_context_data(
+#             pagesize="A4 landscape",
+#             title="Annual Report for {}_{}".format(site, self.kwargs['year']),
+#             **kwargs
+#         )
+#
+#
+
+#
+#
+# def fgp_export(request):
+#
+#     response = reports.generate_fgp_export()
+#     return response
+#
