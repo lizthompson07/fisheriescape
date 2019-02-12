9307dfbc8bb68f723fb2a066f20f0e754f278463
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-12 15:28:19 -0400

    camp: fixing pdf report

--- camp/todo.py
+++ camp/todo.py
@@ -5,30 +5,5 @@
 # TODO: export view for sample by species observations (1 spp per row)
 # TODO: export view for sample by species observations (contingency table)
 
-
-# TODO: pie chart
-## for 2018, title should be modified to have "in June" at the end
-## for legend, should have both eng and fre names... "eng / fre"
-
-# TODO: species richness
-## titre update
-## insert label on black line at each point indicating number of stations
-## change name of black line in legend
-
-# TODO: DO graph
-## updated title
-## insert label above each point indicating number of samples
-
-
-# TODO: GC graph
-## new title
-## insert label above each point indicating number of samples
-
-
-
-
-# TODO: Excel
-## sort by station number
-
-
-## timeline:
+# TODO: y axis scaling is wierd with 4+ spp
+# TODO: pugwash error
