f470c18693e890c9d75495cf04d99038297f6380
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-13 14:19:21 -0400

    ihub changes

--- ihub/todo.py
+++ ihub/todo.py
@@ -1,3 +1,21 @@
 # TODO: sort out the issue with groupings... do they belong with an entry or org? 
 # TODO: email to stacy when a new record is created
 # TODO: set up the capacity report.. this should be a mulitselect field for organization on the report search page
+
+
+# TODO: CRUD needed for sector
+# TODO: organization detail page needed
+# todo: file additions
+#
+#
+# -	Can we click on the name of the organization to bring up a separate page with information on the community? I would like to add info such as current chief, election date, contact people and information etc.
+# 
+# Report
+# -	For the current report can we sum up the amounts for expected and transferred
+# -	I would like to simplify the report a little can we remove the following columns (Entry Id, Leads, Date last modified, Date created, Last modified by and Created by
+# 
+# 
+# Question
+# 1.	Can we attach documents? If so can we attach them in the Actions, next steps or comments section?
+# 
+
