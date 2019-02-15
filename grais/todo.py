588419a02fb8f4081e29f9252e4c9bd35d690ee6
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-15 12:47:07 -0400

    grais i reports...

--- grais/todo.py
+++ grais/todo.py
@@ -5,3 +5,18 @@
 # TODO: report followups
 
 # TODO: delete biofouling field in models.species ... all checks done. safe
+
+
+
+# -	Add a box to enter about follow ups.
+# -	When you click on the list of AIS incidental reports (main page), the report dates don’t match when you actually click on the report. Not sure where those dates are coming from. The correct dates are once you’ve clicked on the report and it gives you the report date at the top.
+
+# -	There are several incidental reports that have nothing to do with a species, but more for inquiring about research
+# information or wanting to inform the DFO about something (sighting of buoys, etc.).
+# I talked it over with Renee and Erica and they would still like these reports to be entered in the app.
+# Currently I can’t enter them because the app requires me to select a species it is concerning.
+# Perhaps we can have a separate section to write these details, and have no species attached to the report.
+
+# -	Be able to search for species rather than the list, similar to how you enter species in the biofouling.
+# -	Have a location to add new species on the list, instead of having to go to the biofoulings (not a huge deal).
+
