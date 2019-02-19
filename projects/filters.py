8e5cfc8fe7b0acd7347e93a284a24c3456ec5269
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-19 16:23:16 -0400

    in order to create dev db, has to temporarily modify many forms and filters. This commit is reverting those changes

--- projects/filters.py
+++ projects/filters.py
@@ -10,15 +10,14 @@ import django_filters
 class ProjectFilter(django_filters.FilterSet):
     FY_CHOICES = [("{}-{}".format(y, y + 1), "{}-{}".format(y, y + 1)) for y in
                   range(timezone.now().year - 2, timezone.now().year + 1)]
-    # SECTION_CHOICES = [(None, "----"),]
-    # DIVISION_CHOICES = [(d.id, str(d)) for d in models.Division.objects.all()]
-    # SECTION_CHOICES = [(s.id, str(s)) for s in models.Section.objects.all()]
+    DIVISION_CHOICES = [(d.id, str(d)) for d in models.Division.objects.all()]
+    SECTION_CHOICES = [(s.id, str(s)) for s in models.Section.objects.all()]
     YES_NO_CHOICES = [(True, "Yes"), (False, "No"), ]
 
-    # project_title = django_filters.CharFilter(field_name='project_title', lookup_expr='icontains')
-    # fiscal_year = django_filters.ChoiceFilter(field_name='fiscal_year', lookup_expr='exact', choices=FY_CHOICES)
-    # division = django_filters.ChoiceFilter(field_name='section__division', lookup_expr='exact', label="Division", choices=DIVISION_CHOICES)
-    # section = django_filters.ChoiceFilter(field_name='section', lookup_expr='exact', label="Section", choices=SECTION_CHOICES)
+    project_title = django_filters.CharFilter(field_name='project_title', lookup_expr='icontains')
+    fiscal_year = django_filters.ChoiceFilter(field_name='fiscal_year', lookup_expr='exact', choices=FY_CHOICES)
+    division = django_filters.ChoiceFilter(field_name='section__division', lookup_expr='exact', label="Division", choices=DIVISION_CHOICES)
+    section = django_filters.ChoiceFilter(field_name='section', lookup_expr='exact', label="Section", choices=SECTION_CHOICES)
     submitted = django_filters.ChoiceFilter(field_name='submitted', lookup_expr='exact', label="Submitted?", choices=YES_NO_CHOICES)
 
 
