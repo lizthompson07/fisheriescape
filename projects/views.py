f6e2905143db73f825fa5a6cc59d94baa2332a61
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-19 10:12:15 -0400

    projects app: project report has been updated

--- projects/views.py
+++ projects/views.py
@@ -23,11 +23,11 @@ from . import reports
 def can_delete(user, project):
     """returns True if user is a custodian in the specified resource"""
     if user.id:
-        # check to see if a superuser
-        if user.is_superuser:
-            return True
+        # # check to see if a superuser
+        # if user.is_superuser:
+        #     return True
         # check to see if they are a section head
-        elif project.section.section_head.id == user.id:
+        if project.section.section_head.id == user.id:
             return True
         # otherwise check to see if they are a project lead
         else:
@@ -242,39 +242,98 @@ class ProjectPrintDetailView(LoginRequiredMixin, PDFTemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         project = models.Project.objects.get(pk=self.kwargs["pk"])
+        context["report_mode"] = True
         context["object"] = project
         context["field_list"] = project_field_list
 
-        salary_total = 0
-        om_total = 0
+        salary_abase = 0
+        om_abase = 0
+        capital_abase = 0
+
+        salary_bbase = 0
+        om_bbase = 0
+        capital_bbase = 0
+
+        salary_cbase = 0
+        om_cbase = 0
+        capital_cbase = 0
+
         gc_total = 0
-        capital_total = 0
 
         # first calc for staff
         for staff in project.staff_members.all():
             # exclude full time employees
             if staff.employee_type.id != 1 or staff.employee_type.id != 6:
-                if staff.employee_type.cost_type is 1:
-                    salary_total += nz(staff.cost, 0)
-                elif staff.employee_type.cost_type is 2:
-                    om_total += nz(staff.cost, 0)
+                # if the staff member is being paid from bbase...
+                if staff.funding_source.id == 1:
+                    # if salary
+                    if staff.employee_type.cost_type is 1:
+                        salary_abase += nz(staff.cost, 0)
+                    # if o&M
+                    elif staff.employee_type.cost_type is 2:
+                        om_abase += nz(staff.cost, 0)
+                elif staff.funding_source.id == 2:
+                    # if salary
+                    if staff.employee_type.cost_type is 1:
+                        salary_bbase += nz(staff.cost, 0)
+                    # if o&M
+                    elif staff.employee_type.cost_type is 2:
+                        om_bbase += nz(staff.cost, 0)
+                elif staff.funding_source.id == 3:
+                    # if salary
+                    if staff.employee_type.cost_type is 1:
+                        salary_cbase += nz(staff.cost, 0)
+                    # if o&M
+                    elif staff.employee_type.cost_type is 2:
+                        om_cbase += nz(staff.cost, 0)
 
         # O&M costs
         for cost in project.om_costs.all():
-            om_total += nz(cost.budget_requested, 0)
+            if cost.funding_source.id == 1:
+                om_abase += nz(cost.budget_requested, 0)
+            elif cost.funding_source.id == 2:
+                om_bbase += nz(cost.budget_requested, 0)
+            elif cost.funding_source.id == 3:
+                om_cbase += nz(cost.budget_requested, 0)
 
         # Capital costs
         for cost in project.capital_costs.all():
-            capital_total += nz(cost.budget_requested, 0)
+            if cost.funding_source.id == 1:
+                capital_abase += nz(cost.budget_requested, 0)
+            elif cost.funding_source.id == 2:
+                capital_bbase += nz(cost.budget_requested, 0)
+            elif cost.funding_source.id == 3:
+                capital_cbase += nz(cost.budget_requested, 0)
 
         # g&c costs
         for cost in project.gc_costs.all():
-            gc_total += nz(cost.budget_requested, 0)
+                gc_total += nz(cost.budget_requested, 0)
 
-        context["salary_total"] = salary_total
-        context["om_total"] = om_total
+        # abase
+        context["salary_abase"] = salary_abase
+        context["om_abase"] = om_abase
+        context["capital_abase"] = capital_abase
+
+        # bbase
+        context["salary_bbase"] = salary_bbase
+        context["om_bbase"] = om_bbase
+        context["capital_bbase"] = capital_bbase
+
+        # cbase
+        context["salary_cbase"] = salary_cbase
+        context["om_cbase"] = om_cbase
+        context["capital_cbase"] = capital_cbase
+
+        context["salary_total"] = salary_abase + salary_bbase + salary_cbase
+        context["om_total"] = om_abase + om_bbase + om_cbase
+        context["capital_total"] = capital_abase + capital_bbase + capital_cbase
         context["gc_total"] = gc_total
-        context["capital_total"] = capital_total
+
+        # import color schemes from funding_source table
+        context["abase"] = models.FundingSource.objects.get(pk=1).color
+        context["bbase"] = models.FundingSource.objects.get(pk=2).color
+        context["cbase"] = models.FundingSource.objects.get(pk=3).color
+
         return context
 
 
