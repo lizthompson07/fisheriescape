beab10cb2f9245b5e2b701d1d46765d51aba03b7
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-19 13:33:02 -0400

    added todo item in projects

--- projects/todo.py
+++ projects/todo.py
@@ -1,12 +1,4 @@
-# TODO: remove ablity to add students and post-doc from staff..? Or we can just leave it.
-# TODO: not sure what to do with overtime hours
-# TODO: index page... with instructions
-# TODO: add new users...
 # TODO: DO We want to add management feedback fields? Questions/comments from other users?
 # TODO: reviewed field?
-# TODO: create visio diagram
-# TODO: full time employees should be on the list of users... so it should grey out the field
-# TODO: function to clean up O&M requests
-
 # TODO: there should always be at least one project lead
 # TODO: if possible, uses should get a warning before removing themselves as lead... they will be locked out of project...
