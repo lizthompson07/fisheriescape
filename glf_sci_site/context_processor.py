deeb72fd56100d0b7a4aad4f658b0cbf4c63496e
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-19 21:31:17 -0400

    fixed untracked files

--- glf_sci_site/context_processor.py
+++ glf_sci_site/context_processor.py
@@ -2,4 +2,7 @@ from django.conf import settings
 
 def my_envr(request):
     # return the value you want as a dictionary. you may add multiple values in there.
-    return {'MY_ENVR': settings.MY_ENVR}
+    return {
+        'MY_ENVR': settings.MY_ENVR,
+        'PROD_CNF': settings.PROD_CNF,
+    }
