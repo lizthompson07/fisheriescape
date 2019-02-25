939ae72ca64af598eabd872bfa2448d7ddaf86f9
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-25 16:24:37 -0400

    projects build

--- projects/todo.py
+++ projects/todo.py
@@ -1,4 +1,4 @@
 # TODO: DO We want to add management feedback fields? Questions/comments from other users?
 # TODO: reviewed field?
 # TODO: there should always be at least one project lead
-# TODO: if possible, uses should get a warning before removing themselves as lead... they will be locked out of project...
+# TODO: should be a delete button in the staff form.. same for costs, collaborators, and agreements
