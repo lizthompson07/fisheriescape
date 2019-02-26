13f05814e628aae87d6370e0727c16b67444a27f
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-26 13:31:33 -0400

    diets: buidling

--- diets/todo.py
+++ diets/todo.py
@@ -4,13 +4,13 @@
 # TODO: bring in the MANAGE views for at least digestion level
 
 
+
 # TODO: smart lookup for pred sp
-# TODO: predator form labels
 # TODO: sampler dropdown... like in herring
-# TODO: pred length in mm should be in cm
 # TODO: content weight (g) missing
-# TODO: dig levels missing code in form
+
 # TODO: "number of prey" missing on prey obs
 # TODO: "hide stomach weight and sensor used"
-# TODO: cruise is missing from predator search
+
 # TODO: FREEZING WHEN SEARCHING PAND
+# TODO: integrate prey adding to predator homepage
