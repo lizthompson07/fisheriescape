b68722c86c0602f8a08fde4671e8f65ab8eb9913
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-02-26 15:56:01 -0400

    diets: completed all of the todo's requested by sylvie

--- diets/todo.py
+++ diets/todo.py
@@ -1,16 +1,5 @@
 # these should all be bilingual exports
 
 # TODO: QC elements... for example, the weight of a prey item should never = fraction x of predator... etc
-# TODO: bring in the MANAGE views for at least digestion level
 
 
-
-# TODO: smart lookup for pred sp
-# TODO: sampler dropdown... like in herring
-# TODO: content weight (g) missing
-
-# TODO: "number of prey" missing on prey obs
-# TODO: "hide stomach weight and sensor used"
-
-# TODO: FREEZING WHEN SEARCHING PAND
-# TODO: integrate prey adding to predator homepage
