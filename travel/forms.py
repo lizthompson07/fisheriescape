f53e003237ac6e8fc661c5060e261a85375da909
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-03-13 08:39:30 -0300

    travel app build

--- travel/forms.py
+++ travel/forms.py
@@ -3,13 +3,13 @@ from django.utils.translation import gettext as _
 from django.contrib.auth.models import User as AuthUser
 from projects import models as projects_models
 
-
 from . import models
 
 YES_NO_CHOICES = (
     (True, _("Yes")),
     (False, _("No")),
 )
+
 
 class EventForm(forms.ModelForm):
     class Meta:
@@ -35,13 +35,18 @@ class ReportSearchForm(forms.Form):
     REPORT_CHOICES = (
         (None, "------"),
         (1, "CFTS export (xlsx)"),
+        (2, "Print Travel Plan PDF"),
     )
     report = forms.ChoiceField(required=True, choices=REPORT_CHOICES)
     # report #1
     fiscal_year = forms.ChoiceField(required=False, label=_('Fiscal year'))
+    # report #2
+    traveller = forms.ChoiceField(required=False, label=_('Fiscal year'))
 
     def __init__(self, *args, **kwargs):
         FY_CHOICES = [(fy.id, str(fy)) for fy in projects_models.FiscalYear.objects.all().order_by("id")]
+        TRAVELLER_CHOICES = [(e['email'], "{}, {}".format(e['last_name'], e['first_name'])) for e in
+                             models.Event.objects.values("email", "first_name", "last_name").order_by("last_name", "first_name").distinct()]
         super().__init__(*args, **kwargs)
         self.fields['fiscal_year'].choices = FY_CHOICES
-
+        self.fields['traveller'].choices = TRAVELLER_CHOICES
