9381b9514798033278ff319259c725f4e5ba2a0d
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-03-20 15:05:37 -0300

    batch of upgrades to dm_tickets

--- shared_models/models.py
+++ shared_models/models.py
@@ -62,6 +62,25 @@ class Region(models.Model):
         ordering = ['name', ]
 
 
+class Branch(models.Model):
+    name = models.CharField(max_length=255, verbose_name=_("name (English)"))
+    nom = models.CharField(max_length=255, blank=True, null=True, verbose_name=_("Name (French)"))
+    abbrev = models.CharField(max_length=10, verbose_name=_("abbreviation"))
+    region = models.ForeignKey(Region, on_delete=models.DO_NOTHING, verbose_name=_("region"))
+
+    def __str__(self):
+        # check to see if a french value is given
+        if getattr(self, str(_("name"))):
+            return "{}".format(getattr(self, str(_("name"))))
+        # if there is no translated term, just pull from the english field
+        else:
+            return "{}".format(self.name)
+
+    class Meta:
+        ordering = ['name', ]
+
+
+
 # projects
 # dm_tickets
 # inventory
@@ -70,7 +89,7 @@ class Division(models.Model):
     name = models.CharField(max_length=255, verbose_name=_("name (English)"))
     nom = models.CharField(max_length=255, blank=True, null=True, verbose_name=_("name (French)"))
     abbrev = models.CharField(max_length=10, blank=True, null=True, verbose_name=_("abbreviation"))
-    region = models.ForeignKey(Region, on_delete=models.DO_NOTHING, verbose_name=_("region"))
+    branch = models.ForeignKey(Branch, on_delete=models.DO_NOTHING, verbose_name=_("branch"))
 
     def __str__(self):
         # check to see if a french value is given
