0b22f59e14924f2fe955e6cdbc6a11f71ed08aa6
onedwd <onedwd@gmail.com>
2019-03-27 17:44:55 -0700

    added deployment model

--- polls/forms.py
+++ polls/forms.py
@@ -6,17 +6,33 @@ from shared_models import models as shared_models
 
 
 class NewInstrumentForm(forms.ModelForm):
+
     class Meta:
+        # TYPE_CHOICES = [('CTD', 'CTD'), ('ADCP', 'ADCP')]
         model = models.Instrument
         fields = [
+            'instrument_type',
+            'serial_number',
             'purchase_date',
+            'date_of_last_service',
+            'date_of_next_service',
             'project_title',
             # 'section',
             # 'last_modified_by',
         ]
         widgets = {
             # 'last_modified_by': forms.HiddenInput(),
-            'project_title': forms.Textarea(attrs={"rows": 5}),
+            # 'purchase_date': forms.DateInput( attrs={"type": "date",'placeholder':'2015-01-01'}),
+            # 'instrument_type': forms.Textarea(attrs={"rows": 1},  choices=TYPE_CHOICES),
+            # 'instrument_type': forms.Select(choices=TYPE_CHOICES),
+            # 'serial_number': forms.Textarea(attrs={"rows": 1}),
+            # 'date_of_last_service': forms.DateInput( attrs={"type": "date",'placeholder':'2015-01-01'}),
+            # 'date_of_next_service': forms.DateInput(attrs={"type": "date", 'placeholder': '2015-01-01'}),
+            # 'end_date': forms.DateInput(attrs={"type": "date"}),
+            # 'last_modified_by': forms.HiddenInput(),
+            'project_title': forms.Textarea(attrs={"rows": 1}),
+            # "description": forms.Textarea(attrs={"rows": 8}),
+            # "notes": forms.Textarea(attrs={"rows": 5}),
         }
 
 
@@ -44,11 +60,46 @@ class InstrumentForm(forms.ModelForm):
             # 'priorities_html',
             # 'deliverables_html',
         ]
+
         widgets = {
-            'purchase_date': forms.DateInput(attrs={"type": "date"}),
-            # 'end_date': forms.DateInput(attrs={"type": "date"}),
-            # 'last_modified_by': forms.HiddenInput(),
-            'project_title': forms.Textarea(attrs={"rows": 5}),
-            # "description": forms.Textarea(attrs={"rows": 8}),
-            # "notes": forms.Textarea(attrs={"rows": 5}),
+        #     # 'last_modified_by': forms.HiddenInput(),
+        #     'purchase_date': forms.DateInput( attrs={"type": "date",'placeholder':'2015-01-01'}),
+        #     # 'instrument_type': forms.Textarea(attrs={"rows": 1},  choices=TYPE_CHOICES),
+        #     # 'instrument_type': forms.Select(choices=TYPE_CHOICES),
+        #     'serial_number': forms.Textarea(attrs={"rows": 1}),
+        #     'date_of_last_service': forms.DateInput( attrs={"type": "date",'placeholder':'2015-01-01'}),
+        #     'date_of_next_service': forms.DateInput(attrs={"type": "date", 'placeholder': '2015-01-01'}),
+        #     # 'end_date': forms.DateInput(attrs={"type": "date"}),
+        #     # 'last_modified_by': forms.HiddenInput(),
+            'project_title': forms.Textarea(attrs={"rows": 1}),
+        #     # "description": forms.Textarea(attrs={"rows": 8}),
+        #     # "notes": forms.Textarea(attrs={"rows": 5}),
+        }
+
+
+class DeploymentForm(forms.ModelForm):
+    class Meta:
+        model = models.Deployment
+        exclude = [
+            'submitted',
+            'date_last_modified',
+            # 'section_head_approved',
+            # 'description_html',
+            # 'priorities_html',
+            # 'deliverables_html',
+        ]
+
+        widgets = {
+        #     # 'last_modified_by': forms.HiddenInput(),
+        #     'purchase_date': forms.DateInput( attrs={"type": "date",'placeholder':'2015-01-01'}),
+        #     # 'instrument_type': forms.Textarea(attrs={"rows": 1},  choices=TYPE_CHOICES),
+        #     # 'instrument_type': forms.Select(choices=TYPE_CHOICES),
+        #     'serial_number': forms.Textarea(attrs={"rows": 1}),
+        #     'date_of_last_service': forms.DateInput( attrs={"type": "date",'placeholder':'2015-01-01'}),
+        #     'date_of_next_service': forms.DateInput(attrs={"type": "date", 'placeholder': '2015-01-01'}),
+        #     # 'end_date': forms.DateInput(attrs={"type": "date"}),
+        #     # 'last_modified_by': forms.HiddenInput(),
+            'description': forms.Textarea(attrs={"rows": 1}),
+        #     # "description": forms.Textarea(attrs={"rows": 8}),
+        #     # "notes": forms.Textarea(attrs={"rows": 5}),
         }
