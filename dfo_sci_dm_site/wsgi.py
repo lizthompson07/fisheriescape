160d74b25154e5fe18b04b1b2df9bc0a118c175c
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-04-01 13:50:26 -0300

    changed main project dir name and all references to it. this addresses issue #18

--- dfo_sci_dm_site/wsgi.py
+++ dfo_sci_dm_site/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for glf_sci_site project.
+WSGI config for dfo_sci_dm_site project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "glf_sci_site.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dfo_sci_dm_site.settings")
 
 application = get_wsgi_application()
