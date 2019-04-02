fa20e66580accc629f47ba91f5377844fe8a82dd
onedwd <onedwd@gmail.com>
2019-04-02 09:42:34 -0700

    added deployment view and forms

---
+++ ios2/filters.py
@@ -0,0 +1,37 @@
+from django.contrib.auth.models import User
+from shared_models import models as shared_models
+
+from . import models
+import django_filters
+
+
+class InstrumentFilter(django_filters.FilterSet):
+    # FY_CHOICES = [(fy.id, str(fy)) for fy in shared_models.FiscalYear.objects.all()]
+    # DIVISION_CHOICES = [(d.id, str(d)) for d in shared_models.Division.objects.filter(branch=1)]
+    # SECTION_CHOICES = [(s.id, str(s)) for s in shared_models.Section.objects.filter(division__branch=1)]
+    TYPE_CHOICES = [('CTD', 'CTD'), ('ADCP', 'ADCP')]
+    YES_NO_CHOICES = [(True, "Yes"), (False, "No"), ]
+
+    project_title = django_filters.CharFilter(field_name='project_title', lookup_expr='icontains')
+    instrument_type = django_filters.ChoiceFilter(field_name='instrument_type', lookup_expr='exact', choices=TYPE_CHOICES)
+    # fiscal_year = django_filters.ChoiceFilter(field_name='year', lookup_expr='exact', choices=FY_CHOICES)
+    # division = django_filters.ChoiceFilter(field_name='section__division', lookup_expr='exact', label="Division", choices=DIVISION_CHOICES)
+    # section = django_filters.ChoiceFilter(field_name='section', lookup_expr='exact', label="Section", choices=SECTION_CHOICES)
+    submitted = django_filters.ChoiceFilter(field_name='submitted', lookup_expr='exact', label="Submitted?", choices=YES_NO_CHOICES)
+
+
+class DeploymentFilter(django_filters.FilterSet):
+    # FY_CHOICES = [(fy.id, str(fy)) for fy in shared_models.FiscalYear.objects.all()]
+    # DIVISION_CHOICES = [(d.id, str(d)) for d in shared_models.Division.objects.filter(branch=1)]
+    # SECTION_CHOICES = [(s.id, str(s)) for s in shared_models.Section.objects.filter(division__branch=1)]
+    TYPE_CHOICES = [('CTD', 'CTD'), ('ADCP', 'ADCP')]
+    YES_NO_CHOICES = [(True, "Yes"), (False, "No"), ]
+
+    mooring = django_filters.CharFilter(field_name='mooring', lookup_expr='icontains')
+    mooring_number = django_filters.CharFilter(field_name='mooring_number', lookup_expr='exact')
+    # mooring = django_filters.CharFilter(field_name='mooring', lookup_expr='icontains')
+    # mooring_number = django_filters.ModelChoiceFilter(queryset=Deployment.objects.all)#, lookup_expr='exact')
+    # fiscal_year = django_filters.ChoiceFilter(field_name='year', lookup_expr='exact', choices=FY_CHOICES)
+    # division = django_filters.ChoiceFilter(field_name='section__division', lookup_expr='exact', label="Division", choices=DIVISION_CHOICES)
+    # section = django_filters.ChoiceFilter(field_name='section', lookup_expr='exact', label="Section", choices=SECTION_CHOICES)
+    submitted = django_filters.ChoiceFilter(field_name='submitted', lookup_expr='exact', label="Submitted?", choices=YES_NO_CHOICES)
---
+++ ios2/forms.py
@@ -0,0 +1,154 @@
+from django import forms
+from django.utils.translation import gettext as _
+from . import models
+from django.contrib.auth.models import User
+from shared_models import models as shared_models
+from django.utils import timezone
+from datetime import date, timedelta, datetime
+
+class NewInstrumentForm(forms.ModelForm):
+    purchase_date = forms.DateField(input_formats=["%Y-%b-%d"],
+                                    widget=forms.DateInput(format="%Y-%b-%d", attrs={"type": "datepicker",
+                                                   "value": timezone.now().strftime("%Y-%b-%d")}))
+    date_of_last_service = forms.DateField(input_formats=["%Y-%b-%d"],
+                                    widget=forms.DateInput(format="%Y-%b-%d", attrs={"type": "datepicker",
+                                                   "value": timezone.now().strftime("%Y-%b-%d")}))
+    date_of_next_service = forms.DateField(input_formats=["%Y-%b-%d"],
+                                    widget=forms.DateInput(format="%Y-%b-%d", attrs={"type": "datepicker",
+                                                   "value": (timezone.now()+ timedelta(days=365)).strftime("%Y-%b-%d")}))
+
+    class Meta:
+        # TYPE_CHOICES = [('CTD', 'CTD'), ('ADCP', 'ADCP')]
+        model = models.Instrument
+
+        fields = [
+            'instrument_type',
+            'serial_number',
+            'purchase_date',
+            'date_of_last_service',
+            'date_of_next_service',
+            'project_title',
+            # 'last_modified_by',
+        ]
+        widgets = {
+            # 'last_modified_by': forms.HiddenInput(),
+            # 'purchase_date' : forms.DateInput(attrs={"type": "datepicker",
+            #                                        "value": timezone.now().strftime("%Y-%b-%d")},
+            #                                 ),
+            # 'instrument_type': forms.Textarea(attrs={"rows": 1},  choices=TYPE_CHOICES),
+            # 'instrument_type': forms.Select(choices=TYPE_CHOICES),
+            # 'serial_number': forms.Textarea(attrs={"rows": 1}),
+            # 'date_of_last_service': forms.DateInput(attrs={"type": "date",
+            #                                                "value": timezone.now().strftime("%Y-%b-%d")}),
+            # 'date_of_next_service': forms.DateInput(attrs={"type": "date",
+            #                                                "value": (timezone.now() + timedelta(days=365)).strftime("%Y-%b-%d")}),
+            # 'end_date': forms.DateInput(attrs={"type": "date"}),
+            # 'last_modified_by': forms.HiddenInput(),
+            'project_title': forms.Textarea(attrs={"rows": 1}),
+            'scientist': forms.Textarea(attrs={"rows": 1}),
+            # "description": forms.Textarea(attrs={"rows": 8}),
+            # "notes": forms.Textarea(attrs={"rows": 5}),
+        }
+
+
+class InstrumentSubmitForm(forms.ModelForm):
+    class Meta:
+        model = models.Instrument
+        fields = [
+            # 'last_modified_by',
+            'submitted',
+        ]
+        widgets = {
+            # 'last_modified_by': forms.HiddenInput(),
+            'submitted': forms.HiddenInput(),
+        }
+
+
+class InstrumentForm(forms.ModelForm):
+    purchase_date = forms.DateField(input_formats=["%Y-%b-%d"])
+    class Meta:
+        model = models.Instrument
+        exclude = [
+            'submitted',
+            'date_last_modified',
+            # 'section_head_approved',
+            # 'description_html',
+            # 'priorities_html',
+            # 'deliverables_html',
+        ]
+
+        widgets = {
+        #     # 'last_modified_by': forms.HiddenInput(),
+        #     'purchase_date': forms.DateInput( attrs={"type": "date",'placeholder':'2015-01-01'}),
+        #     # 'instrument_type': forms.Textarea(attrs={"rows": 1},  choices=TYPE_CHOICES),
+        #     # 'instrument_type': forms.Select(choices=TYPE_CHOICES),
+        #     'serial_number': forms.Textarea(attrs={"rows": 1}),
+        #     'date_of_last_service': forms.DateInput( attrs={"type": "date",
+        #                                                     'value': timezone.now().strftime("%Y-%b-%d")}),
+        #     'date_of_next_service': forms.DateInput(attrs={"type": "date",
+        #                                                    'value': (timezone.now()+ timedelta(days=365)).strftime("%Y-%b-%d")}),
+        #     # 'end_date': forms.DateInput(attrs={"type": "date"}),
+        #     # 'last_modified_by': forms.HiddenInput(),
+            'scientist': forms.Textarea(attrs={"rows": 1}),
+            'project_title': forms.Textarea(attrs={"rows": 1}),
+        #     # "description": forms.Textarea(attrs={"rows": 8}),
+        #     # "notes": forms.Textarea(attrs={"rows": 5}),
+        }
+
+
+class DeploymentForm(forms.ModelForm):
+    class Meta:
+        model = models.Deployment
+        exclude = [
+            'submitted',
+            'date_last_modified',
+
+        ]
+
+        widgets = {
+            # 'instrument': forms.HiddenInput(),
+            # 'instruments': forms.HiddenInput(),
+            'mooring': forms.Textarea(attrs={"rows": 1}),
+            'mooring_number': forms.Textarea(attrs={"rows": 1}),
+            'deploy_time': forms.DateTimeInput(attrs={"type": "date",
+                                                'input_formats': ['%Y-%m-%d %H:%M:%S'],
+                                                           'placeholder': (timezone.now()+ timedelta(days=365)).strftime("%Y-%b-%d")}),
+            'recover_time': forms.DateTimeInput(attrs={"type": "date",
+                                                           'value': (timezone.now() + timedelta(days=365)).strftime(
+                                                               "%Y-%b-%d %T")}),
+            'depth': forms.Textarea(attrs={"rows": 1}),
+            'lat': forms.Textarea(attrs={"rows": 1}),
+            'lon': forms.Textarea(attrs={"rows": 1}),
+            'comments': forms.Textarea(attrs={"rows": 2}),
+        #     # "description": forms.Textarea(attrs={"rows": 8}),
+        #     # "notes": forms.Textarea(attrs={"rows": 5}),
+        }
+
+
+class InstrumentDeploymentForm(forms.ModelForm):
+    class Meta:
+        model = models.InstrumentDeployment
+        exclude = [
+            'submitted',
+            'date_last_modified',
+
+        ]
+
+        widgets = {
+            # 'instrument': forms.HiddenInput(),
+            # 'instruments': forms.HiddenInput(),
+            'mooring': forms.Textarea(attrs={"rows": 1}),
+            'mooring_number': forms.Textarea(attrs={"rows": 1}),
+            'deploy_time': forms.DateTimeInput(attrs={"type": "date",
+                                                'input_formats': ['%Y-%m-%d %H:%M:%S'],
+                                                           'placeholder': (timezone.now()+ timedelta(days=365)).strftime("%Y-%b-%d")}),
+            'recover_time': forms.DateTimeInput(attrs={"type": "date",
+                                                           'value': (timezone.now() + timedelta(days=365)).strftime(
+                                                               "%Y-%b-%d %T")}),
+            'depth': forms.Textarea(attrs={"rows": 1}),
+            'lat': forms.Textarea(attrs={"rows": 1}),
+            'lon': forms.Textarea(attrs={"rows": 1}),
+            'comments': forms.Textarea(attrs={"rows": 2}),
+        #     # "description": forms.Textarea(attrs={"rows": 8}),
+        #     # "notes": forms.Textarea(attrs={"rows": 5}),
+        }
