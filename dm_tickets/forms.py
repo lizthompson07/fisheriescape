5af8ec88d132767236db7748a9a1cecc02475ec7
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-04-08 13:55:33 -0300

    disconnecting the bugs app and plugging in the dm_tickets app in place

--- dm_tickets/forms.py
+++ dm_tickets/forms.py
@@ -4,6 +4,7 @@ from django.utils.translation import gettext as _
 from . import models
 from shared_models import models as shared_models
 
+
 class TicketForm(forms.ModelForm):
     generic_file_to_load = forms.CharField(required=False, widget=forms.HiddenInput())
 
@@ -26,7 +27,7 @@ class TicketForm(forms.ModelForm):
                         User.objects.all().order_by("last_name", "first_name")]
         USER_CHOICES.insert(0, tuple((None, "---")))
         STAFF_USER_CHOICES = [(u.id, "{}, {}".format(u.last_name, u.first_name)) for u in
-                        User.objects.filter(is_staff=True).order_by("last_name", "first_name")]
+                              User.objects.filter(is_staff=True).order_by("last_name", "first_name")]
         STAFF_USER_CHOICES.insert(0, tuple((None, "---")))
         SECTION_CHOICES = [(s.id, s.full_name) for s in
                            shared_models.Section.objects.all().order_by("division__branch__region", "division__branch", "division", "name")]
@@ -37,6 +38,43 @@ class TicketForm(forms.ModelForm):
         self.fields['sd_primary_contact'].choices = USER_CHOICES
         self.fields['assigned_to'].choices = STAFF_USER_CHOICES
         self.fields['section'].choices = SECTION_CHOICES
+
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = models.Ticket
+        exclude = [
+            'notes_html',
+            "resolved_email_date",
+            'notes',
+            "date_opened",
+            "date_modified",
+            "date_closed",
+            "fiscal_year",
+            "people_notes",
+            'sd_ref_number',
+            'sd_ticket_url',
+            'sd_primary_contact',
+            'sd_description',
+            'sd_date_logged',
+            'financial_follow_up_needed',
+            'financial_coding',
+            'estimated_cost',
+            'assigned_to',
+            'section',
+        ]
+        widgets = {
+            'date_closed': forms.DateInput(attrs={'type': 'date'}),
+            'sd_date_logged': forms.DateInput(attrs={'type': 'date'}),
+            'description': forms.Textarea(attrs={'rows': '5'}),
+            'primary_contact': forms.HiddenInput(),
+            'status': forms.HiddenInput(),
+        }
+        labels = {
+            'app': _("Application name (if applicable)"),
+            'description': _("Feedback details"),
+            'title': _("Subject"),
+        }
 
 
 class TicketNoteForm(forms.ModelForm):
