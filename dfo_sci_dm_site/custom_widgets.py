15b9b78fa8e6f7a28b1ee2e12c6c3921e0238078
upsonp <upsonp@gmail.com>
2019-04-11 13:00:15 -0300

    Removed debug statement
    Signed-off-by: upsonp <upsonp@gmail.com>

--- dfo_sci_dm_site/custom_widgets.py
+++ dfo_sci_dm_site/custom_widgets.py
@@ -4,7 +4,6 @@ from django.db import models
 class OracleTextField(models.TextField):
     def db_type(self, connection):
         if connection.settings_dict['ENGINE'] == 'django.db.backends.oracle':
-            print("You are here 2")
             return 'NVARCHAR2(2000)'
         else:
             return super.db_type(self, connection)
