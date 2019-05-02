dc13905b325256d13a3c65082a640e37d2f68c46
david fishman <davjfish@gmail.com>
2019-05-02 10:23:13 -0300

    related to issue #91

--- spot/urls.py
+++ spot/urls.py
@@ -6,17 +6,7 @@ app_name = 'spot'
 urlpatterns = [
     path('close/', views.CloserTemplateView.as_view(), name="close_me"),
     path('', views.IndexTemplateView.as_view(), name="index"),
-    #
-    # # PERSON #
-    # ##########
-    # path('people/', views.PersonListView.as_view(), name="person_list"),
-    # path('person/new/', views.PersonCreateView.as_view(), name="person_new"),
-    # path('person/new/popout/', views.PersonCreateViewPopout.as_view(), name="person_new_pop"),
-    # path('person/<int:pk>/view/', views.PersonDetailView.as_view(), name="person_detail"),
-    # path('person/<int:pk>/edit/', views.PersonUpdateView.as_view(), name="person_edit"),
-    # path('person/<int:pk>/edit/popout/', views.PersonUpdateViewPopout.as_view(), name="person_edit_pop"),
-    # path('person/<int:pk>/delete/', views.PersonDeleteView.as_view(), name="person_delete"),
-    #
+
     # ORGANIZATION #
     ################
     path('organizations/', views.OrganizationListView.as_view(), name="org_list"),
@@ -29,7 +19,17 @@ urlpatterns = [
     ######################
     path('organization/<int:org>/member/new/', views.MemberCreateView.as_view(), name="member_new"),
     path('member/<int:pk>/edit/', views.MemberUpdateView.as_view(), name="member_edit"),
-    path('member/<int:pk>/delete/', views.member_delete, name="member_delete"),
+    path('member/<int:pk>/delete/', views.MemberDeleteView.as_view(), name="member_delete"),
+
+    # PERSON #
+    ##########
+    path('people/', views.PersonListView.as_view(), name="person_list"),
+    path('person/new/', views.PersonCreateView.as_view(), name="person_new"),
+    path('person/new/popout/', views.PersonCreateViewPopout.as_view(), name="person_new_pop"),
+    path('person/<int:pk>/view/', views.PersonDetailView.as_view(), name="person_detail"),
+    path('person/<int:pk>/edit/', views.PersonUpdateView.as_view(), name="person_edit"),
+    path('person/<int:pk>/edit/popout/', views.PersonUpdateViewPopout.as_view(), name="person_edit_pop"),
+    path('person/<int:pk>/delete/', views.PersonDeleteView.as_view(), name="person_delete"),
 
     # # RECIPIENTS #
     # ##############
