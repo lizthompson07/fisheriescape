7889abcc84ba0240e0ac1eea68587954a2273b77
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-05-08 14:16:58 -0300

    added select-chosen class to fields in dm_tickets

--- static/js/chosen/my-init.js
+++ static/js/chosen/my-init.js
@@ -1,6 +1,6 @@
 var config = {
-    '.chosen-select': {},
-    '.chosen-select-contains': {search_contains: true},
+    '.chosen-select': {placeholder_text_multiple: "Select multiple"},
+    '.chosen-select-contains': {placeholder_text_multiple: "Select multiple", search_contains:true},
     '.chosen-select-deselect': {allow_single_deselect: true},
     '.chosen-select-no-single': {disable_search_threshold: 10},
     '.chosen-select-no-results': {no_results_text: 'Oops, nothing found!'},
