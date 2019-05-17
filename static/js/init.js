5d1085f1b91ff48afff0d0a865a6c59e947d4e53
david fishman <davjfish@gmail.com>
2019-05-17 13:25:44 -0300

    issue #105

--- static/js/init.js
+++ static/js/init.js
@@ -2,7 +2,7 @@
 
 var config = {
     '.chosen-select': {placeholder_text_multiple: "Select multiple"},
-    '.chosen-select-contains': {placeholder_text_multiple: "Select multiple", search_contains:true},
+    '.chosen-select-contains': {placeholder_text_multiple: "Select multiple", search_contains: true},
     '.chosen-select-deselect': {allow_single_deselect: true},
     '.chosen-select-no-single': {disable_search_threshold: 10},
     '.chosen-select-no-results': {no_results_text: 'Oops, nothing found!'},
@@ -14,7 +14,6 @@ for (var selector in config) {
 }
 
 
-
 var fpConfig = {
     '.fp-date': {},
     '.fp-date-time': {enableTime: true, time_24hr: true},
@@ -22,3 +21,30 @@ var fpConfig = {
 for (var selector in fpConfig) {
     $(selector).flatpickr(fpConfig[selector]);
 }
+
+
+// for . editable things i.e. medium-editor / medium-editor-table
+var editor = new MediumEditor('.editable', {
+    buttonLabels: 'fontawesome',
+    extensions: {
+        table: new MediumEditorTable()
+    },
+    toolbar: {
+        buttons: [
+            'h2',
+            'bold',
+            'italic',
+            'unorderedlist',
+            'orderedlist',
+            'table'
+        ],
+        static: true,
+        sticky: true
+    }
+});
+
+// enable popovers everywhere
+$(function () {
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="popover"]')[0].style.cursor = "pointer";
+});
