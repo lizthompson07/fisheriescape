44c908d47b8f47a9d8193f5fa79e1477c5bf2d38
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-06-05 13:05:44 -0300

    issue #139

--- projects/emails.py
+++ projects/emails.py
@@ -1,117 +1,22 @@
-from django.conf import settings
 from django.template import loader
-from . import models
+from . import views
+from_email = 'DoNotReply@DMApps.com'
 
-#
-# app_name = settings.WEB_APP_NAME # should be a single word with one space
-# from_email='DoNotReply@{}.com'.format(app_name)
-# admin_email = 'david.fishman@dfo-mpo.gc.ca'
-#
-# class CertificationRequestEmail:
-#
-#     def __init__(self, person_object):
-#         self.person_object = person_object
-#         self.subject = 'Your metadata inventory'
-#         self.message = self.load_html_template()
-#         self.from_email = admin_email
-#         self.to_list = [person_object.user.email, admin_email]
-#         # self.cc_list = [admin_email,]
-#
-#     def load_html_template(self):
-#         t = loader.get_template('inventory/email_certification_request.html')
-#         context ={
-#             'object': self.person_object,
-#             'queryset':  self.person_object.resource_people.filter(role=1)
-#         }
-#         rendered = t.render(context)
-#         return rendered
-#
-# class SectionReportEmail:
-#
-#     def __init__(self, person_object, section):
-#         self.person_object = person_object
-#         self.section = section
-#         self.queryset = section.resources.all().order_by("title_eng")
-#         self.subject = "Your Section's Data Inventory"
-#         self.message = self.load_html_template()
-#         self.from_email = admin_email
-#         self.to_list = [person_object.user.email,admin_email]
-#
-#     def load_html_template(self):
-#         t = loader.get_template('inventory/email_section_head_report.html')
-#         context ={
-#             'object': self.person_object,
-#             'queryset':  self.queryset,
-#             'section':  self.section,
-#         }
-#         rendered = t.render(context)
-#         return rendered
-#
-# class FlagForDeletionEmail:
-#
-#     def __init__(self, object, user):
-#         self.subject = 'A data resource has been flagged for deletion'
-#         self.message = self.load_html_template(object,user)
-#         self.from_email = from_email
-#         self.to_list = [admin_email,]
-#
-#     def load_html_template(self, object,user):
-#         t = loader.get_template('inventory/email_flagged_for_deletion.html')
-#         context ={
-#             'object': object,
-#             'user': user,
-#         }
-#         rendered = t.render(context)
-#         return rendered
-#
-#
-# class FlagForPublicationEmail:
-#
-#     def __init__(self, object, user):
-#         self.subject = 'A data resource has been flagged for publication'
-#         self.message = self.load_html_template(object,user)
-#         self.from_email = from_email
-#         self.to_list = [admin_email,]
-#
-#     def load_html_template(self, object,user):
-#         t = loader.get_template('inventory/email_flagged_for_publication.html')
-#         context ={
-#             'object': object,
-#             'user': user,
-#         }
-#         rendered = t.render(context)
-#         return rendered
-#
-# class AddedAsCustodianEmail:
-#
-#     def __init__(self, object, user):
-#         self.subject = 'You have been added as a custodian'
-#         self.message = self.load_html_template(object,user)
-#         self.from_email = from_email
-#         self.to_list = [user.email,]
-#
-#     def load_html_template(self, object,user):
-#         t = loader.get_template('inventory/email_added_as_custodian.html')
-#         context ={
-#             'object': object,
-#             'user': user,
-#         }
-#         rendered = t.render(context)
-#         return rendered
-#
-# class RemovedAsCustodianEmail:
-#
-#     def __init__(self, object, user):
-#         self.subject = 'You have been removed as a custodian'
-#         self.message = self.load_html_template(object,user)
-#         self.from_email = from_email
-#         self.to_list = [user.email,]
-#
-#     def load_html_template(self, object,user):
-#         t = loader.get_template('inventory/email_removed_as_custodian.html')
-#         context ={
-#             'object': object,
-#             'user': user,
-#         }
-#         rendered = t.render(context)
-#         return rendered
+class ProjectSubmissionEmail:
+    def __init__(self, object):
+        self.subject = 'A project has been submitted under your section'
+        self.message = self.load_html_template(object)
+        self.from_email = from_email
+        try:
+            self.to_list = [object.section.head.email]
+        except AttributeError:
+            self.to_list = ["david.fishman@dfo-mpo.gc.ca"]
+
+    def __str__(self):
+        return "FROM: {}\nTO: {}\nSUBJECT: {}\nMESSAGE:{}".format(self.from_email, self.to_list, self.subject, self.message)
+
+    def load_html_template(self, object):
+        t = loader.get_template('projects/email_project_submitted.html')
+        context = {'object': object, 'field_list':views.project_field_list}
+        rendered = t.render(context)
+        return rendered
