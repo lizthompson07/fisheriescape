842954b4f6cebce79bcb68b67e116b25944abedb
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-06-07 10:21:50 -0300

    changed project name and upgraded django to ver 2.2

--- dm_apps/urls.py
+++ dm_apps/urls.py
@@ -1,4 +1,4 @@
-"""dfo_sci_dm_site URL Configuration
+"""dm_apps URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
