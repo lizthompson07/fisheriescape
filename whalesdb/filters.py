a3507832cdbb28394fb7ffbcff44e1cb6ba9153e
Upson <UpsonP@dfo-mpo.ca>
2019-07-16 14:55:50 -0300

    whale updates lots of new forms

--- whalesdb/filters.py
+++ whalesdb/filters.py
@@ -1,13 +1,30 @@
-from . import models
+from django.utils.translation import gettext_lazy as _
 from django_filters import FilterSet
 
+from . import models
+from . import forms
 
-class FilterHydrophone(FilterSet):
+
+class EquipmentFilterSet(FilterSet):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        labels = forms.get_short_labels(self._meta.model)
+        for key in labels.keys():
+            self.filters[key].label = labels[key]
+
+
+class FilterRecorder(EquipmentFilterSet):
+
+    class Meta:
+        model = models.EqrRecorderProperties
+        fields = ['emm', 'eqc_max_channels', 'eqc_max_sample_rate']
+
+
+class FilterHydrophone(EquipmentFilterSet):
 
     class Meta:
         model = models.EqhHydrophoneProperties
         fields = ['emm', 'eqh_range_max', 'eqh_range_min']
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
