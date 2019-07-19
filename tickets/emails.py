087548dfc04470d2edfb9691b6dd293f72054c6c
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-07-19 11:17:37 -0300

    update to new hostname

--- tickets/emails.py
+++ tickets/emails.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.template import loader
 
-from_email = 'DoNotReply@DFOScienceDataManagement.com'
+from_email = 'DoNotReply@DMApps.com'
 admin_email = 'david.fishman@dfo-mpo.gc.ca'
 
 
