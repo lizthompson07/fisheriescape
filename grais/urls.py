01f19659ce791cd1a2065e7ce8dde0c8eb0742f0
david fishman <davjfish@gmail.com>
2019-07-26 16:42:54 -0300

    184

--- grais/urls.py
+++ grais/urls.py
@@ -165,5 +165,8 @@ urlpatterns = [
     path('reports/search/', views.ReportSearchFormView.as_view(), name="report_search"),
     path('reports/species-by-sample-spreadsheet/<str:species_list>/', views.species_sample_spreadsheet_export,
          name="spp_sample_xlsx"),
+    path('reports/opendata1/<str:year>/', views.export_open_data_ver1, name="od1_report"),
+    path('reports/opendata1/', views.export_open_data_ver1, name="od1_report"),
+    path('reports/opendata1/dictionary/', views.export_open_data_ver1_dictionary, name="od1_dictionary"),
 
 ]
