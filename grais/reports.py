fdf8fc02cb3639202e0c33fa5f959f4e6f137ca9
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-08-06 11:48:34 -0300

    184

--- grais/reports.py
+++ grais/reports.py
@@ -491,3 +491,93 @@ def generate_open_data_ver_1_report(year=None):
         writer.writerow(data_row)
 
     return response
+
+
+def generate_open_data_ver_1_wms_report(year=None):
+    """
+    Simple report for web mapping service on FGP
+    """
+
+    # Botrylloïdes violaceus, Botryllus shlosseri, Caprella mutica, Ciona intestinalis, Codium fragile, Membranipora membranacea, Styela clava
+    species_id_list = [48, 24, 47, 23, 55, 59, 25]
+    species_qs = models.Species.objects.filter(id__in=species_id_list)
+
+    filename = "open_data_ver1_wms_report.csv"
+
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+    response.write(u'\ufeff'.encode('utf8'))  # BOM (optional...Excel needs it to open UTF-8 file properly)
+    writer = csv.writer(response)
+
+    header_row_eng = [
+        'Station code',
+        'Station name',
+        'Station province',
+        'Station latitude',
+        'Station longitude',
+        'List of other species observed',
+    ]
+
+    header_row_fra = [
+        'code de station',
+        'Nom de station',
+        'province de station',
+        'Latitude de station',
+        'Longitude de station',
+        'Liste des espèces observées',
+    ]
+
+    for species in species_qs:
+        first_name = species.scientific_name.split(" ")[0][:1].upper()
+        if len(species.scientific_name.split(" ")) > 2:
+            second_name = " ".join(species.scientific_name.split(" ")[1:])
+        else:
+            second_name = species.scientific_name.split(" ")[1]
+        display_name = "{}. {}".format(first_name, second_name, )
+        header_row_eng.append("{} detected?".format(display_name))
+        header_row_fra.append("{} détecté?".format(display_name))
+
+    writer.writerow(header_row_eng)
+    writer.writerow(header_row_fra)
+
+    samples = models.Sample.objects.all()
+    # if there is a year provided, filter by only this year
+    if year and year != "None":
+        samples = samples.filter(season=year)
+
+    stations = [models.Station.objects.get(pk=obj["station"]) for obj in samples.order_by("station").values("station").distinct()]
+    # make sure to exclude the lost lines and surfaces; this is sort of redundant since if a line is line, all surfaces should also be labelled as lost.
+    surfacespecies = models.SurfaceSpecies.objects.filter(
+        surface__line__sample_id__in=[obj["id"] for obj in samples.order_by("id").values("id").distinct()],
+        surface__line__is_lost=False,
+        surface__is_lost=False,
+    )
+
+    for station in stations:
+        other_spp = listrify([str(models.Species.objects.get(pk=obj["species"])) for obj in
+                              surfacespecies.filter(surface__line__sample__station=station).order_by("species").values("species").distinct()
+                              if obj["species"] not in species_id_list])
+
+        data_row = [
+            station.id,
+            station.station_name,
+            "{} / {}".format(station.province.abbrev_eng,station.province.abbrev_fre),
+            station.latitude_n,
+            station.longitude_w,
+            other_spp,
+        ]
+
+        for species in species_qs:
+            spp_count = surfacespecies.filter(
+                surface__line__sample__station=station,
+                species=species,
+            ).count()
+            if spp_count > 0:
+                data_row.append(True)
+            else:
+                data_row.append(False)
+
+        writer.writerow(data_row)
+
+    return response
