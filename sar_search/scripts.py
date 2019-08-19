15d2e3a21438f48e671403192653eff6aeb9c739
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-08-19 11:43:05 -0300

    191

--- sar_search/scripts.py
+++ sar_search/scripts.py
@@ -42,18 +42,67 @@ def import_nb_county_points():
                 name=row["ENG_NAME"],
                 province_id=1
             )
-            if my_region.id == 21:
 
-                # create a new polygon
-                my_poly = models.RegionPolygon.objects.get(
-                    region=my_region,
-                    old_id=row["ID"],
-                )
+            # create a new polygon
+            my_poly = models.RegionPolygon.objects.get(
+                region=my_region,
+                old_id=row["ID"],
+            )
 
-                # clear any existing point associated with this polygon
-                # my_poly.points.all().delete()
+            # clear any existing point associated with this polygon
+            # my_poly.points.all().delete()
 
-                my_poly.points.create(
-                    latitude=float(row["y"]),
-                    longitude=float(row["x"]),
-                )
+            my_poly.points.create(
+                latitude=float(row["y"]),
+                longitude=float(row["x"]),
+            )
+
+
+
+
+def import_ns_county_polygons():
+    # open the csv we want to read
+    rootdir = "C:\\Users\\fishmand\\Desktop\\dump"
+    with open(os.path.join(rootdir, "ns_county_polygons.csv"), 'r') as csv_read_file:
+        my_csv = csv.DictReader(csv_read_file)
+        # count = 0
+        for row in my_csv:
+            # get or create the name of the county
+            my_region, created = models.Region.objects.get_or_create(
+                name=row["county"],
+                province_id=3
+            )
+            # update the french name
+            my_region.nom = row["county"]
+            my_region.save()
+
+            # create a new polygon
+            my_poly, created = models.RegionPolygon.objects.get_or_create(
+                region=my_region,
+                old_id=row["ID"],
+            )
+
+
+def import_ns_county_points():
+    # open the csv we want to read
+    rootdir = "C:\\Users\\fishmand\\Desktop\\dump"
+    with open(os.path.join(rootdir, "ns_county_points.csv"), 'r') as csv_read_file:
+        my_csv = csv.DictReader(csv_read_file)
+        # count = 0
+        for row in my_csv:
+            # get or create the name of the county
+            my_region = models.Region.objects.get(
+                name=row["county"],
+                province_id=3
+            )
+
+            # create a new polygon
+            my_poly = models.RegionPolygon.objects.get(
+                region=my_region,
+                old_id=row["ID"],
+            )
+
+            my_poly.points.create(
+                latitude=float(row["y"]),
+                longitude=float(row["x"]),
+            )
