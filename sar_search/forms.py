7b7770bb534bf9f6b41d7a676fa5b96766d0511a
david.fishman@dfo-mpo.gc.ca <david.fishman@dfo-mpo.gc.ca>
2019-08-20 11:00:11 -0300

    191

--- sar_search/forms.py
+++ sar_search/forms.py
@@ -41,6 +41,15 @@ class RecordForm(forms.ModelForm):
 
         }
 
+class RegionPolygonForm(forms.ModelForm):
+    class Meta:
+        model = models.RegionPolygon
+        exclude = ["date_last_modified"]
+        widgets = {
+            "last_modified_by": forms.HiddenInput(),
+            "region": forms.HiddenInput(),
+        }
+
     # def __init__(self, *args, **kwargs):
     #     super().__init__(*args, **kwargs)
     #     if kwargs.get("instance") or kwargs.get("initial"):
@@ -138,57 +147,17 @@ CoordFormSetNoExtra = modelformset_factory(
     extra=0,
 )
 
-#
-# class RiverForm(forms.ModelForm):
-#     class Meta:
-#         model = shared_models.River
-#         fields = "__all__"
-#
-#
 
-#
-# class SampleForm(forms.ModelForm):
-#     class Meta:
-#         model = models.Sample
-#         exclude = ["last_modified", 'season']
-#         widgets = {
-#             "site": forms.HiddenInput(),
-#             "last_modified_by": forms.HiddenInput(),
-#             "samplers": forms.Textarea(attrs={"rows": "2", }),
-#             "notes": forms.Textarea(attrs={"rows": "3", }),
-#             "arrival_date": forms.DateTimeInput(attrs=attr_fp_date_time),
-#             "departure_date": forms.DateTimeInput(attrs=attr_fp_date_time),
-#         }
-#
-# class EntryForm(forms.ModelForm):
-#     class Meta:
-#         model = models.Entry
-#         fields = "__all__"
-#         widgets = {
-#             'species': forms.HiddenInput(),
-#             'sample': forms.HiddenInput(),
-#             'notes': forms.Textarea(attrs={"rows": "3"}),
-#             "date_tagged": forms.DateTimeInput(attrs=attr_fp_date),
-#         }
-#
-#
-# class ReportSearchForm(forms.Form):
-#     REPORT_CHOICES = (
-#         (None, "---"),
-#         (1, "List of samples (trap data) (.CSV)"),
-#         (2, "List of entries (fish data) (.CSV)"),
-#         (3, "OPEN DATA - summary by site by year (.CSV)"),
-#     )
-#
-#     report = forms.ChoiceField(required=True, choices=REPORT_CHOICES)
-#     year = forms.CharField(required=False, widget=forms.NumberInput(), label="Year (optional)")
-#     sites = forms.MultipleChoiceField(required=False, label="Sites (optional)")
-#
-#     # site = forms.ChoiceField(required=False)
-#     # ais_species = forms.MultipleChoiceField(required=False, label="AIS species")
-#
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         site_choices = [(obj.id, str(obj)) for obj in models.RiverSite.objects.all() if obj.samples.count() > 0]
-#         self.fields['sites'].choices = site_choices
-#
+class RPCoordForm(forms.ModelForm):
+    class Meta:
+        model = models.RegionPolygonPoint
+        fields = "__all__"
+        widgets = {
+            "region_polygon": forms.HiddenInput(),
+        }
+
+RPCoordFormSet = modelformset_factory(
+    model=models.RegionPolygonPoint,
+    form=RPCoordForm,
+    extra=1,
+)
