5d6368063df2f4c9f87182528270801890956c4a
David Fishman <FISHMAND@dfo-mpo.ca>
2019-09-25 14:35:45 -0300

    #206

--- masterlist/models.py
+++ masterlist/models.py
@@ -56,16 +56,10 @@ class Reserve(models.Model):
 
 
 class Nation(models.Model):
-    name = models.CharField(max_length=255, verbose_name=_("name (English)"))
-    nom = models.CharField(max_length=255, blank=True, null=True, verbose_name=_("Name (French)"))
+    name = models.CharField(max_length=255, verbose_name=_("name"))
 
     def __str__(self):
-        # check to see if a french value is given
-        if getattr(self, str(_("name"))):
-            return "{}".format(getattr(self, str(_("name"))))
-        # if there is no translated term, just pull from the english field
-        else:
-            return "{}".format(self.name)
+        return self.name
 
     class Meta:
         ordering = ['name', ]
