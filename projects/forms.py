fcaaef5dbb8e36c59232b508c3b07e4a58889d58
David Fishman <FISHMAND@dfo-mpo.ca>
2019-10-10 16:23:05 -0300

    #105

--- projects/forms.py
+++ projects/forms.py
@@ -323,8 +323,8 @@ class ReportSearchForm(forms.Form):
         (4, "Science program list (MS Excel)"),
 
         # Gulf region reports
-        (10, _("GULF: Weeks Worked by Employee")),
-        (11, _("GULF: Total OT Hours Requested")),
+        (10, _("GULF: Weeks Worked by Employees")),
+        (11, _("GULF: Total Overtime Hours Requested")),
         (12, _("GULF: Cost Summary by Section")),
         (13, _("GULF: List of Collaborators")),
         (14, _("GULF: Doug's Report")),
