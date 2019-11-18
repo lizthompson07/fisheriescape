2ba815589e3f6c899f06be1d2740d26416f24b79
Upson <UpsonP@dfo-mpo.ca>
2019-11-18 09:00:06 -0400

    removed whalesdb debugging statements

--- whalesdb/views.py
+++ whalesdb/views.py
@@ -680,7 +680,6 @@ class DetailsHydrophone(DetailsMakeModel):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print("kwargs: " + str(kwargs['object'].emm))
 
         labels = forms.get_descriptions(models.EqhHydrophoneProperties)
         context['objects'].append({
@@ -689,7 +688,6 @@ class DetailsHydrophone(DetailsMakeModel):
         })
         context['url'] = 'eqh'
 
-        print("context: " + str(context['objects'][0]['object'].eqt))
         return context
 
 
@@ -770,7 +768,6 @@ class SimpleEditView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('whalesdb:close_me')
 
     def get_initial(self):
-        print("init")
         return super().get_initial()
 
     def get_context_data(self, **kwargs):
