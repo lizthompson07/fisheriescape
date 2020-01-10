cedc4ccc2f6869442a8f3726c05c59b9ad897e4f
David Fishman <davjfish@gmail.com>
2020-01-10 11:21:28 -0400

    revamp

--- projects/models.py
+++ projects/models.py
@@ -377,7 +377,7 @@ class Project(models.Model):
     # rds_approved = models.BooleanField(default=False, verbose_name=_("RDS approved"))
     # rds_feedback = models.TextField(blank=True, null=True, verbose_name=_("RDS feedback"))
 
-    meeting_notes = models.TextField(blank=True, null=True, verbose_name=_("meeting notes"))
+    meeting_notes = models.TextField(blank=True, null=True, verbose_name=_("notes / comments"))
 
     is_hidden = models.NullBooleanField(default=False, verbose_name=_("Should the project be hidden from other users?"))
 
