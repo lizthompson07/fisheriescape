aa54f7a787768e8b7440c5cb6737d1d7f159c2ba
Upson <UpsonP@dfo-mpo.ca>
2020-01-21 15:38:52 -0400

    Whales snap

--- whalesdb/urls.py
+++ whalesdb/urls.py
@@ -7,6 +7,14 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('close/', views.CloserTemplateView.as_view(), name="close_me"),
 
+    path('create/stn', views.CreateStn.as_view(), name="create_stn"),
+    path('details/<int:pk>/stn', views.DetailsStn.as_view(), name="details_stn"),
+    path('list/stn/', views.ListStn.as_view(), name="list_stn"),
+
+    path('create/prj', views.CreatePrj.as_view(), name="create_prj"),
+    path('details/<int:pk>/prj', views.DetailsPrj.as_view(), name="details_prj"),
+    path('list/prj/', views.ListPrj.as_view(), name="list_prj"),
+
     # General EMM Make and Model forms
     path('create/emm', views.CreateMakeModel.as_view(), name="create_emm"),
     path('details/<int:pk>/emm', views.DetailsMakeModel.as_view(), name="details_emm"),
