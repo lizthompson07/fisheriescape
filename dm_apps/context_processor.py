2e7414d67d848d126ef903770fba59516178c3a2
David Fishman <davjfish@gmail.com>
2020-01-21 16:20:41 -0400

    integrating django-decouple package

--- dm_apps/context_processor.py
+++ dm_apps/context_processor.py
@@ -1,9 +1,12 @@
 from django.conf import settings
 
+
 def my_envr(request):
     # return the value you want as a dictionary. you may add multiple values in there.
     return {
         'PRODUCTION_SERVER': settings.PRODUCTION_SERVER,
         'USING_PRODUCTION_DB': settings.USING_PRODUCTION_DB,
         'USING_LOCAL_DB': settings.USING_LOCAL_DB,
+        'DEV_DB_NAME': settings.DEV_DB_NAME,
+        'DEV_DB_HOST': settings.DEV_DB_HOST,
     }
--- dm_apps/utils.py
+++ dm_apps/utils.py
@@ -1,8 +1,11 @@
-from decouple import config
+from decouple import config, UndefinedValueError
 
 def is_connection_available(prefix):
-    return config(prefix + '_DB_HOST') and \
-           config(prefix + '_DB_PORT') and \
-           config(prefix + '_DB_NAME') and \
-           config(prefix + '_DB_USER') and \
-           config(prefix + '_DB_PASSWORD')
+    try:
+        return config(prefix + '_DB_HOST') and \
+               config(prefix + '_DB_PORT') and \
+               config(prefix + '_DB_NAME') and \
+               config(prefix + '_DB_USER') and \
+               config(prefix + '_DB_PASSWORD')
+    except UndefinedValueError:
+        return False
