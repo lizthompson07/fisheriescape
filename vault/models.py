a31e7aeb7a306e4d5bdec2518df5e6309a263e27
ricardd <daniel.ricard@gmail.com>
2020-01-22 15:23:13 -0400

    work on vault database structure

--- vault/models.py
+++ vault/models.py
@@ -4,147 +4,164 @@ from django.urls import reverse
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from shared_models import models as shared_models
-#
-# class Species(models.Model):
-#     common_name_eng = models.CharField(max_length=255, blank=True, null=True)
-#     common_name_fre = models.CharField(max_length=255, blank=True, null=True)
-#     scientific_name = models.CharField(max_length=255, blank=True, null=True)
-#     abbrev = models.CharField(max_length=255, blank=True, null=True)
-#     tsn = models.IntegerField(blank=True, null=True, verbose_name="ITIS TSN")
-#     aphia_id = models.IntegerField(blank=True, null=True, verbose_name="AphiaID")
-#
-#     def __str__(self):
-#         if self.common_name_eng:
-#             return "{}-{}".format(self.id, self.common_name_eng)
-#         else:
-#             return "{}-{}".format(self.id, self.scientific_name)
-#
-#     class Meta:
-#         ordering = ['id']
-#
-#     def get_absolute_url(self):
-#         return reverse('diets:species_detail', kwargs={'pk': self.id})
-#
-#     def save(self, *args, **kwargs):
-#         self.last_modified_date = timezone.now()
-#         return super().save(*args, **kwargs)
-#
-#     @property
-#     def full_name(self):
-#         return "{} (<em>{}</em>)".format(self.common_name_eng, self.scientific_name)
-#
-#
-# class Sampler(models.Model):
-#     first_name = models.CharField(max_length=255, blank=True, null=True)
-#     last_name = models.CharField(max_length=255, blank=True, null=True)
-#     notes = models.TextField(null=True, blank=True)
-#
-#     def __str__(self):
-#         return "{} {}".format(self.first_name, self.last_name)
-#
-#     class Meta:
-#         ordering = ['first_name', 'last_name']
-#
-#
-# class Predator(models.Model):
-#     cruise = models.ForeignKey(shared_models.Cruise, related_name='predators', on_delete=models.DO_NOTHING, blank=True, null=True)
-#     samplers = models.ManyToManyField(Sampler, blank=True)
-#     stomach_id = models.CharField(max_length=10, blank=True, null=True, verbose_name="stomach ID", unique=True)
-#     processing_date = models.DateTimeField(verbose_name="processing date", default=timezone.now, blank=True, null=True)
-#     set = models.IntegerField(blank=True, null=True)
-#     species = models.ForeignKey(Species, related_name='predators', on_delete=models.DO_NOTHING, verbose_name="predator species")
-#     fish_number = models.IntegerField(blank=True, null=True)
-#     somatic_length_cm = models.FloatField(null=True, blank=True, verbose_name="body length (cm)")
-#     stomach_wt_g = models.FloatField(null=True, blank=True, verbose_name="stomach weight (g)")
-#     content_wt_g = models.FloatField(null=True, blank=True, verbose_name="content weight (g)")
-#     comments = models.TextField(blank=True, null=True)
-#
-#     # not actively used
-#     stratum = models.IntegerField(blank=True, null=True)
-#     somatic_wt_g = models.FloatField(null=True, blank=True, verbose_name="body weight (g)")
-#     old_seq_num = models.IntegerField(blank=True, null=True)
-#     # meta
-#     date_last_modified = models.DateTimeField(blank=True, null=True, default=timezone.now)
-#     last_modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
-#
-#     # TODO: IN FUTURE YEARS COMBO OF YEAR AND STOMACH ID SHOULD BE UNIQUE
-#
-#     class Meta:
-#         ordering = ['-processing_date', ]
-#
-#     def __str__(self):
-#         return " Predator {}".format(self.id)
-#
-#     def get_absolute_url(self):
-#         return reverse('diets:predator_detail', kwargs={'pk': self.id})
-#
-#     def save(self, *args, **kwargs):
-#         self.date_last_modified = timezone.now()
-#         if self.stomach_id:
-#             self.stomach_id = self.stomach_id.upper()
-#         return super().save(*args, **kwargs)
-#
-#     @property
-#     def season(self):
-#         return self.processing_date.year
-#
-#
-# class DigestionLevel(models.Model):
-#     code = models.IntegerField(unique=True)
-#     level = models.CharField(max_length=150)
-#     interpretation = models.TextField(blank=True, null=True)
-#
-#     class Meta:
-#         ordering = ['id']
-#
-#     def __str__(self):
-#         return "{}-{}".format(self.code, self.level)
-#
-#
-# class Prey(models.Model):
-#     predator = models.ForeignKey(Predator, on_delete=models.DO_NOTHING, related_name="prey_items")
-#     species = models.ForeignKey(Species, on_delete=models.DO_NOTHING, related_name="prey_items")
-#     digestion_level = models.ForeignKey(DigestionLevel, on_delete=models.DO_NOTHING, blank=True, null=True)
-#     somatic_length_mm = models.FloatField(null=True, blank=True, verbose_name="body length (mm)")
-#     length_comment = models.CharField(max_length=250, blank=True, null=True)
-#     number_of_prey = models.IntegerField(blank=True, null=True, verbose_name="number of prey")
-#     somatic_wt_g = models.FloatField(null=True, blank=True, verbose_name="body weight (g)")
-#     comments = models.TextField(blank=True, null=True)
-#
-#     # not used
-#     censored_length = models.BooleanField(default=False)
-#     stomach_wt_g = models.FloatField(null=True, blank=True, verbose_name="stomach weight (g)")
-#
-#     # meta
-#     old_id = models.IntegerField(blank=True, null=True)
-#     last_modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
-#     date_last_modified = models.DateTimeField(blank=True, null=True, default=timezone.now)
+
+
+class Species(models.Model):
+    code = models.CharField(max_length=4, blank=True, null=True, verbose_name=_(""))
+    english_name = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    french_name = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    latin_name = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    vor_code = models.CharField(max_length=250, blank=True, null=True, verbose_name=_("VOR code"))
+    quebec_code = models.CharField(max_length=250, blank=True, null=True, verbose_name=_("Quebec code"))
+    aphia_id = models.IntegerField(null=True, blank=True)
+
+class PersonType(models.Model):
+    name = models.CharField(max_length=255)
+    nom = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        # check to see if a french value is given
+        if getattr(self, str(_("name"))):
+
+            return "{}".format(getattr(self, str(_("name"))))
+        # if there is no translated term, just pull from the english field
+        else:
+            return "{}".format(self.name)
+
+    class Meta:
+        ordering = ['name', ]
+
+class Person(models.Model):
+    person_type = models.ForeignKey(PersonType, on_delete=models.DO_NOTHING, related_name="people", verbose_name=_(""))
+    first_name = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    last_name = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    organisation = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    email = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+
+class Instrument(models.Model):
+    name = models.CharField(max_length=255)
+    nom = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        # check to see if a french value is given
+        if getattr(self, str(_("name"))):
+
+            return "{}".format(getattr(self, str(_("name"))))
+        # if there is no translated term, just pull from the english field
+        else:
+            return "{}".format(self.name)
+
+    class Meta:
+        ordering = ['name', ]
 
 class ObservationPlatform(models.Model):
-    platform_name = models.CharField(max_length=250, blank=True, null=True)
+    type_choices = (
+        (1, _("plane")),
+        (2, _("boat")),
+        (3, _("drone")),
+        (4, _("mooring")),
+        (5, _("glider")),
+        (6, _("land")),
+        (7, _("space")),
+    )
+    type = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""), choices=type_choices)
+    authority = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    owner = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    make_model = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    name = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
 
-class MediaDataPath(models.Model):
+class Outing(models.Model):
+    observation_platform = models.ForeignKey(ObservationPlatform, on_delete=models.DO_NOTHING, related_name="outings",
+                                             verbose_name=_("observation platform"))
+    region = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    purpose = models.CharField(max_length=250, blank=True, null=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    identifier_string = models.CharField(max_length=250, blank=True, null=True)
+
+    def __str__(self):
+        return self.identifier_string
+
+class Observation(models.Model):
+    outing = models.ForeignKey(Outing, on_delete=models.DO_NOTHING, related_name="sightings", verbose_name=_(""))
+    instrument = models.ForeignKey(Instrument, on_delete=models.DO_NOTHING, related_name="sightings", verbose_name=_(""))
+    datetime = models.DateTimeField(null=True, blank=True, verbose_name=_(""))
+    longitude = models.FloatField(null=True, blank=True, verbose_name=_(""))
+    latitude = models.FloatField(null=True, blank=True, verbose_name=_(""))
+    observer = models.ForeignKey(Person, on_delete=models.DO_NOTHING, related_name="sightings", verbose_name=_(""), null=True, blank=True)
+    beaufort = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    glare_left = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    glare_right = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    cloud_percent = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+
+class Certainty(models.Model):
+    code = models.IntegerField(blank=True, null=True, verbose_name=_(""))
+    english_certainty_description = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    french_certainty_description = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+
+class Sex(models.Model):
+    name = models.CharField(max_length=255)
+    nom = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        # check to see if a french value is given
+        if getattr(self, str(_("name"))):
+
+            return "{}".format(getattr(self, str(_("name"))))
+        # if there is no translated term, just pull from the english field
+        else:
+            return "{}".format(self.name)
+
+    class Meta:
+        ordering = ['name', ]
+
+class LifeStage(models.Model):
+    name = models.CharField(max_length=255)
+    nom = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        # check to see if a french value is given
+        if getattr(self, str(_("name"))):
+
+            return "{}".format(getattr(self, str(_("name"))))
+        # if there is no translated term, just pull from the english field
+        else:
+            return "{}".format(self.name)
+
+    class Meta:
+        ordering = ['name', ]
+
+
+class ObservationSighting(models.Model):
+    observation = models.ForeignKey(Observation, on_delete=models.DO_NOTHING, related_name="observation_sightings", verbose_name=_(""))
+    species = models.ForeignKey(Species, on_delete=models.DO_NOTHING, related_name="observation_sightings", null=True, blank=True)
+    certainty = models.ForeignKey(Certainty, on_delete=models.DO_NOTHING, related_name="observation_sightings", null=True, blank=True)
+    sex = models.ForeignKey(Sex, on_delete=models.DO_NOTHING, related_name="observation_sightings", null=True, blank=True)
+    life_stage = models.ForeignKey(LifeStage, on_delete=models.DO_NOTHING, related_name="observation_sightings", null=True, blank=True)
+    verified = models.BooleanField(default=False, verbose_name=_(""))
+    # known_individual = models.ForeignKey(Individual)
+
+class OriginalMediafile(models.Model):
     file_path = models.CharField(max_length=1000, blank=True, null=True)
     filename = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
-    platform = models.ForeignKey(ObservationPlatform, on_delete=models.DO_NOTHING, related_name="media_data_paths", verbose_name=_("platform"))
+    observation = models.ForeignKey(Observation, on_delete=models.DO_NOTHING, related_name="original_mediafiles", verbose_name=_(""))
 
+class MediafileSighting(models.Model):
+    mediafile = models.ForeignKey(OriginalMediafile, on_delete=models.DO_NOTHING, related_name="mediafile_sightings", verbose_name=_(""))
+    species = models.ForeignKey(Species, on_delete=models.DO_NOTHING, related_name="mediafile_sightings", null=True, blank=True)
+    certainty = models.ForeignKey(Certainty, on_delete=models.DO_NOTHING, related_name="mediafile_sightings", null=True, blank=True)
+    sex = models.ForeignKey(Sex, on_delete=models.DO_NOTHING, related_name="mediafile_sightings", null=True, blank=True)
+    life_stage = models.ForeignKey(LifeStage, on_delete=models.DO_NOTHING, related_name="mediafile_sightings", null=True, blank=True)
+    verified = models.BooleanField(default=False, verbose_name=_(""))
+    # known_individual = models.ForeignKey(Individual)
 
-    # species = models.ForeignKey(Species, on_delete=models.DO_NOTHING, related_name="prey_items")
-    #
-    # digestion_level = models.ForeignKey(DigestionLevel, on_delete=models.DO_NOTHING, blank=True, null=True)
-    # somatic_length_mm = models.FloatField(null=True, blank=True, verbose_name="body length (mm)")
-    # length_comment = models.CharField(max_length=250, blank=True, null=True)
-    # number_of_prey = models.IntegerField(blank=True, null=True, verbose_name="number of prey")
-    # somatic_wt_g = models.FloatField(null=True, blank=True, verbose_name="body weight (g)")
-    # comments = models.TextField(blank=True, null=True)
-    #
-    # # not used
-    # censored_length = models.BooleanField(default=False)
-    # stomach_wt_g = models.FloatField(null=True, blank=True, verbose_name="stomach weight (g)")
-    #
-    # # meta
-    # old_id = models.IntegerField(blank=True, null=True)
-    # last_modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True)
-    # date_last_modified = models.DateTimeField(blank=True, null=True, default=timezone.now)
-    #
-
+class ProcessedMediafile(models.Model):
+    file_path = models.CharField(max_length=1000, blank=True, null=True)
+    filename = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    original_mediafile = models.ForeignKey(OriginalMediafile, on_delete=models.DO_NOTHING, related_name="processed_mediafiles",
+                                 verbose_name=_("original mediafile"))
+    species = models.ManyToManyField(Species, verbose_name=_(""), blank=True)
+    verified = models.BooleanField(default=False, verbose_name=_(""))
+    analyst = models.ForeignKey(Person, on_delete=models.DO_NOTHING)
+    date_analysed = models.DateTimeField()
+    date_created = models.DateTimeField()
