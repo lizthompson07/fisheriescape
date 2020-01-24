486bdaefcc21126058c300a283a26336be2c7380
David Fishman <davjfish@gmail.com>
2020-01-24 09:59:55 -0400

    #261

--- travel/urls.py
+++ travel/urls.py
@@ -8,33 +8,33 @@ urlpatterns = [
 
     # TRIP REQUEST #
     ################
-    path('trips/', views.TripListView.as_view(), name="trip_list"),
-    path('trip/new/', views.TripCreateView.as_view(), name="trip_new"),
-    path('trip/<int:pk>/view/', views.TripDetailView.as_view(), name="trip_detail"),
-    path('trip/<int:pk>/print/', views.TravelPlanPDF.as_view(), name="trip_print"),
-    path('trip/<int:pk>/edit/', views.TripUpdateView.as_view(), name="trip_edit"),
-    path('trip/<int:pk>/edit/<str:pop>/', views.TripUpdateView.as_view(), name="trip_edit"),
-    path('trip/<int:pk>/delete/', views.TripDeleteView.as_view(), name="trip_delete"),
-    path('trip/<int:pk>/delete/pop/<str:pop>/', views.TripDeleteView.as_view(), name="trip_delete"),
-    path('trip/<int:pk>/duplicate/', views.TripCloneUpdateView.as_view(), name="duplicate_event"),
-    path('trip/<int:pk>/new-child-trip/', views.TripCreateView.as_view(), name="trip_new"),
-    path('trip/<int:pk>/clone-duplicate/pop/<str:pop>', views.ChildTripCloneUpdateView.as_view(), name="child_duplicate_event"),
-    path('trip/<int:pk>/submit/', views.TripSubmitUpdateView.as_view(), name="trip_submit"),
-    path('trip/<int:pk>/re-add-reviewers/', views.reset_reviewers, name="reset_reviewers"),
+    path('requests/', views.TripRequestListView.as_view(), name="request_list"),
+    path('request/new/', views.TripRequestCreateView.as_view(), name="request_new"),
+    path('request/<int:pk>/view/', views.TripRequestDetailView.as_view(), name="request_detail"),
+    path('request/<int:pk>/print/', views.TravelPlanPDF.as_view(), name="request_print"),
+    path('request/<int:pk>/edit/', views.TripRequestUpdateView.as_view(), name="request_edit"),
+    path('request/<int:pk>/edit/<str:pop>/', views.TripRequestUpdateView.as_view(), name="request_edit"),
+    path('request/<int:pk>/delete/', views.TripRequestDeleteView.as_view(), name="request_delete"),
+    path('request/<int:pk>/delete/pop/<str:pop>/', views.TripRequestDeleteView.as_view(), name="request_delete"),
+    path('request/<int:pk>/duplicate/', views.TripRequestCloneUpdateView.as_view(), name="duplicate_event"),
+    path('request/<int:pk>/new-child-request/', views.TripRequestCreateView.as_view(), name="request_new"),
+    path('request/<int:pk>/clone-duplicate/pop/<str:pop>', views.ChildTripRequestCloneUpdateView.as_view(), name="child_duplicate_event"),
+    path('request/<int:pk>/submit/', views.TripRequestSubmitUpdateView.as_view(), name="request_submit"),
+    path('request/<int:pk>/re-add-reviewers/', views.reset_reviewers, name="reset_reviewers"),
 
     # REVIEWER APPROVAL
-    path('trips/review/', views.TripReviewListView.as_view(), name="trip_review_list"),
-    path('trips/review/<str:which_ones>/', views.TripReviewListView.as_view(), name="trip_review_list"),
+    path('requests/review/', views.TripRequestReviewListView.as_view(), name="request_review_list"),
+    path('requests/review/<str:which_ones>/', views.TripRequestReviewListView.as_view(), name="request_review_list"),
     path('review/<int:pk>/approve/', views.ReviewerApproveUpdateView.as_view(), name="review_approve"),
     path('review/<int:pk>/skip/', views.SkipReviewerUpdateView.as_view(), name="reviewer_skip"),
 
     # ADMIN APPROVAL
-    path('admin/approval/', views.TripAdminApprovalListView.as_view(), name="admin_approval_list"),
-    # path('admin/<int:pk>/approve/', views.TripAdminApproveUpdateView.as_view(), name="admin_approve"),
+    path('admin/approval/', views.TripRequestAdminApprovalListView.as_view(), name="admin_approval_list"),
+    # path('admin/<int:pk>/approve/', views.TripRequestAdminApproveUpdateView.as_view(), name="admin_approve"),
 
     # REVIEWERS #
     #############
-    path('trip/<int:trip>/manage-reviewers/', views.manage_reviewers, name="manage_reviewers"),
+    path('request/<int:request>/manage-reviewers/', views.manage_reviewers, name="manage_reviewers"),
     path('reviewer/<int:pk>/delete/', views.delete_reviewer, name="delete_reviewer"),
 
 
@@ -49,7 +49,7 @@ urlpatterns = [
 
     # FILES #
     #########
-    path('trip/<int:trip>/file/new/', views.FileCreateView.as_view(), name='file_new'),
+    path('request/<int:request>/file/new/', views.FileCreateView.as_view(), name='file_new'),
     path('file/<int:pk>/view/', views.FileDetailView.as_view(), name='file_detail'),
     path('file/<int:pk>/edit/', views.FileUpdateView.as_view(), name='file_edit'),
     path('file/<int:pk>/delete/', views.FileDeleteView.as_view(), name='file_delete'),
@@ -58,7 +58,7 @@ urlpatterns = [
     ###########
     path('reports/search/', views.ReportSearchFormView.as_view(), name="report_search"),
     path('reports/export-cfts-list/year/<int:fy>/user/<int:user>/', views.export_cfts_list, name="export_cfts_list"),
-    path('reports/cfts/trip/<int:pk>/', views.export_trip_cfts, name="export_cfts"),
+    path('reports/cfts/request/<int:pk>/', views.export_request_cfts, name="export_cfts"),
     # path('event/<int:fy>/<str:email>/print/', views.TravelPlanPDF.as_view(), name="travel_plan"),
 
 
