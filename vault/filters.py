c4034d7895209ed9dbe3c7fa14f2ee3b211f056d
ricardd <daniel.ricard@gmail.com>
2020-02-03 14:02:51 -0400

    adding some front-end features to the vault

--- vault/filters.py
+++ vault/filters.py
@@ -3,11 +3,11 @@ from . import models
 from django import forms
 
 #
-# class SpeciesFilter(django_filters.FilterSet):
-#     search_term = django_filters.CharFilter(field_name='search_term', label="Species (any part of name...)",
-#                                             lookup_expr='icontains', widget=forms.TextInput())
-#
-#
+class SpeciesFilter(django_filters.FilterSet):
+    search_term = django_filters.CharFilter(field_name='search_term', label="Species (any part of name...)",
+                                            lookup_expr='icontains', widget=forms.TextInput())
+
+
 # class PredatorFilter(django_filters.FilterSet):
 #     search_term = django_filters.CharFilter(field_name='search_term', label="Species (any part of name...)",
 #                                             lookup_expr='icontains', widget=forms.TextInput())
