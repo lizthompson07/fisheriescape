e3b94eeafd05becfa081064f65e0fd71e833f52d
David Fishman <davjfish@gmail.com>
2020-02-06 11:03:08 -0400

    #261

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('denied/', views.access_denied, name='denied_access'),
     path('denied/custodians-only/', views.access_denied_custodian, name='denied_access_custodian'),
+    path('denied/adm-admin-only/', views.access_denied_adm, name='denied_access_adm'),
     path('denied/project-leads-only/', views.access_denied_project_leads_only, name='denied_project_leads_only'),
     path('denied/section-heads-only/', views.access_denied_section_heads_only, name='denied_section_heads_only'),
     path('denied/section-heads-only/', views.access_denied_manager_or_admin_only, name='denied_manager_or_admin_only'),
