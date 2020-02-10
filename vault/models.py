69428467955b33cb3be3f9377a779e1dd98d3d10
Thompson <THOMPSONE@dfo-mpo.ca>
2020-02-10 10:47:02 -0400

    Added Items filter
    Starting a sub-app for Inventory tracking

--- vault/models.py
+++ vault/models.py
@@ -5,6 +5,43 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from shared_models import models as shared_models
 
+class Items(models.Model):
+    unique_id = models.CharField(max_length=250, blank=False, null=False, verbose_name=_(""))
+    item_name = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    description = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    owner = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    size = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    container_space = models.IntegerField(null=True, blank=True)
+    category = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    type = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+
+class Quantity(models.Model):
+    items_quantity = models.ForeignKey(Items, on_delete=models.DO_NOTHING, related_name="items",
+                      verbose_name=_("items"))
+    unique_id = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    serial_number = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    quantity_oh = models.IntegerField(null=True, blank=True)
+    quantity_oo = models.IntegerField(null=True, blank=True)
+    last_audited = models.DateTimeField(blank=True, null=True)
+    last_audited_by = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    location_stored = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    bin_id = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+
+class Supplier(models.Model):
+    items_supplier = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    supplier = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    contact_number = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    email = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    last_invoice = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    last_purchased = models.DateTimeField(blank=True, null=True)
+    last_purchased_by = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+
+class Lending(models.Model):
+    quantity_unique_id = models.ForeignKey(Quantity, on_delete=models.DO_NOTHING, related_name="items",
+                      verbose_name=_("items"))
+    quantity = models.IntegerField(null=True, blank=True)
+    lent_to = models.CharField(max_length=250, blank=True, null=True, verbose_name=_(""))
+    lent_date = models.DateTimeField(blank=True, null=True)
 
 class Species(models.Model):
     code = models.CharField(max_length=10, blank=True, null=True, verbose_name=_(""))
