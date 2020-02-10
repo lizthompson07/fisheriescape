89698ef4dfd4ac311cffa835809830145c9fce46
Thompson <THOMPSONE@dfo-mpo.ca>
2020-02-10 10:47:02 -0400

    Added Items filter
    Starting a sub-app for Inventory tracking

--- vault/filters.py
+++ vault/filters.py
@@ -4,10 +4,12 @@ from django import forms
 
 #
 class SpeciesFilter(django_filters.FilterSet):
-    search_term = django_filters.CharFilter(field_name='search_term', label="Species (any part of name...)",
+    search_term = django_filters.CharFilter(field_name='search_term', label="Items (any part of name...)",
                                             lookup_expr='icontains', widget=forms.TextInput())
 
-
+class ItemsFilter(django_filters.FilterSet):
+    search_term = django_filters.CharFilter(field_name='search_term', label="Items (any part of name...)",
+                                            lookup_expr='icontains', widget=forms.TextInput())
 # class PredatorFilter(django_filters.FilterSet):
 #     search_term = django_filters.CharFilter(field_name='search_term', label="Species (any part of name...)",
 #                                             lookup_expr='icontains', widget=forms.TextInput())
