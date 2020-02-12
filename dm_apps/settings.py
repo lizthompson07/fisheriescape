ece1205925314fe716144c864e7018ef4005af6d
david fishman <davjfish@gmail.com>
2020-02-11 22:06:17 -0400

    prep for cloud

--- dm_apps/settings.py
+++ dm_apps/settings.py
@@ -189,7 +189,7 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = STATIC_DIR
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
