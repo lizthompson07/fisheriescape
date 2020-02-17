2ef6f50e6f9e15315c502b36fd0398c469c1e9d5
david fishman <davjfish@gmail.com>
2020-02-17 14:56:04 -0400

    pipelines;

--- create_oauth_file.py
+++ create_oauth_file.py
@@ -11,8 +11,8 @@ args = parser.parse_args()
 
 new_file = os.path.join("oauth_settings.yml")
 with open(new_file, 'w') as write_file:
-    write_file.write('app_id: {}\n'.format(nz(args.app_id, "")))
-    write_file.write('app_secret: {}\n'.format(nz(args.app_secret, "")))
+    write_file.write('app_id: "{}"\n'.format(nz(args.app_id, "")))
+    write_file.write('app_secret: "{}"\n'.format(nz(args.app_secret, "")))
     write_file.write('redirect: "https://dmapps-dev.azurewebsites.net/accounts/callback"\n')
     write_file.write('scopes: "openid user.read"\n')
     write_file.write('authority: "https://login.microsoftonline.com/1594fdae-a1d9-4405-915d-011467234338"\n')
