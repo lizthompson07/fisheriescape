4020eebda04baa9b346f1dd791b9fcd3f4328495
david fishman <davjfish@gmail.com>
2020-02-17 14:15:27 -0400

    pipelines;

--- create_oauth_file.py
+++ create_oauth_file.py
@@ -1,14 +1,20 @@
 import os
-import sys
+import argparse
+
+from lib.templatetags.custom_filters import nz
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--app-id', help='application id issued by AAD')
+parser.add_argument('--app-secret', help='application secret issued by AAD')
+args = parser.parse_args()
+
 
 new_file = os.path.join("oauth_settings.yml")
 with open(new_file, 'w') as write_file:
-    write_file.write('app_id: "{}"\n'.format(sys.argv[1]))
-    write_file.write('app_secret: "{}"\n'.format(sys.argv[2]))
+    write_file.write('app_id: {}\n'.format(nz(args.app_id, "")))
+    write_file.write('app_secret: {}\n'.format(nz(args.app_secret, "")))
     write_file.write('redirect: "https://dmapps-dev.azurewebsites.net/accounts/callback"\n')
     write_file.write('scopes: "openid user.read"\n')
     write_file.write('authority: "https://login.microsoftonline.com/1594fdae-a1d9-4405-915d-011467234338"\n')
     write_file.write('authorize_endpoint: "/oauth2/v2.0/authorize"\n')
     write_file.write('token_endpoint: "/oauth2/v2.0/token"\n')
-
-
