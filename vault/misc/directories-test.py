03dd7d73ad8d5f739ba6247793c9248e0b9319e3
ricardd <daniel.ricard@gmail.com>
2020-02-18 08:57:33 -0400

    adding python script to list directories

--- vault/misc/directories-test.py
+++ vault/misc/directories-test.py
@@ -1,13 +1,29 @@
+## https://mkyong.com/python/python-how-to-list-all-files-in-a-directory/
+# import os
+#
+# path = 'A:\\C&P Imagery\\'
+#
+# files = []
+# # r=root, d=directories, f = files
+# for r, d, f in os.walk(path):
+#     for file in f:
+#         if '.JPG' in file:
+#             files.append(os.path.join(r, file))
+#
+# for f in files:
+#     print(f)
+#
+
 import os
 
 path = 'A:\\C&P Imagery\\'
 
-files = []
+folders = []
+
 # r=root, d=directories, f = files
 for r, d, f in os.walk(path):
-    for file in f:
-        if '.JPG' in file:
-            files.append(os.path.join(r, file))
+    for folder in d:
+        folders.append(os.path.join(r, folder))
 
-for f in files:
+for f in folders:
     print(f)
