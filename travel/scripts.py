07e70dd8e6e4b8c23351f5503808bf4953ae9cfe
David Fishman <davjfish@gmail.com>
2020-02-18 14:41:51 -0400

    pipelines; making a small change to test out deployments

--- travel/scripts.py
+++ travel/scripts.py
@@ -1,11 +1,10 @@
 from . import models
 from . import utils
 
-def resave_all(events = models.TripRequest.objects.all()):
-    for obj in events:
-        if obj.status_id == 8:
-            obj.reviewers.all().delete()
-            utils.get_reviewers(obj)
+def resave_all():
+    conf_list = models.Conference.objects.all()
+    for obj in conf_list:
+        obj.save()
 
 
 
