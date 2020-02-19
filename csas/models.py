c37ecb3afe4486cb73328b1ace1fcb16ca0efc1d
Upson <UpsonP@dfo-mpo.ca>
2020-02-19 09:47:01 -0400

    Example lookup table

--- csas/models.py
+++ csas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from shared_models import models as shared_models
+
 
 # Create your models here.
+class CotType(models.Model):
+    cot_id = models.models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+
+    def __str__(self):
+        return "{}".format(self.name)
