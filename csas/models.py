94b5c26b828f1e16993995c8fe05790eadfef819
Upson <UpsonP@dfo-mpo.ca>
2020-02-20 15:30:42 -0400

    Added forms for contact

--- csas/models.py
+++ csas/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-#from shared_models import models as shared_models
+from django.utils.translation import gettext_lazy as _
+
+from shared_models import models as shared_models
 
 
 # Create models
 class CohHonorific(models.Model):
     coh_id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=45)
+    name = models.CharField(max_length=45, verbose_name="Honorific")
 
     def __str__(self):
         return "{}".format(self.name)
@@ -54,22 +56,22 @@ class SecSector(models.Model):
 
 class ConContact(models.Model):
     con_id = models.AutoField(primary_key=True)
-    honorific = models.ForeignKey(CohHonorific, models.DO_NOTHING)
-    first_name = models.CharField(max_length=100)
+    honorific = models.ForeignKey(CohHonorific, on_delete=models.DO_NOTHING)
+    first_name = models.CharField(max_length=100, help_text=_("Some help here"))
     last_name = models.CharField(max_length=100)
     affiliation = models.CharField(max_length=100)
     job_title = models.CharField(max_length=100)
-    language = models.IntegerField()
-    contact_type = models.ForeignKey(CotType, models.DO_NOTHING)    # should use IntegerField or ForeignKey
+    language = models.ForeignKey(LanLanguage, on_delete=models.DO_NOTHING)
+    contact_type = models.ForeignKey(CotType, on_delete=models.DO_NOTHING)    # should use IntegerField or ForeignKey
     notification_preference = models.ForeignKey(NotNotificationPreference, models.DO_NOTHING)    # should use IntegerField or ForeignKey
     phone = models.CharField(max_length=12)
     email = models.CharField(max_length=255)
-    region = models.IntegerField()    # should use IntegerField or ForeignKey, we don't have region class
-    section = models.ForeignKey(SecSector, models.DO_NOTHING)    # should use IntegerField or ForeignKey
-    role = models.ForeignKey(RolRole, models.DO_NOTHING)    # should use IntegerField or ForeignKey
+    region = models.ForeignKey(shared_models.Region, on_delete=models.DO_NOTHING)
+    section = models.ForeignKey(SecSector, on_delete=models.DO_NOTHING)    # should use IntegerField or ForeignKey
+    role = models.ForeignKey(RolRole, on_delete=models.DO_NOTHING)    # should use IntegerField or ForeignKey
     expertise = models.CharField(max_length=100)
-    cc_grad = models.IntegerField()    # what is TINYINT
-    notes = models.CharField(max_length=400)   # what is MEDIUMTEXT
+    cc_grad = models.BooleanField()    # what is TINYINT
+    notes = models.TextField()   # what is MEDIUMTEXT
 
     def __str__(self):
         return "{}".format(self.last_name)
