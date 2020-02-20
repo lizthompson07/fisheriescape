0d189afbae0a8e610de42c2fbde2787b7dfb8429
huy <yongcun.hu@dfo-mpo.gc.ca>
2020-02-20 12:51:54 -0400

    Doing contacts

--- csas/urls.py
+++ csas/urls.py
@@ -5,6 +5,7 @@ app_name = 'csas'
 
 urlpatterns = [
     path('', views.IndexTemplateView.as_view(), name="index"),
+    path('contacts/', views.ContactsTemplateView.as_view(), name="contacts"),
     # path('close/', views.CloserTemplateView.as_view(), name="close_me"),
     # path('search/', views.SearchFormView.as_view(), name="sample_search"),
     # path('dataflow/', views.DataFlowTemplateView.as_view(), name ="dataflow" ),
