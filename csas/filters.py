be9ad7e0bf28d1f5ef296e9ee43297954e933d5d
Upson <UpsonP@dfo-mpo.ca>
2020-02-25 15:51:34 -0400

    Will require a csas table drop and migration

--- csas/filters.py
+++ csas/filters.py
@@ -7,3 +7,9 @@ class ContactFilter(FilterSet):
     class Meta:
         model = models.ConContact
         fields = ['last_name', 'first_name', 'contact_type']
+
+
+class MeetingFilter(FilterSet):
+    class Meta:
+        model = models.MetMeeting
+        fields = ['quarter', 'title_en', 'title_fr']
