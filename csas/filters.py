657a438b4adbd4388e72c919eb943b2e4e5107fe
Upson <UpsonP@dfo-mpo.ca>
2020-02-25 09:00:31 -0400

    I've used my whales app to provide some simple to override tempaltes for fast form creation

---
+++ csas/filters.py
@@ -0,0 +1,9 @@
+from django_filters import FilterSet
+
+from . import models
+
+
+class ContactFilter(FilterSet):
+    class Meta:
+        model = models.ConContact
+        fields = ['last_name', 'first_name']
--- csas/urls.py
+++ csas/urls.py
@@ -5,7 +5,12 @@ app_name = 'csas'
 
 urlpatterns = [
     path('',              views.IndexTemplateView.as_view(),        name="index"),
-    path('contacts/',     views.ContactsTemplateView.as_view(),     name="contacts"),
+
+    path('contacts/',     views.ContactsList.as_view(),  name="list_con"),
+    path('contacts/new/', views.ContactsEntry.as_view(), name="create_con"),
+    path('contacts/update/<int:pk>', views.ContactsUpdate.as_view(), name="update_con"),
+    path('contacts/details/<int:pk>', views.ContactDetails.as_view(), name="details_con"),
+
     path('meetings/',     views.MeetingsTemplateView.as_view(),     name="meetings"),
     path('publications/', views.PublicationsTemplateView.as_view(), name="publications"),
     path('requests/',     views.RequestsTemplateView.as_view(),     name="requests"),
