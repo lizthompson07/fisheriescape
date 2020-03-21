c05635dcb65677caa762090634863c62ad3d98e1
david fishman <davjfish@gmail.com>
2020-03-21 06:51:26 -0300

    trying to address azure blob permissions issue

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -1,20 +1,21 @@
-from django.conf import settings
+from decouple import config
 from msrestazure.azure_active_directory import MSIAuthentication
 from storages.backends.azure_storage import AzureStorage
 
-token_credential = MSIAuthentication(resource=f'https://{settings.AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net')
+AZURE_STORAGE_ACCOUNT_NAME = config("AZURE_STORAGE_ACCOUNT_NAME", cast=str, default="")
+token_credential = MSIAuthentication(resource=f'https://{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net')
 
 
 # from https://medium.com/@DawlysD/django-using-azure-blob-storage-to-handle-static-media-assets-from-scratch-90cbbc7d56be
 class AzureMediaStorage(AzureStorage):
-    account_name = settings.AZURE_STORAGE_ACCOUNT_NAME
+    account_name = AZURE_STORAGE_ACCOUNT_NAME
     token_credential = token_credential
     azure_container = 'media'
     expiration_secs = None
 
 
 class AzureStaticStorage(AzureStorage):
-    account_name = settings.AZURE_STORAGE_ACCOUNT_NAME
+    account_name = AZURE_STORAGE_ACCOUNT_NAME
     token_credential = token_credential
     azure_container = 'static'
     expiration_secs = None
