0a4d9bbf0eae2cfe6211ece5c7d276e66b42c9a7
David Fishman <davjfish@gmail.com>
2020-03-22 23:09:33 -0300

    spring cleanup build

--- spring_cleanup/urls.py
+++ spring_cleanup/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexTemplateView.as_view(), name="index"),
+    path('route/<int:pk>/view/', views.RouteDetailView.as_view(), name="route_detail"),
 
 ]
 app_name = 'spring_cleanup'
