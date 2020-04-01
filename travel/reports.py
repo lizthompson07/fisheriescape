efacae55ffae9e0f6b13ef71f6f3d0c34e499820
Upson <UpsonP@dfo-mpo.ca>
2020-04-01 15:38:20 -0300

    Moved role of participant field to Purpose column #356

--- travel/reports.py
+++ travel/reports.py
@@ -138,10 +138,7 @@ def generate_cfts_spreadsheet(fiscal_year=None, trip_request=None, trip=None):
             else:
                 my_purpose = tr.purpose_long_text
 
-            my_role = "{} - {}".format(
-                nz(tr.role, "MISSING"),
-                nz(tr.role_of_participant, "No description provided")
-            )
+            my_role = "{}".format(nz(tr.role, "MISSING"),)
 
             my_name = "{}, {}".format(tr.last_name, tr.first_name)
             if tr.is_research_scientist:
