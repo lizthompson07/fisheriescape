d067b69383d3d8353efed506db527dc8820c91e3
David Fishman <davjfish@gmail.com>
2020-04-06 16:33:02 -0300

    spring_cleanup build and modification to allowed hosts for stagging url on azure

--- spring_cleanup/forms.py
+++ spring_cleanup/forms.py
@@ -11,3 +11,16 @@ attr_fp_date = {"class": "fp-date", "placeholder": "Click to select a date.."}
 multi_select_js = {"class": "multi-select"}
 chosen_js = {"class": "chosen-select-contains"}
 
+
+class OutingForm(forms.ModelForm):
+    class Meta:
+        model = models.Outing
+        exclude = [
+            'route',
+            'fiscal_year',
+        ]
+        widgets = {
+            'date': forms.TextInput(attrs=attr_fp_date),
+            'birds': forms.SelectMultiple(attrs=chosen_js),
+            'users': forms.SelectMultiple(attrs=chosen_js),
+        }
