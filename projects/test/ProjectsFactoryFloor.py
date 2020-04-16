201c454089ac02b8b0de998e5b0bed8e01e9bfcb
David Fishman <davjfish@gmail.com>
2020-04-16 17:48:16 -0300

    changes to projects tests to allow to run without having to load fixtures first

--- projects/test/ProjectsFactoryFloor.py
+++ projects/test/ProjectsFactoryFloor.py
@@ -3,20 +3,24 @@ import datetime
 from faker import Factory
 from django.utils import timezone
 
-from django.contrib.auth import models as django_models
-
 from projects import models
 from shared_models.test.SharedModelsFactoryFloor import UserFactory, SectionFactory
 
 faker = Factory.create()
 
 
-
-
 class ActivityFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.ActivityType
 
+
+class FundingSourceFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.FundingSource
+
+    name = factory.lazy_attribute(lambda o: faker.word())
+    funding_source_type = factory.lazy_attribute(
+        lambda o: models.FundingSourceType.objects.all()[faker.random_int(0, models.FundingSourceType.objects.count() - 1)])
 
 
 class StaffFactory(factory.django.DjangoModelFactory):
@@ -27,11 +31,11 @@ class StaffFactory(factory.django.DjangoModelFactory):
 
 
 class IndeterminateStaffFactory(StaffFactory):
-    employee_type = models.EmployeeType.objects.get(pk=1)
+    employee_type = factory.lazy_attribute(lambda o: models.EmployeeType.objects.get(pk=1))
 
 
 class StudentStaffFactory(StaffFactory):
-    employee_type = models.EmployeeType.objects.get(pk=4)
+    employee_type = factory.lazy_attribute(lambda o: models.EmployeeType.objects.get(pk=4))
 
 
 class OMCostFactory(factory.django.DjangoModelFactory):
@@ -39,6 +43,7 @@ class OMCostFactory(factory.django.DjangoModelFactory):
         model = models.OMCost
 
     budget_requested = factory.lazy_attribute(lambda o: faker.random_int(100, 1000))
+    funding_source = factory.SubFactory(FundingSourceFactory)
 
 
 class OMCostTravelFactory(OMCostFactory):
@@ -50,11 +55,11 @@ class OMCostEquipmentFacotry(OMCostFactory):
 
 
 class ProjectFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = models.Project
 
     section = factory.SubFactory(SectionFactory)
-    activity_type = factory.lazy_attribute(lambda o: models.ActivityType.objects.all()[faker.random_int(0, models.ActivityType.objects.count() - 1)])
+    activity_type = factory.lazy_attribute(
+        lambda o: models.ActivityType.objects.all()[faker.random_int(0, models.ActivityType.objects.count() - 1)])
     start_date = factory.lazy_attribute(lambda o: faker.date_time(tzinfo=timezone.get_current_timezone()))
     end_date = factory.lazy_attribute(lambda o: o.start_date + datetime.timedelta(days=faker.random_int(1, 365)))
