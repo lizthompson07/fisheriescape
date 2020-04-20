38e5ac21daf4decf701f24dc28d1dec1032c1e9a
david fishman <davjfish@gmail.com>
2020-04-20 12:17:02 -0300

    #370

--- travel/test/test_list_views.py
+++ travel/test/test_list_views.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from django.utils.translation import activate
 from django.urls import reverse_lazy
 from django.test import tag
+from django_filters.views import FilterView
 
 from travel.test import TravelFactoryFloor as FactoryFloor
 from travel.test.TravelFactoryFloor import IndividualTripRequestFactory, TripFactory
@@ -10,29 +11,28 @@ from .. import views
 from travel.test.common_tests import CommonTravelTest
 
 
-class IndividualTripRequestDelete(CommonTravelTest):
+class TripRequestListView(CommonTravelTest):
     def setUp(self):
         super().setUp()
         self.tr = IndividualTripRequestFactory()
-        self.test_url = reverse_lazy('travel:request_delete', kwargs={"pk": self.tr.pk})
-        self.expected_template = 'travel/trip_request_confirm_delete.html'
+        self.test_url = reverse_lazy('travel:request_list')
+        self.expected_template = 'travel/trip_request_list.html'
 
-    @tag("trip_request", 'delete', "view")
+    @tag("trip_request", 'list', "view")
     def test_view_class(self):
         # make sure the view is inheriting from CanModify Mixin
-        self.assert_inheritance(views.TripRequestDeleteView, views.CanModifyMixin)
+        self.assert_inheritance(views.TripRequestListView, FilterView)
 
-    @tag("trip_request", 'delete', "access")
+    @tag("trip_request", 'list', "access")
     def test_view(self):
         self.assert_not_broken(self.test_url)
         # create an admin user (who should always be able to delete) and check to see there is a 200 response
-        admin_user = self.get_and_login_user(in_group="travel_admin")
-        self.assert_login_required_view(test_url=self.test_url, expected_template=self.expected_template, user=admin_user)
+        self.assert_non_public_view(test_url=self.test_url, expected_template=self.expected_template)
 
-    @tag("trip_request", 'delete', "submit")
-    def test_submit(self):
-        # use an admin user because they should always be able to delete
-        admin_user = self.get_and_login_user(in_group="travel_admin")
-        self.assert_success_url(self.test_url)
-        # ensure the user is deleted
-        self.assertEqual(models.TripRequest.objects.filter(pk=self.tr.pk).count(), 0)
+    # Test that the context contains the proper vars
+    @tag("trip_request", 'list', "context")
+    def test_context(self):
+        context_vars = [
+            "field_list",
+        ]
+        self.assert_presence_of_context_vars(self.test_url, context_vars)
