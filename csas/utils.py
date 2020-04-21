748c2af53011fdf4153232d6717ecba4abb56ef1
Upson <UpsonP@dfo-mpo.ca>
2020-04-21 11:21:39 -0300

    Added csas_user vs csas_admin distinction

--- csas/utils.py
+++ csas/utils.py
@@ -1,2 +1,7 @@
 def csas_authorized(user):
+    return user.is_authenticated and (user.groups.filter(name='csas_user').exists() or
+                                      csas_admin(user))
+
+
+def csas_admin(user):
     return user.is_authenticated and user.groups.filter(name='csas_admin').exists()
