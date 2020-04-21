d365d01b64ea0cb158034c51640c2d8460043899
David Fishman <davjfish@gmail.com>
2020-04-21 10:04:37 -0300

    #375

--- inventory/test/todo.py
+++
@@ -1,6 +0,0 @@
-# TODO: warnings should be sent when trip costs mount above 10000 CAD
-# TODO: trip requests must always have a fiscal year after saving
-# TODO: make sure that the correct permissions are enforced with respect to editing a trip request
-# TODO: make sure that the correct permissions are enforced with respect to editing a trip (both for adm and non-adm)
-
-# TODO: test to see if the correct views are inheriting from the correct mixin classes. I do not know the best way to do this.
