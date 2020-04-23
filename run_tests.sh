08fe533c961077f936c83bd4789498a39d63a38d
David Fishman <davjfish@gmail.com>
2020-04-23 00:04:48 -0300

    CI script for bash

--- run_tests.sh
+++ run_tests.sh
@@ -3,7 +3,9 @@ echo "RUNNING TESTS FOR: shared_models app"
 python manage.py test --keepdb shared_models.test
 
 echo "RUNNING TESTS FOR: projects app"
-mkdir ./projects/temp
+mkdir ./media
+mkdir ./media/projects
+mkdir ./media/projects/temp
 python manage.py test --keepdb projects.test
 
 echo "RUNNING TESTS FOR: travel app"
