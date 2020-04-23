131a1958c5fcff43d4bc519805e769e8dda37ce4
David Fishman <davjfish@gmail.com>
2020-04-22 22:12:11 -0300

    #363

--- shiny/forms.py
+++ shiny/forms.py
@@ -11,7 +11,7 @@ multi_select_js = {"class": "multi-select"}
 class AppForm(forms.ModelForm):
     class Meta:
         model = models.App
-        fields = "__all__"
+        exclude = ["last_modified",]
         widgets = {
-            "owner": forms.HiddenInput(),
+            "owner": forms.Select(attrs=chosen_js),
         }
