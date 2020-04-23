bb69e5395a347c830e7f72d6fc9db98e809eb2f5
David Fishman <davjfish@gmail.com>
2020-04-23 09:20:32 -0300

    #363

--- run_tests.sh
+++ run_tests.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
+echo "\n"
 echo "RUNNING TESTS FOR: shared_models app"
 python manage.py test --keepdb shared_models.test
 
+echo "\n"
 echo "RUNNING TESTS FOR: projects app"
 mkdir ./media
 mkdir ./media/projects
 mkdir ./media/projects/temp
 python manage.py test --keepdb projects.test
 
+echo "\n"
 echo "RUNNING TESTS FOR: travel app"
 python manage.py test --keepdb travel.test
 
+echo "\n"
 echo "RUNNING TESTS FOR: whalesdb"
 python manage.py test --keepdb whalesdb.test
 
+echo "\n"
 echo "RUNNING TESTS FOR: inventory"
 python manage.py test --keepdb inventory.test
+
+echo "\n"
+echo "RUNNING TESTS FOR: shiny"
+python manage.py test --keepdb shiny.test
---
+++ shiny/test/test_update_views.py
@@ -0,0 +1,35 @@
+from django.urls import reverse_lazy
+from django.test import tag
+from django.views.generic import CreateView, UpdateView
+
+from shiny.test import FactoryFloor
+from shiny.test.common_tests import CommonShinyTest as CommonTest
+from .. import views
+
+class TestAppUpdateView(CommonTest):
+    def setUp(self):
+        super().setUp()
+        self.instance = FactoryFloor.AppFactory()
+        self.test_url = reverse_lazy('shiny:update', kwargs={"pk":self.instance.pk})
+        self.expected_template = 'shiny/app_form.html'
+
+    @tag("shiny", 'update', "view")
+    def test_view_class(self):
+        self.assert_inheritance(views.AppUpdateView, UpdateView)
+
+    @tag("shiny", 'update', "access")
+    def test_view(self):
+        self.assert_not_broken(self.test_url)
+        self.assert_non_public_view(test_url=self.test_url, expected_template=self.expected_template)
+
+    @tag("shiny", 'update', "context")
+    def test_context(self):
+        context_vars = [
+            "field_list",
+        ]
+        self.assert_presence_of_context_vars(self.test_url, context_vars)
+
+    @tag("shiny", 'update', "submit")
+    def test_submit(self):
+        data = FactoryFloor.AppFactory.get_valid_data()
+        self.assert_success_url(self.test_url, data=data)
