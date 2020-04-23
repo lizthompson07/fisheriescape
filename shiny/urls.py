6a587459cc5a0d4bc2523c1c89a52a2de8355c79
David Fishman <davjfish@gmail.com>
2020-04-22 21:30:41 -0300

    #363

--- shiny/urls.py
+++ shiny/urls.py
@@ -5,5 +5,8 @@ app_name = 'shiny'
 
 urlpatterns = [
     path('', views.IndexTemplateView.as_view(), name="index"),
+    path('app/<int:pk>/create/', views.AppCreateView.as_view(), name="create"),
+    path('app/<int:pk>/edit/', views.AppUpdateView.as_view(), name="update"),
+    path('app/<int:pk>/delete/', views.AppDeleteView.as_view(), name="delete"),
 
 ]
