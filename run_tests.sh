dfa9713c4a474370b0a62ad5cb907b3204ed0c6a
David Fishman <davjfish@gmail.com>
2020-04-22 23:48:18 -0300

    CI script for bash

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-echo "\n\nRUNNING TESTS FOR: shared_models app"
+echo "RUNNING TESTS FOR: shared_models app"
 python manage.py test --keepdb shared_models.test
 
-echo "\n\nRUNNING TESTS FOR: projects app"
+echo "RUNNING TESTS FOR: projects app"
+mkdir ./projects/temp
 python manage.py test --keepdb projects.test
 
-echo "\n\nRUNNING TESTS FOR: travel app"
+echo "RUNNING TESTS FOR: travel app"
 python manage.py test --keepdb travel.test
 
-echo "\n\nRUNNING TESTS FOR: whalesdb"
+echo "RUNNING TESTS FOR: whalesdb"
 python manage.py test --keepdb whalesdb.test
 
-echo "\n\nRUNNING TESTS FOR: inventory"
+echo "RUNNING TESTS FOR: inventory"
 python manage.py test --keepdb inventory.test
