e379330670140b9d9d6caa532f3b87bedb77bbc6
David Fishman <davjfish@gmail.com>
2020-04-24 11:20:24 -0300

    open data wordle

--- run_tests.sh
+++ run_tests.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-echo "\n"
+printf "\n"
 echo "RUNNING TESTS FOR: shared_models app"
 python manage.py test --keepdb shared_models.test
 
-echo "\n"
+printf "\n"
 echo "RUNNING TESTS FOR: projects app"
 mkdir ./media
 mkdir ./media/projects
 mkdir ./media/projects/temp
 python manage.py test --keepdb projects.test
 
-echo "\n"
+printf "\n"
 echo "RUNNING TESTS FOR: travel app"
 python manage.py test --keepdb travel.test
 
-echo "\n"
+printf "\n"
 echo "RUNNING TESTS FOR: whalesdb"
 python manage.py test --keepdb whalesdb.test
 
-echo "\n"
+printf "\n"
 echo "RUNNING TESTS FOR: inventory"
 python manage.py test --keepdb inventory.test
 
-echo "\n"
+printf "\n"
 echo "RUNNING TESTS FOR: shiny"
 python manage.py test --keepdb shiny.test
