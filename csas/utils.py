f7ec50e371de8522143a8818c657c17cd6b12a8f
Hu <HuY@dfo-mpo.ca>
2020-04-28 10:54:31 -0300

    after migration, April 28

--- csas/utils.py
+++ csas/utils.py
@@ -1,6 +1,6 @@
 # Staff_Users group who can input New Request and New Contact
 def csas_authorized(user):
-    return user.is_authenticated and (user.groups.filter(name='Staff_Users').exists() or csas_admin(user))
+    return user.is_authenticated and (user.groups.filter(name='csas_users').exists() or csas_admin(user))
 
 
 # csas_admin group who can input New Request, New Contact, New Meeting and New Publication
