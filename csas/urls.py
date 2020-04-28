d2bef56fe4632dc50ce8cfb5c76e8ff7a3281559
Hu <HuY@dfo-mpo.ca>
2020-04-28 09:54:32 -0300

    before merge, April 28

--- csas/urls.py
+++ csas/urls.py
@@ -27,11 +27,11 @@ urlpatterns = [
     path('publication/details/<int:pk>/',       views.PublicationDetails.as_view(), name="details_pub"),
 
     # for Contacts
-    path('contacts/',                           views.ContactsList.as_view(),       name="list_con"),
-    path('contacts/new/',                       views.ContactsEntry.as_view(),      name="create_con"),
-    path('contacts/update/<int:pk>/<str:pop>/', views.ContactsUpdate.as_view(),     name="update_con"),
-    path('contacts/update/<int:pk>/',           views.ContactsUpdate.as_view(),     name="update_con"),
-    path('contacts/details/<int:pk>/',          views.ContactsDetails.as_view(),    name="details_con"),
+    path('contacts/',                           views.ContactList.as_view(),       name="list_con"),
+    path('contacts/new/',                       views.ContactEntry.as_view(),      name="create_con"),
+    path('contacts/update/<int:pk>/<str:pop>/', views.ContactUpdate.as_view(),     name="update_con"),
+    path('contacts/update/<int:pk>/',           views.ContactUpdate.as_view(),     name="update_con"),
+    path('contacts/details/<int:pk>/',          views.ContactDetails.as_view(),    name="details_con"),
 
     path('close/',                              views.CloserTemplateView.as_view(), name="close_me"),
     path('create/honorific/',                   views.HonorificView.as_view(),      name="create_coh"),
