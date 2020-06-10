c708135c85f09e9f94e2400f2745c5369dca5021
Hu <HuY@dfo-mpo.ca>
2020-06-10 11:36:15 -0300

    before pull origin csas from github, Patrick just did a push

--- csas/scripts/table_load.py
+++ csas/scripts/table_load.py
@@ -1,4 +1,5 @@
 import csv
+import string
 import MySQLdb
 
 mydb = MySQLdb.connect(host='localhost',
@@ -8,32 +9,35 @@ mydb = MySQLdb.connect(host='localhost',
 
 cursor = mydb.cursor()
 
-csv_data = csv.reader(file('contacts.csv'))
+csv_file = open('C:\DFO-MPO\DMApps\dm_apps\csas\scripts\contacts.csv')
+csv_data = csv.reader(csv_file, delimiter=',')
 
+print(' -- Inserting data into CSAS table CONCONTACT ............')
 for row in csv_data:
 
-    cursor.execute('INSERT INTO testcsv(con_id, \
-                   first_name, \
-                   last_name, \
-                   affiliation, \
-                   job_title, \
-                   phone, \
-                   email, \
-                   expertise, \
-                   cc_grad, \
-                   notes, \
-                   contact_type_id, \
-                   honorific_id, \
-                   language_id, \
-                   notification_preference_id, \
-                   region_id, \
-                   role_id, \
-                   sector_id )' \
-                   'VALUES("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s",'
-                   ' "%s", "%s", "%s", "%s", "%s", "%s", "%s")',
-                   row)
+    # remove all the spaces in the list
+    row = [elem.strip(' ') for elem in row]
+
+    cursor.execute("INSERT INTO csas_concontact(id, \
+                                                first_name, \
+                                                last_name, \
+                                                affiliation, \
+                                                job_title, \
+                                                phone, \
+                                                email, \
+                                                expertise, \
+                                                cc_grad, \
+                                                notes, \
+                                                contact_type_id, \
+                                                honorific_id, \
+                                                language_id, \
+                                                notification_preference_id, \
+                                                region_id, \
+                                                role_id, \
+                                                sector_id) \
+                    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", row)
 
 # close the connection to the database.
 mydb.commit()
 cursor.close()
-print("Done")
+print(" -- Done")
