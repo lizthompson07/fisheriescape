1943b1b2301cad6cc922b45a853a6d1dbe75479a
David Fishman <davjfish@gmail.com>
2020-06-17 09:07:47 -0300

    addding uuid to shared models in preparation for migration to cloud

--- shared_models/models.py
+++ shared_models/models.py
@@ -351,7 +351,7 @@ class Vessel(models.Model):
 # snowcrab
 class Cruise(models.Model):
     institute = models.ForeignKey(Institute, on_delete=models.DO_NOTHING, blank=True, null=True)
-    mission_number = models.CharField(max_length=255, unique=True)
+    mission_number = models.CharField(max_length=255)  # once we are sure the data is clean, this should be set to `unique`
     mission_name = models.CharField(max_length=255)
     description = models.CharField(max_length=255, null=True, blank=True)
     chief_scientist = models.CharField(max_length=255)
