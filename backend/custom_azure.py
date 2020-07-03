5277e79524952ff193af62b3c771b79ac79ecbf3
david fishman <davjfish@gmail.com>
2020-07-02 22:41:18 -0300

    docker

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -9,7 +9,8 @@ if IN_PIPELINE:
     print("we are in a pipeline :)")
     account_key = config("AZURE_STORAGE_SECRET_KEY", cast=str, default="")
 else:
-    token_credential = MSIAuthentication(resource=f'https://{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net')
+    account_key = config("AZURE_STORAGE_SECRET_KEY", cast=str, default="")
+    # token_credential = MSIAuthentication(resource=f'https://{AZURE_STORAGE_ACCOUNT_NAME}.blob.core.windows.net')
 
 
 # from https://medium.com/@DawlysD/django-using-azure-blob-storage-to-handle-static-media-assets-from-scratch-90cbbc7d56be
@@ -18,7 +19,8 @@ class AzureMediaStorage(AzureStorage):
     if IN_PIPELINE:
         account_key = account_key
     else:
-        token_credential = token_credential
+        account_key = account_key
+        # token_credential = token_credential
 
     azure_container = 'media'
     expiration_secs = None
@@ -29,6 +31,7 @@ class AzureStaticStorage(AzureStorage):
     if IN_PIPELINE:
         account_key = account_key
     else:
-        token_credential = token_credential
+        account_key = account_key
+        # token_credential = token_credential
     azure_container = 'static'
     expiration_secs = None
