20139d0c253989a3b2bb5396452bae82f1689ffd
david fishman <davjfish@gmail.com>
2020-07-03 20:42:01 -0300

    docker

--- run_tests.sh
+++ run_tests.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 printf "\n"
 echo "RUNNING TESTS FOR: shared_models app"
-python manage.py test --keepdb shared_models.test -v 0
+python manage.py test -v0 shared_models.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: projects app"
 mkdir ./media
 mkdir ./media/projects
 mkdir ./media/projects/temp
-python manage.py test --keepdb projects.test -v 0
+python manage.py test -v0 projects.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: travel app"
-python manage.py test --keepdb travel.test -v 0
+python manage.py test -v0 travel.test -v 0
 
 printf "\n"
 echo "RUNNING TESTS FOR: whalesdb"
-python manage.py test --keepdb whalesdb.test -v 0
+python manage.py test -v0 whalesdb.test -v 0
 
 printf "\n"
 echo "RUNNING TESTS FOR: inventory"
-python manage.py test --keepdb inventory.test -v 0
+python manage.py test -v0 inventory.test -v 0
 
 printf "\n"
 echo "RUNNING TESTS FOR: shiny"
-python manage.py test --keepdb shiny.test -v 0
+python manage.py test -v0 shiny.test -v 0
