046a121a70b42b6a0d965c9462e48e3fc465351b
Tyler Hernandez <58124139+tynando@users.noreply.github.com>
2020-08-17 16:08:42 -0400

    Wrote rough home page and configured urls and views

--- engagements/admin.py
+++ engagements/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("legal_name",)}
+
+
+@admin.register(models.Individual)
+class ContactAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("first_name", "last_name")}
--- engagements/views.py
+++ engagements/views.py
@@ -1,1 +1,9 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+from . import models
+
+def engagements_home(request):
+    return render(request, 'engagements/engagements_home.html', {'nbar': 'home'})
+
