a07dfad62b3a7e21288779bfeed41153218d1673
Upson <UpsonP@dfo-mpo.ca>
2020-08-25 09:24:37 -0300

    fixed up the contact and requests load scripts

--- csas/scripts/table_load_requests.py
+++ csas/scripts/table_load_requests.py
@@ -1,14 +1,14 @@
 import csv
-import MySQLdb
+import os
 
-mydb = MySQLdb.connect(host='localhost',
-                       user='root',
-                       passwd='Bio+13579',
-                       db='dmapps_dev')
+from csas.models import ReqRequest
 
-cursor = mydb.cursor()
+# data file name
+file_name = 'requests.csv'
+# The data file is expected to be in the [app]/scripts/data folder
+file_path = os.path.dirname(os.path.realpath(__file__)) + os.path.sep + "data" + os.path.sep + file_name
 
-csv_file = open('C:\DFO-MPO\DMApps\dm_apps\csas\scripts\/requests.csv')
+csv_file = open(file_path)
 csv_data = csv.reader(csv_file, delimiter=',')
 
 print(' -- Inserting data into CSAS table REQREQUEST ............')
@@ -17,30 +17,31 @@ for row in csv_data:
     # remove all the spaces in the list
     row = [elem.strip(' ') for elem in row]
 
-    cursor.execute("INSERT INTO csas_reqrequest(id, \
-                                                assigned_req_id, \
-                                                title, \
-                                                in_year_request, \
-                                                client_name, \
-                                                client_title, \
-                                                client_email, \
-                                                issue, \
-                                                rationale, \
-                                                rationale_for_timing, \
-                                                funding, \
-                                                funding_notes, \
-                                                science_discussion, \
-                                                science_discussion_notes, \
-                                                adviser_submission, \
-                                                rd_submission, \
-                                                decision_date, \
-                                                client_sector_id, \
-                                                priority_id, \
-                                                proposed_timing_id, \
-                                                region_id) \
-                    VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", row)
+    # I'm going to assume the title for a request is suppose to be unique
+    if not ReqRequest.objects.filter(title=row[1]):
+        print("Inserting request {}".format(row[1]))
+        ReqRequest(assigned_req_id=row[1],
+                   title=row[2],
+                   in_year_request=row[3],
+                   client_name=row[4],
+                   client_title=row[5],
+                   client_email=row[6],
+                   issue=row[7],
+                   rationale=row[8],
+                   rationale_for_timing=row[9],
+                   funding=row[10],
+                   funding_notes=row[11],
+                   science_discussion=row[12],
+                   science_discussion_notes=row[13],
+                   adviser_submission=row[14],
+                   rd_submission=row[15],
+                   decision_date=row[16],
+                   client_sector_id=row[17],
+                   priority_id=row[18],
+                   proposed_timing_id=row[19],
+                   region_id=row[20]
+                   ).save()
+
 
 # close the connection to the database.
-mydb.commit()
-cursor.close()
 print(" -- Done")
