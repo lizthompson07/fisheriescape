d58eee6680cddb78e912a7ca0c275e44d8c68807
Upson <UpsonP@dfo-mpo.ca>
2020-10-08 07:14:15 -0300

    simplified mixin tests

--- whalesdb/test/test_mixins.py
+++ whalesdb/test/test_mixins.py
@@ -4,27 +4,28 @@ from shared_models import models as shared_models
 from django.test import TestCase, tag
 
 
-class CommonMixinsTest(TestCase):
+class CommonMixinsTest(object):
     mixin = None
     expected_key = None
     expected_title = None
     expected_model = None
     expected_form = None
 
-    def assertKeyExists(self):
+    def test_key_exists(self):
         self.assertEqual(self.expected_key, self.mixin.key)
 
-    def assertTitleExists(self):
+    def test_correct_title(self):
         self.assertEqual(self.expected_title, self.mixin.title)
 
-    def assertCorrectModel(self):
+    def test_correct_model(self):
         self.assertEqual(self.expected_model, self.mixin.model)
 
-    def assertCorrectForm(self):
+    def test_correct_form(self):
         self.assertEqual(self.expected_form, self.mixin.form_class)
 
 
-class TestCru(CommonMixinsTest):
+@tag('cru', 'mixin')
+class TestCru(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.CruMixin
@@ -33,24 +34,9 @@ class TestCru(CommonMixinsTest):
         self.expected_model = shared_models.Cruise
         self.expected_form = forms.CruForm
 
-    @tag('cru', 'mixin', 'key')
-    def test_cru_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('cru', 'mixin', 'title')
-    def test_cru_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('cru', 'mixin', 'model')
-    def test_cru_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('cru', 'mixin', 'form')
-    def test_cru_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestDep(CommonMixinsTest):
+@tag('dep', 'mixin')
+class TestDep(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.DepMixin
@@ -59,24 +45,9 @@ class TestDep(CommonMixinsTest):
         self.expected_model = models.DepDeployment
         self.expected_form = forms.DepForm
 
-    @tag('dep', 'mixin', 'key')
-    def test_dep_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('dep', 'mixin', 'title')
-    def test_dep_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('dep', 'mixin', 'model')
-    def test_dep_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('dep', 'mixin', 'form')
-    def test_dep_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEcc(CommonMixinsTest):
+@tag('ecc', 'mixin')
+class TestEcc(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EccMixin
@@ -85,24 +56,9 @@ class TestEcc(CommonMixinsTest):
         self.expected_model = models.EccCalibrationValue
         self.expected_form = forms.EccForm
 
-    @tag('ecc', 'mixin', 'key')
-    def test_ecc_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('ecc', 'mixin', 'title')
-    def test_ecc_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('ecc', 'mixin', 'model')
-    def test_ecc_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('ecc', 'mixin', 'form')
-    def test_ecc_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEca(CommonMixinsTest):
+@tag('eca', 'mixin')
+class TestEca(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EcaMixin
@@ -111,24 +67,9 @@ class TestEca(CommonMixinsTest):
         self.expected_model = models.EcaCalibrationEvent
         self.expected_form = forms.EcaForm
 
-    @tag('eca', 'mixin', 'key')
-    def test_eca_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('eca', 'mixin', 'title')
-    def test_eca_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('eca', 'mixin', 'model')
-    def test_eca_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('eca', 'mixin', 'form')
-    def test_eca_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEda(CommonMixinsTest):
+@tag('eda', 'mixin')
+class TestEda(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EdaMixin
@@ -137,24 +78,9 @@ class TestEda(CommonMixinsTest):
         self.expected_model = models.EdaEquipmentAttachment
         self.expected_form = forms.EdaForm
 
-    @tag('eda', 'mixin', 'key')
-    def test_eda_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('eda', 'mixin', 'title')
-    def test_eda_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('eda', 'mixin', 'model')
-    def test_eda_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('eda', 'mixin', 'form')
-    def test_eda_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEmm(CommonMixinsTest):
+@tag('emm', 'mixin')
+class TestEmm(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EmmMixin
@@ -163,24 +89,9 @@ class TestEmm(CommonMixinsTest):
         self.expected_model = models.EmmMakeModel
         self.expected_form = forms.EmmForm
 
-    @tag('emm', 'mixin', 'key')
-    def test_emm_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('emm', 'mixin', 'title')
-    def test_emm_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('emm', 'mixin', 'model')
-    def test_emm_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('emm', 'mixin', 'form')
-    def test_emm_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEqh(CommonMixinsTest):
+@tag('eqh', 'mixin')
+class TestEqh(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EqhMixin
@@ -189,24 +100,9 @@ class TestEqh(CommonMixinsTest):
         self.expected_model = models.EqhHydrophoneProperty
         self.expected_form = forms.EqhForm
 
-    @tag('eqh', 'mixin', 'key')
-    def test_eqh_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('eqh', 'mixin', 'title')
-    def test_eqh_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('eqh', 'mixin', 'model')
-    def test_eqh_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('eqh', 'mixin', 'form')
-    def test_eqh_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEqp(CommonMixinsTest):
+@tag('eqp', 'mixin')
+class TestEqp(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EqpMixin
@@ -215,24 +111,9 @@ class TestEqp(CommonMixinsTest):
         self.expected_model = models.EqpEquipment
         self.expected_form = forms.EqpForm
 
-    @tag('eqp', 'mixin', 'key')
-    def test_eqp_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('eqp', 'mixin', 'title')
-    def test_eqp_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('eqp', 'mixin', 'model')
-    def test_eqp_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('eqp', 'mixin', 'form')
-    def test_eqp_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEqo(CommonMixinsTest):
+@tag('eqo', 'mixin')
+class TestEqo(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EqoMixin
@@ -241,24 +122,9 @@ class TestEqo(CommonMixinsTest):
         self.expected_model = models.EqoOwner
         self.expected_form = forms.EqoForm
 
-    @tag('eqo', 'mixin', 'key')
-    def test_eqo_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('eqo', 'mixin', 'title')
-    def test_eqo_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('eqo', 'mixin', 'model')
-    def test_eqo_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('eqo', 'mixin', 'form')
-    def test_eqo_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEqr(CommonMixinsTest):
+@tag('eqr', 'mixin')
+class TestEqr(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EqrMixin
@@ -267,24 +133,9 @@ class TestEqr(CommonMixinsTest):
         self.expected_model = models.EqrRecorderProperties
         self.expected_form = forms.EqrForm
 
-    @tag('eqr', 'mixin', 'key')
-    def test_eqr_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('eqr', 'mixin', 'title')
-    def test_eqr_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('eqr', 'mixin', 'model')
-    def test_eqr_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('eqr', 'mixin', 'form')
-    def test_eqr_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestEtr(CommonMixinsTest):
+@tag('etr', 'mixin')
+class TestEtr(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.EtrMixin
@@ -293,24 +144,9 @@ class TestEtr(CommonMixinsTest):
         self.expected_model = models.EtrTechnicalRepairEvent
         self.expected_form = forms.EtrForm
 
-    @tag('etr', 'mixin', 'key')
-    def test_etr_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('etr', 'mixin', 'title')
-    def test_etr_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('etr', 'mixin', 'model')
-    def test_etr_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('etr', 'mixin', 'form')
-    def test_etr_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestMor(CommonMixinsTest):
+@tag('mor', 'mixin')
+class TestMor(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.MorMixin
@@ -319,24 +155,9 @@ class TestMor(CommonMixinsTest):
         self.expected_model = models.MorMooringSetup
         self.expected_form = forms.MorForm
 
-    @tag('mor', 'mixin', 'key')
-    def test_mor_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('mor', 'mixin', 'title')
-    def test_mor_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('mor', 'mixin', 'model')
-    def test_mor_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('mor', 'mixin', 'form')
-    def test_mor_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestPrj(CommonMixinsTest):
+@tag('prj', 'mixin')
+class TestPrj(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.PrjMixin
@@ -345,24 +166,9 @@ class TestPrj(CommonMixinsTest):
         self.expected_model = models.PrjProject
         self.expected_form = forms.PrjForm
 
-    @tag('prj', 'mixin', 'key')
-    def test_prj_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('prj', 'mixin', 'title')
-    def test_prj_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('prj', 'mixin', 'model')
-    def test_prj_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('prj', 'mixin', 'form')
-    def test_prj_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestRci(CommonMixinsTest):
+@tag('rci', 'mixin')
+class TestRci(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.RciMixin
@@ -371,24 +177,9 @@ class TestRci(CommonMixinsTest):
         self.expected_model = models.RciChannelInfo
         self.expected_form = forms.RciForm
 
-    @tag('rci', 'mixin', 'key')
-    def test_rci_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('rci', 'mixin', 'title')
-    def test_rci_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('rci', 'mixin', 'model')
-    def test_rci_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('rci', 'mixin', 'form')
-    def test_rci_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestRec(CommonMixinsTest):
+@tag('rec', 'mixin')
+class TestRec(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.RecMixin
@@ -397,24 +188,9 @@ class TestRec(CommonMixinsTest):
         self.expected_model = models.RecDataset
         self.expected_form = forms.RecForm
 
-    @tag('rec', 'mixin', 'key')
-    def test_rec_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('rec', 'mixin', 'title')
-    def test_rec_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('rec', 'mixin', 'model')
-    def test_rec_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('rec', 'mixin', 'form')
-    def test_rec_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestRee(CommonMixinsTest):
+@tag('ree', 'mixin')
+class TestRee(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.ReeMixin
@@ -423,24 +199,9 @@ class TestRee(CommonMixinsTest):
         self.expected_model = models.ReeRecordingEvent
         self.expected_form = forms.ReeForm
 
-    @tag('ree', 'mixin', 'key')
-    def test_ree_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('ree', 'mixin', 'title')
-    def test_ree_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('ree', 'mixin', 'model')
-    def test_ree_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('ree', 'mixin', 'form')
-    def test_ree_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestRet(CommonMixinsTest):
+@tag('ret', 'mixin')
+class TestRet(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.RetMixin
@@ -449,24 +210,9 @@ class TestRet(CommonMixinsTest):
         self.expected_model = models.RetRecordingEventType
         self.expected_form = forms.RetForm
 
-    @tag('ret', 'mixin', 'key')
-    def test_ret_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('ret', 'mixin', 'title')
-    def test_ret_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('ret', 'mixin', 'model')
-    def test_ret_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('ret', 'mixin', 'form')
-    def test_ret_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestRsc(CommonMixinsTest):
+@tag('rsc', 'mixin')
+class TestRsc(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.RscMixin
@@ -475,24 +221,9 @@ class TestRsc(CommonMixinsTest):
         self.expected_model = models.RscRecordingSchedule
         self.expected_form = forms.RscForm
 
-    @tag('rsc', 'mixin', 'key')
-    def test_rsc_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('rsc', 'mixin', 'title')
-    def test_rsc_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('rsc', 'mixin', 'model')
-    def test_rsc_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('rsc', 'mixin', 'form')
-    def test_rsc_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestRst(CommonMixinsTest):
+@tag('rst', 'mixin')
+class TestRst(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.RstMixin
@@ -501,24 +232,9 @@ class TestRst(CommonMixinsTest):
         self.expected_model = models.RstRecordingStage
         self.expected_form = forms.RstForm
 
-    @tag('rst', 'mixin', 'key')
-    def test_rst_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('rst', 'mixin', 'title')
-    def test_rst_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('rst', 'mixin', 'model')
-    def test_rst_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('rst', 'mixin', 'form')
-    def test_rst_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestRtt(CommonMixinsTest):
+@tag('rtt', 'mixin')
+class TestRtt(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.RttMixin
@@ -527,24 +243,9 @@ class TestRtt(CommonMixinsTest):
         self.expected_model = models.RttTimezoneCode
         self.expected_form = forms.RttForm
 
-    @tag('rtt', 'mixin', 'key')
-    def test_rtt_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('rtt', 'mixin', 'title')
-    def test_rtt_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('rtt', 'mixin', 'model')
-    def test_rtt_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('rtt', 'mixin', 'form')
-    def test_rtt_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestSte(CommonMixinsTest):
+@tag('ste', 'mixin')
+class TestSte(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.SteMixin
@@ -553,24 +254,9 @@ class TestSte(CommonMixinsTest):
         self.expected_model = models.SteStationEvent
         self.expected_form = forms.SteForm
 
-    @tag('ste', 'mixin', 'key')
-    def test_ste_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('ste', 'mixin', 'title')
-    def test_ste_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('ste', 'mixin', 'model')
-    def test_ste_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('ste', 'mixin', 'form')
-    def test_ste_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestStn(CommonMixinsTest):
+@tag('stn', 'mixin')
+class TestStn(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.StnMixin
@@ -579,24 +265,9 @@ class TestStn(CommonMixinsTest):
         self.expected_model = models.StnStation
         self.expected_form = forms.StnForm
 
-    @tag('stn', 'mixin', 'key')
-    def test_stn_mixin_key(self):
-        super().assertKeyExists()
 
-    @tag('stn', 'mixin', 'title')
-    def test_stn_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('stn', 'mixin', 'model')
-    def test_stn_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('stn', 'mixin', 'form')
-    def test_stn_mixin_form(self):
-        super().assertCorrectForm()
-
-
-class TestTea(CommonMixinsTest):
+@tag('tea', 'mixin')
+class TestTea(CommonMixinsTest, TestCase):
 
     def setUp(self) -> None:
         self.mixin = mixins.TeaMixin
@@ -604,21 +275,3 @@ class TestTea(CommonMixinsTest):
         self.expected_title = "Team Member"
         self.expected_model = models.TeaTeamMember
         self.expected_form = forms.TeaForm
-
-    @tag('tea', 'mixin', 'key')
-    def test_tea_mixin_key(self):
-        super().assertKeyExists()
-
-    @tag('tea', 'mixin', 'title')
-    def test_tea_mixin_title(self):
-        super().assertTitleExists()
-
-    @tag('tea', 'mixin', 'model')
-    def test_tea_mixin_model(self):
-        super().assertCorrectModel()
-
-    @tag('tea', 'mixin', 'form')
-    def test_tea_mixin_form(self):
-        super().assertCorrectForm()
-
-
