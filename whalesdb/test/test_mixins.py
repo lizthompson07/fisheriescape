65a1f16e4cc86ab9afa1ce354e885bf1d2fa0fce
Upson <Patrick.Upson@dfo-mpo.gc.ca>
2020-10-13 13:08:47 -0300

    Fixed a translation testing issue with mixin testing

--- whalesdb/test/test_mixins.py
+++ whalesdb/test/test_mixins.py
@@ -2,6 +2,7 @@ from whalesdb import mixins, models, forms
 from shared_models import models as shared_models
 
 from django.test import TestCase, tag
+from django.utils.translation import gettext_lazy as _
 
 
 class CommonMixinsTest(object):
@@ -30,7 +31,7 @@ class TestCru(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.CruMixin
         self.expected_key = 'cru'
-        self.expected_title = "Cruise"
+        self.expected_title = _("Cruise")
         self.expected_model = shared_models.Cruise
         self.expected_form = forms.CruForm
 
@@ -41,7 +42,7 @@ class TestDep(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.DepMixin
         self.expected_key = 'dep'
-        self.expected_title = "Deployment"
+        self.expected_title = _("Deployment")
         self.expected_model = models.DepDeployment
         self.expected_form = forms.DepForm
 
@@ -52,7 +53,7 @@ class TestEcc(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EccMixin
         self.expected_key = 'ecc'
-        self.expected_title = "Calibration Values"
+        self.expected_title = _("Calibration Values")
         self.expected_model = models.EccCalibrationValue
         self.expected_form = forms.EccForm
 
@@ -63,7 +64,7 @@ class TestEca(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EcaMixin
         self.expected_key = 'eca'
-        self.expected_title = "Calibration Event"
+        self.expected_title = _("Calibration Event")
         self.expected_model = models.EcaCalibrationEvent
         self.expected_form = forms.EcaForm
 
@@ -74,7 +75,7 @@ class TestEda(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EdaMixin
         self.expected_key = 'eda'
-        self.expected_title = "Equipment Attachment"
+        self.expected_title = _("Equipment Attachment")
         self.expected_model = models.EdaEquipmentAttachment
         self.expected_form = forms.EdaForm
 
@@ -85,7 +86,7 @@ class TestEmm(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EmmMixin
         self.expected_key = 'emm'
-        self.expected_title = "Make/Model"
+        self.expected_title = _("Make/Model")
         self.expected_model = models.EmmMakeModel
         self.expected_form = forms.EmmForm
 
@@ -96,7 +97,7 @@ class TestEhe(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EheMixin
         self.expected_key = 'ehe'
-        self.expected_title = "Hydrophone Event"
+        self.expected_title = _("Hydrophone Event")
         self.expected_model = models.EheHydrophoneEvent
         self.expected_form = forms.EheForm
 
@@ -107,7 +108,7 @@ class TestEqh(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EqhMixin
         self.expected_key = 'eqh'
-        self.expected_title = "Hydrophone Properties"
+        self.expected_title = _("Hydrophone Properties")
         self.expected_model = models.EqhHydrophoneProperty
         self.expected_form = forms.EqhForm
 
@@ -118,7 +119,7 @@ class TestEqp(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EqpMixin
         self.expected_key = 'eqp'
-        self.expected_title = "Equipment"
+        self.expected_title = _("Equipment")
         self.expected_model = models.EqpEquipment
         self.expected_form = forms.EqpForm
 
@@ -129,7 +130,7 @@ class TestEqo(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EqoMixin
         self.expected_key = 'eqo'
-        self.expected_title = "Equipment Owner"
+        self.expected_title = _("Equipment Owner")
         self.expected_model = models.EqoOwner
         self.expected_form = forms.EqoForm
 
@@ -140,7 +141,7 @@ class TestEqr(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EqrMixin
         self.expected_key = 'eqr'
-        self.expected_title = "Recorder Properties"
+        self.expected_title = _("Recorder Properties")
         self.expected_model = models.EqrRecorderProperties
         self.expected_form = forms.EqrForm
 
@@ -151,7 +152,7 @@ class TestEtr(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.EtrMixin
         self.expected_key = 'etr'
-        self.expected_title = "Technical Repair Event"
+        self.expected_title = _("Technical Repair Event")
         self.expected_model = models.EtrTechnicalRepairEvent
         self.expected_form = forms.EtrForm
 
@@ -162,7 +163,7 @@ class TestMor(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.MorMixin
         self.expected_key = 'mor'
-        self.expected_title = "Mooring Setup"
+        self.expected_title = _("Mooring Setup")
         self.expected_model = models.MorMooringSetup
         self.expected_form = forms.MorForm
 
@@ -173,7 +174,7 @@ class TestPrj(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.PrjMixin
         self.expected_key = 'prj'
-        self.expected_title = "Project"
+        self.expected_title = _("Project")
         self.expected_model = models.PrjProject
         self.expected_form = forms.PrjForm
 
@@ -184,7 +185,7 @@ class TestRci(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.RciMixin
         self.expected_key = 'rci'
-        self.expected_title = "Channel Information"
+        self.expected_title = _("Channel Information")
         self.expected_model = models.RciChannelInfo
         self.expected_form = forms.RciForm
 
@@ -195,7 +196,7 @@ class TestRec(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.RecMixin
         self.expected_key = 'rec'
-        self.expected_title = "Dataset"
+        self.expected_title = _("Dataset")
         self.expected_model = models.RecDataset
         self.expected_form = forms.RecForm
 
@@ -206,7 +207,7 @@ class TestRee(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.ReeMixin
         self.expected_key = 'ree'
-        self.expected_title = "Recording Events"
+        self.expected_title = _("Recording Events")
         self.expected_model = models.ReeRecordingEvent
         self.expected_form = forms.ReeForm
 
@@ -217,7 +218,7 @@ class TestRet(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.RetMixin
         self.expected_key = 'ret'
-        self.expected_title = "Recording Event Type"
+        self.expected_title = _("Recording Event Type")
         self.expected_model = models.RetRecordingEventType
         self.expected_form = forms.RetForm
 
@@ -228,7 +229,7 @@ class TestRsc(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.RscMixin
         self.expected_key = 'rsc'
-        self.expected_title = "Recording Schedule"
+        self.expected_title = _("Recording Schedule")
         self.expected_model = models.RscRecordingSchedule
         self.expected_form = forms.RscForm
 
@@ -239,7 +240,7 @@ class TestRst(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.RstMixin
         self.expected_key = 'rst'
-        self.expected_title = "Recording Stage"
+        self.expected_title = _("Recording Stage")
         self.expected_model = models.RstRecordingStage
         self.expected_form = forms.RstForm
 
@@ -250,7 +251,7 @@ class TestRtt(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.RttMixin
         self.expected_key = 'rtt'
-        self.expected_title = "Time Zone"
+        self.expected_title = _("Time Zone")
         self.expected_model = models.RttTimezoneCode
         self.expected_form = forms.RttForm
 
@@ -261,7 +262,7 @@ class TestSte(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.SteMixin
         self.expected_key = 'ste'
-        self.expected_title = "Station Event"
+        self.expected_title = _("Station Event")
         self.expected_model = models.SteStationEvent
         self.expected_form = forms.SteForm
 
@@ -272,7 +273,7 @@ class TestStn(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.StnMixin
         self.expected_key = 'stn'
-        self.expected_title = "Station"
+        self.expected_title = _("Station")
         self.expected_model = models.StnStation
         self.expected_form = forms.StnForm
 
@@ -283,6 +284,6 @@ class TestTea(CommonMixinsTest, TestCase):
     def setUp(self) -> None:
         self.mixin = mixins.TeaMixin
         self.expected_key = 'tea'
-        self.expected_title = "Team Member"
+        self.expected_title = _("Team Member")
         self.expected_model = models.TeaTeamMember
         self.expected_form = forms.TeaForm
