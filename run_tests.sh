e89d6b234533c337245097eace2ad89bb1e43ff8
Fishman <David.Fishman@dfo-mpo.gc.ca>
2020-10-21 15:50:18 -0300

    updating run_tests.sh to python3.8 syntax

--- run_tests.sh
+++ run_tests.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 printf "\n"
 echo "RUNNING TESTS FOR: shared_models app"
-python manage.py test  shared_models.test
+python3.8 manage.py test  shared_models.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: projects app"
 mkdir ./media
 mkdir ./media/projects
 mkdir ./media/projects/temp
-python manage.py test projects.test
+python3.8 manage.py test projects.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: travel app"
-python manage.py test travel.test
+python3.8 manage.py test travel.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: whalesdb"
-python manage.py test whalesdb.test
+python3.8 manage.py test whalesdb.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: inventory"
-python manage.py test inventory.test
+python3.8 manage.py test inventory.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: shiny"
-python manage.py test shiny.test
+python3.8 manage.py test shiny.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: ihub"
-python manage.py test ihub.test
+python3.8 manage.py test ihub.test
 
 printf "\n"
 echo "RUNNING TESTS FOR: csas"
-python manage.py test csas.test
+python3.8 manage.py test csas.test
