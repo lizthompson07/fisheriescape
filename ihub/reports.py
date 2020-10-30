1e35c694847e364db7e2b431f2125c8cf10e38c2
David Fishman <davjfish@gmail.com>
2020-10-30 10:49:59 -0300

    #476

--- ihub/reports.py
+++ ihub/reports.py
@@ -102,6 +102,7 @@ def generate_capacity_spreadsheet(fy, orgs, sectors):
 
             if e.organizations.count() > 0:
                 orgs = str([str(obj) for obj in e.organizations.all()]).replace("[", "").replace("]", "").replace("'", "").replace('"',
+                                                                                                                                   "")
             else:
                 orgs = None
 
