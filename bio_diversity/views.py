5b04257a344c0a8e75c853e27fda4efea24c97fe
Patrick Upson <upsonp@gmail.com>
2020-11-25 15:04:01 -0400

    Fixed missing templates and as_view issue

--- bio_diversity/views.py
+++ bio_diversity/views.py
@@ -7,16 +7,25 @@ from . import models, forms
 
 
 class IndexTemplateView(TemplateView):
+    nav_menu = 'bio_diversity/bio_diversity_nav_menu.html'
+    site_css = 'bio_diversity/bio_diversity_css.css'
+    home_url_name = "bio_diversity:index"
+
     template_name = 'bio_diversity/index.html'
 
 
 # CommonCreate Extends the UserPassesTestMixin used to determine if a user has
 # has the correct privileges to interact with Creation Views
 class CommonCreate(CommonCreateView):
-    pass
+    nav_menu = 'bio_diversity/bio_diversity_nav.html'
+    site_css = 'bio_diversity/bio_diversity.css'
+    home_url_name = "bio_diversity:index"
 
 
 class CreateInst(CommonCreate):
     form_class = forms.InstForm
     model = models.InstDetCode
     title = "Some title"
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(**kwargs)
