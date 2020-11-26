c3d314bdc4a1bd74262abdcbbf66d78260b0fac8
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2020-11-26 15:58:26 -0400

    adding details, list views, in progress. compiles, but needs tests

---
+++ bio_diversity/filters.py
@@ -0,0 +1,27 @@
+import django
+import django_filters
+from django.utils.translation import gettext as _
+
+from . import models
+import shared_models.models as shared_models
+
+
+class InstFilter(django_filters.FilterSet):
+
+    class Meta:
+        model = models.Instrument
+        fields = ["instc", "serial_number", "comments", "created_by", "created_date",]
+
+
+class InstcFilter(django_filters.FilterSet):
+
+    class Meta:
+        model = models.InstrumentCode
+        fields = ["name", "nom", "description_en", "description_fr", "created_by", "created_date", ]
+
+
+class InstdcFilter(django_filters.FilterSet):
+
+    class Meta:
+        model = models.InstDetCode
+        fields = ["name", "nom", "description_en", "description_fr", "created_by", "created_date", ]
--- bio_diversity/views.py
+++ bio_diversity/views.py
@@ -1,8 +1,8 @@
-from django.views.generic import TemplateView
-from shared_models.views import CommonAuthCreateView
+from django.views.generic import TemplateView, DetailView
+from shared_models.views import CommonAuthCreateView, CommonAuthFilterView
 from django.urls import reverse_lazy
 
-from . import models, forms, mixins
+from . import models, forms, mixins, filters
 
 
 class IndexTemplateView(TemplateView):
@@ -15,14 +15,23 @@ class IndexTemplateView(TemplateView):
 
 # CommonCreate Extends the UserPassesTestMixin used to determine if a user has
 # has the correct privileges to interact with Creation Views
+# --------------------Create Views----------------------------------------
 class CommonCreate(CommonAuthCreateView):
 
     nav_menu = 'bio_diversity/bio_diversity_nav.html'
     site_css = 'bio_diversity/bio_diversity.css'
     home_url_name = "bio_diversity:index"
 
+    # Upon success most creation views will be redirected to their respective 'CommonList' view. To send
+    # a successful creation view somewhere else, override this method
     def get_success_url(self):
-        return reverse_lazy("shared_models:close_me_no_refresh")
+        success_url = self.success_url if self.success_url else reverse_lazy("bio_diversity:list_{}".format(self.key))
+
+        if self.kwargs.get("pop"):
+            # create views intended to be pop out windows should close the window upon success
+            success_url = reverse_lazy("shared_models:close_me_no_refresh")
+
+        return success_url
 
     # overrides the UserPassesTestMixin test to check that a user belongs to the whalesdb_admin group
     def test_func(self):
@@ -41,6 +50,139 @@ class InstdcCreate(mixins.InstdcMixin, CommonCreate):
     pass
 
 
+# ---------------------------Detail VIEWS-----------------------------------------------
+class CommonDetails(DetailView):
+    # default template to use to create a details view
+    template_name = "bio_diversity/bio_details.html"
+
+    # title to display on the list page
+    title = None
+
+    # key used for creating default list and update URLs in the get_context_data method
+    key = None
+
+    # URL linking the details page back to the proper list
+    list_url = None
+    update_url = None
+
+    # By default detail objects are editable, set to false to remove update buttons
+    editable = True
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        if self.title:
+            context['title'] = self.title
+
+        if self.fields:
+            context['fields'] = self.fields
+
+        context['list_url'] = self.list_url if self.list_url else "bio_diversity:list_{}".format(self.key)
+        context['update_url'] = None # self.update_url if self.update_url else "bio_diversity:update_{}".format(self.key)
+        # for the most part if the user is authorized then the content is editable
+        # but extending classes can choose to make content not editable even if the user is authorized
+        context['auth'] = None # utils.whales_authorized(self.request.user)
+        context['editable'] = context['auth'] and self.editable
+
+        return context
 
 
+class InstDetails(mixins.InstMixin, CommonDetails):
+    fields = ["instc", "serial_number", "comments", "created_by", "created_date",]
 
+
+class InstcDetails(mixins.InstMixin, CommonDetails):
+    fields = ["name", "nom", "description_en", "description_fr", "created_by", "created_date",]
+
+
+class InstdcDetails(mixins.InstMixin, CommonDetails):
+    fields = ["name", "nom", "description_en", "description_fr", "created_by", "created_date",]
+
+
+# ----------------------------LIST VIEWS-----------------------------
+class CommonList(CommonAuthFilterView):
+
+    nav_menu = 'bio_diversity/bio_diversity_nav.html'
+    site_css = 'bio_diversity/bio_diversity.css'
+    home_url_name = "bio_diversity:index"
+
+    # fields to be used as columns to display an object in the filter view table
+    fields = []
+
+    # URL to use to create a new object to be added to the filter view
+    create_url = None
+
+    # URL to use for the details button element in the filter view's list
+    details_url = None
+
+    # URL to use for the update button element in the filter view's list
+    update_url = None
+
+    # URL to use for the delete button element in the filter view's list
+    delete_url = False
+
+    # The height of the popup dialog used to display the creation/update form
+    # if not set by the extending class the default popup height will be used
+    creation_form_height = None
+
+    # By default Listed objects will have an update button, set editable to false in extending classes to disable
+    editable = True
+
+    def get_fields(self):
+        if self.fields:
+            return self.fields
+
+        return ['tname|Name', 'tdescription|Description']
+
+    def get_create_url(self):
+        return self.create_url if self.create_url is not None else "bio_diversity:create_{}".format(self.key)
+
+    def get_details_url(self):
+        return None # self.details_url if self.details_url is not None else "bio_diversity:details_{}".format(self.key)
+
+    def get_update_url(self):
+        return None # self.update_url if self.update_url is not None else "bio_diversity:update_{}".format(self.key)
+
+    def get_delete_url(self):
+        return None # self.delete_url if self.delete_url is not None else "bio_diversity:delete_{}".format(self.key)
+
+    def get_context_data(self, *args, object_list=None, **kwargs):
+        context = super().get_context_data(*args, object_list=object_list, **kwargs)
+
+        context['fields'] = self.get_fields()
+
+        # if the url is not None, use the value specified by the url variable.
+        # if the url is None, create a url using the views key
+        # this way if no URL, say details_url, is provided it's assumed the default RUL will be 'whalesdb:details_key'
+        # if the details_url = False in the extending view then False will be passed to the context['detials_url']
+        # variable and in the template where the variable is used for buttons and links the button and/or links can
+        # be left out without causing URL Not Found issues.
+        context['create_url'] = self.get_create_url()
+        context['details_url'] = self.get_details_url()
+        context['update_url'] = self.get_update_url()
+        context['delete_url'] = self.get_delete_url()
+
+        # for the most part if the user is authorized then the content is editable
+        # but extending classes can choose to make content not editable even if the user is authorized
+        # context['auth'] = utils.whales_authorized(self.request.user)
+        context['editable'] = context['auth'] and self.editable
+
+        if self.creation_form_height:
+            context['height'] = self.creation_form_height
+
+        return context
+
+
+class InstList(mixins.InstMixin, CommonList):
+    filterset_class = filters.InstFilter
+    fields = ["instc", "serial_number", "comments", "created_by", "created_date",]
+
+
+class InstcList(mixins.InstMixin, CommonList):
+    filterset_class = filters.InstFilter
+    fields = ["name", "nom", "description_en", "description_fr", "created_by", "created_date",]
+
+
+class InstdcList(mixins.InstMixin, CommonList):
+    filterset_class = filters.InstFilter
+    fields = ["name", "nom", "description_en", "description_fr", "created_by", "created_date",]
