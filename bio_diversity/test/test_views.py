5df45bd447968ed5c5565d9d18e2dc42779ca70a
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2020-12-01 13:17:56 -0400

    clean up test_views, complete

--- bio_diversity/test/test_views.py
+++ bio_diversity/test/test_views.py
@@ -1,4 +1,4 @@
-from django.test import tag
+from django.test import tag, RequestFactory
 from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 from faker import Faker
@@ -18,12 +18,36 @@ faker = Faker()
 # This is used to simulate calling the as_veiw() function normally called in the urls.py
 # this will return a view that can then have it's internal methods tested
 def setup_view(view, request, *args, **kwargs):
-
     view.request = request
     view.args = args
     view.kwargs = kwargs
     return view
 
+
+class MockCommonCreate(views.CommonCreate):
+    pass
+
+
+@tag('CreateCommon')
+class TestCommonCreate(CommonTest):
+
+    def setUp(self):
+        self.view = MockCommonCreate()
+
+    def test_get_init(self):
+        # test created_by field auto population
+        req_faq = RequestFactory()
+        request = req_faq.get(None)
+
+        # create and login a user to be expected by the inital function
+        user = self.get_and_login_user(in_group="bio_diversity_admin")
+        request.user = user
+
+        setup_view(self.view, request)
+
+        init = self.view.get_initial()
+        self.assertIsNotNone(init)
+        self.assertEqual(init['created_by'], user.username)
 
 
 
