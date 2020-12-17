c4f2571e425efee09db479390cfdc15504d910f1
Fishman <David.Fishman@dfo-mpo.gc.ca>
2020-12-17 05:53:45 -0400

    azure identity

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -2,30 +2,31 @@ import requests
 from decouple import config
 from django.conf import settings
 from storages.backends.azure_storage import AzureStorage
+from azure.identity import DefaultAzureCredential
 
 AZURE_STORAGE_ACCOUNT_NAME = config("AZURE_STORAGE_ACCOUNT_NAME", cast=str, default="")
 
 
-def get_auth_token():
-    url = 'http://169.254.169.254/metadata/identity/oauth2/token'
-    params = {"api-version": "2018-02-01", "resource": "https://management.azure.com/"}
-    headers = {'Metadata': 'true'}
-    r = requests.get(url, headers=headers, params=params)
-    return r.json()["access_token"]
+# def get_auth_token():
+#     url = 'http://169.254.169.254/metadata/identity/oauth2/token'
+#     params = {"api-version": "2018-02-01", "resource": "https://management.azure.com/"}
+#     headers = {'Metadata': 'true'}
+#     r = requests.get(url, headers=headers, params=params)
+#     return r.json()["access_token"]
+default_credential = DefaultAzureCredential()
 
 
 # from https://medium.com/@DawlysD/django-using-azure-blob-storage-to-handle-static-media-assets-from-scratch-90cbbc7d56be
 class AzureMediaStorage(AzureStorage):
     account_name = AZURE_STORAGE_ACCOUNT_NAME
-    token_credential = get_auth_token()
-
+    credential = default_credential
     azure_container = 'media'
     expiration_secs = None
 
 
 class AzureStaticStorage(AzureStorage):
     account_name = AZURE_STORAGE_ACCOUNT_NAME
-    token_credential = get_auth_token()
+    credential = default_credential
     azure_container = '$web'
     expiration_secs = None
 
