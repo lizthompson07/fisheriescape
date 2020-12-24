dfad6ecac7e1fdb1125240e300556c9947cc3418
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2020-12-24 11:12:47 -0400

    some more functional tests, that'll get working after xmas #565

--- bio_diversity/test/functional_test/test_functions.py
+++ bio_diversity/test/functional_test/test_functions.py
@@ -1,12 +1,12 @@
 from time import sleep
 
-from django.contrib.auth import authenticate, login
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse_lazy, resolve
 from selenium import webdriver
 
-from django.test import tag, override_settings
-from bio_diversity import forms
+from django.test import tag
+from selenium.webdriver import ActionChains
+
 from bio_diversity.test import BioFactoryFloor
 # from ..test.common_tests import CommonProjectTest as CommonTest
 from django.conf import settings
@@ -23,6 +23,8 @@ class CommonFunctionalTest(StaticLiveServerTestCase):
         # need to download the webdriver and stick it in the path/point to it here:
         # can be downloaded from: https://chromedriver.chromium.org/downloads
         self.browser = webdriver.Chrome('bio_diversity/test/functional_test/chromedriver.exe')
+        #self.browser.maximize_window()
+        self.browser.implicitly_wait(3)
         # generate a user
         user_data = UserFactory.get_valid_data()
         user = User.objects.create_superuser(username=user_data['username'], email=user_data['email1'], password=UserFactory.get_test_password())
@@ -38,8 +40,10 @@ class CommonFunctionalTest(StaticLiveServerTestCase):
         self.browser.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
         self.browser.refresh()  # selenium will set cookie domain based on current page domain
 
+        self.actions = ActionChains(self.browser)
+
     def tearDown(self):
-        self.browser.quit()  # causes connection was forcibly closed errors due to chrome being out of date
+        # self.browser.quit()  # causes connection was forcibly closed errors due to chrome being out of date
         super().tearDown()
 
 @tag("Functional", "Basic")
@@ -49,7 +53,9 @@ class TestHomePageTitle(CommonFunctionalTest):
         self.browser.get(self.live_server_url)
         assert 'DM Apps' in self.browser.title
         self.browser.implicitly_wait(3)
-        self.browser.find_element_by_xpath("//h4[contains(text(), 'Biodiversity')]").click()
+        biod_btn = self.browser.find_element_by_xpath("//h4[contains(text(), 'Biodiversity')]")
+        self.actions.move_to_element(biod_btn).perform()
+        biod_btn.click()
         assert 'Biodiversity' in self.browser.title
 
 
@@ -57,6 +63,36 @@ class TestHomePageTitle(CommonFunctionalTest):
 class TestSimpleLookup(CommonFunctionalTest):
 
     def test_navigate_through_views(self):
+        # user starts on app homepage:
+        self.browser.get("{}{}".format(self.live_server_url, "/en/bio_diversity/"))
+
+        # user clicks on a common lookup, ends up a list view
+        instc_btn = self.browser.find_element_by_xpath("//a[@class='btn btn-secondary btn-lg' and contains(text(), 'Instrument Code')]")
+        self.actions.move_to_element(instc_btn).perform()
+        instc_btn.click()
+        self.browser.implicitly_wait(3)
+        assert 'Instrument Code' in self.browser.title
+
+        # user creates a new instance of the lookup
+        self.browser.find_element_by_xpath("//a[@class='btn btn-primary' and contains(text(), '+')]").click()
+        instc = BioFactoryFloor.InstcFactory.build_valid_data()
+        name_field = self.browser.find_element_by_xpath("//input[@name='name']")
+        description_field = self.browser.find_element_by_xpath("//textarea[@name='description_en']")
+        name_field.send_keys(instc["name"])
+        description_field.send_keys(instc["description_en"])
+
+        submit_btn = self.browser.find_element_by_xpath("//button[@class='btn btn-success']")
+        self.browser.execute_script("arguments[0].scrollIntoView();", submit_btn)
+        self.browser.implicitly_wait(2)
+        submit_btn.click()
+        sleep(3)
+
+
+        # user checks to make sure that the instance is created and looks at its details
+
+        # user updates the instances details and goes on their way
+
+
         self.browser.get('%s%s' % (self.live_server_url, '/en/bio_diversity/create/instc/'))
         assert 'Instrument Code' in self.browser.title
 
