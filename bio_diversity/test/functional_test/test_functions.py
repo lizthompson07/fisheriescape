cbc3a0ad9bad5b7fa7593f784c99d547e01ebbe1
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2020-12-30 12:02:02 -0400

    add functional tests for adding location via event details view

--- bio_diversity/test/functional_test/test_functions.py
+++ bio_diversity/test/functional_test/test_functions.py
@@ -1,20 +1,14 @@
 from time import sleep
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-from django.urls import reverse_lazy, resolve
 from selenium import webdriver
 
 from django.test import tag
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 
-from bio_diversity.models import EventCode
+from bio_diversity.models import EventCode, Event, LocCode
 from bio_diversity.test import BioFactoryFloor
-# from ..test.common_tests import CommonProjectTest as CommonTest
-from django.conf import settings
 from shared_models.test.SharedModelsFactoryFloor import UserFactory, GroupFactory
-from shared_models.test.common_tests import CommonTest
 from django.contrib.auth.models import User
 
 
@@ -39,11 +33,9 @@ class CommonFunctionalTest(StaticLiveServerTestCase):
 
         self.browser.get(self.live_server_url + '/en/')
         self.client.force_login(user)  # Native django test client
-        cookie = self.client.cookies['sessionid'] #grab the login cookie
+        cookie = self.client.cookies['sessionid']  # grab the login cookie
         self.browser.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
         self.browser.refresh()  # selenium will set cookie domain based on current page domain
-
-        self.actions = ActionChains(self.browser)
 
     def tearDown(self):
         # self.browser.quit()  # causes connection was forcibly closed errors due to chrome being out of date
@@ -53,8 +45,35 @@ class CommonFunctionalTest(StaticLiveServerTestCase):
 def scroll_n_click(driver, element):
     # scroll to bring element to bottom of screen, then scroll up once to move element off of banners and click on it
     driver.execute_script("arguments[0].scrollIntoView(false);", element)
-    driver.execute_script("window.scrollBy(0,50)");
+    driver.execute_script("window.scrollBy(0,50)")
     element.click()
+
+
+def get_col_val(row, col):
+    # get text in col'th column of a row on list view
+    return row.find_elements_by_tag_name("td")[col].text
+
+
+def fill_n_submit_form(browser, data, exclude=[]):
+    # browser must be on a create page
+    # data should be a dict output from a factory build_valid_data method
+    for field_key in data.keys():
+        if field_key not in exclude:
+            form_field = browser.find_element_by_xpath("//*[@name='{}']".format(field_key))
+            if field_key[-3:] == "_id":
+                # foreign key fields
+                select = Select(form_field)
+                select.select_by_value(str(data[field_key]))
+            elif 'date' in form_field.get_attribute("class") and field_key not in ["created_date"]:
+                # date fields
+                browser.execute_script('document.getElementsByName("{}")[0].removeAttribute("readonly")'.format(field_key))
+                form_field.send_keys(str(data[field_key]))
+            elif field_key not in ["created_by", "created_date"]:
+                # text fields
+                form_field.send_keys(str(data[field_key]).replace("\n", " "))
+
+    submit_btn = browser.find_element_by_xpath("//button[@class='btn btn-success']")
+    scroll_n_click(browser, submit_btn)
 
 
 @tag("Functional", "Basic")
@@ -68,10 +87,9 @@ class TestHomePageTitle(CommonFunctionalTest):
         self.assertIn('Biodiversity', self.browser.title, "not on correct page")
 
 
-
 @tag("Functional", "Evnt")
 class TestEvntFunctional(CommonFunctionalTest):
-    # put factories in here and not just in class to make factory boy use selenium database.
+    # put factories in setUp and not in class to make factory boy use selenium database.
     def setUp(self):
         super().setUp()
         self.object_verbose = 'Event'
@@ -89,32 +107,50 @@ class TestEvntFunctional(CommonFunctionalTest):
 
         # user creates a new instance of the lookup
         self.browser.find_element_by_xpath("//a[@class='btn btn-primary' and contains(text(), '+')]").click()
-
-        for field_key in self.object_data.keys():
-            form_field = self.browser.find_element_by_xpath("//*[@name='{}']".format(field_key))
-            if field_key[-3:] == "_id":
-                select = Select(form_field)
-                select.select_by_value(str(self.object_data[field_key]))
-            elif field_key not in ["evnt_start", "evnt_end", "created_by", "created_date"]:
-                form_field.send_keys(self.object_data[field_key].replace("\n", " "))
-            elif field_key == "evnt_end":
-                self.browser.execute_script('document.getElementsByName("{}")[0].removeAttribute("readonly")'.format(field_key))
-                form_field.send_keys(str(self.object_data[field_key]))
-                # pick_date(self.browser, form_field, self.object_data[field_key])
-
-        submit_btn = self.browser.find_element_by_xpath("//button[@class='btn btn-success']")
-        scroll_n_click(self.browser, submit_btn)
+        fill_n_submit_form(self.browser, self.object_data)
 
         # user checks to make sure that the instance is created
         details_table = self.browser.find_element_by_xpath("//table[@id='details_table']/tbody")
+        evntc_used = EventCode.objects.filter(pk=self.object_data["evntc_id"]).get().__str__()
         rows = details_table.find_elements_by_tag_name("tr")
-        new_object_row = False
-        for row in rows:
-            evntc_cell = row.find_elements_by_tag_name("td")[1]
-            evntc_used = EventCode.objects.filter(pk=self.object_data["evntc_id"]).get().__str__()
-            if evntc_cell.text == evntc_used:
-                new_object_row = row
-        self.assertTrue(new_object_row, "New object not displayed, {} not equal {}".format(evntc_cell.text, evntc_used))
+        self.assertIn(evntc_used, [get_col_val(row, 1) for row in rows])
+
+
+@tag("Functional", "EvntDet")
+class TestEvntDetailsFunctional(CommonFunctionalTest):
+    # put factories in setUp and not in class to make factory boy use selenium database.
+    def setUp(self):
+        super().setUp()
+        self.evnt_data = BioFactoryFloor.EvntFactory()
+
+    def test_details_interaction(self):
+        # user navigates to a details view for an event
+        self.browser.get("{}{}{}".format(self.live_server_url, "/en/bio_diversity/details/evnt/", self.evnt_data.id))
+        self.assertIn('Event', self.browser.title, "not on correct page")
+
+        # user makes sure details are correct
+
+        # user adds a location to the event
+        location_data = BioFactoryFloor.LocFactory.build_valid_data()
+        location_details = self.browser.find_element_by_xpath('//div[@name="evnt-location-details"]')
+        location_details.find_element_by_xpath('//a[@name="add-location-btn"]').click()
+        self.browser.switch_to.window(self.browser.window_handles[1])
+        # make sure pre fill field is filled:
+        evnt_form_field = self.browser.find_element_by_xpath("//*[@name='{}']".format("evnt_id"))
+        selected_element = evnt_form_field.find_element_by_xpath("//*[@selected]")
+        self.assertIn(self.evnt_data.__str__(), selected_element.text)
+
+        fill_n_submit_form(self.browser, location_data, ["evnt_id"])
+        self.browser.switch_to.window(self.browser.window_handles[0])
+        self.browser.refresh()
+        location_details = self.browser.find_element_by_xpath('//div[@name="evnt-location-details"]')
+        details_table = location_details.find_element_by_xpath("//table/tbody")
+        locc_used = LocCode.objects.filter(pk=location_data["locc_id"]).get().__str__()
+        rows = details_table.find_elements_by_tag_name("tr")
+        self.assertIn(locc_used, [get_col_val(row, 0) for row in rows])
+        # user adds an animal cross reference to the event
+
+        # user add a container cross reference to the event
 
 
 @tag("Functional", "Instc")
@@ -137,24 +173,19 @@ class InstcTestSimpleLookup(CommonFunctionalTest):
 
         # user creates a new instance of the lookup
         self.browser.find_element_by_xpath("//a[@class='btn btn-primary' and contains(text(), '+')]").click()
-        name_field = self.browser.find_element_by_xpath("//input[@name='name']")
-        description_field = self.browser.find_element_by_xpath("//textarea[@name='description_en']")
-        name_field.send_keys(self.lookup_data["name"])
-        description_field.send_keys(self.lookup_data["description_en"])
-        submit_btn = self.browser.find_element_by_xpath("//button[@class='btn btn-success']")
-        scroll_n_click(self.browser, submit_btn)
+        fill_n_submit_form(self.browser, self.lookup_data)
 
         # user checks to make sure that the instance is created
         details_table = self.browser.find_element_by_xpath("//table[@id='details_table']/tbody")
         rows = details_table.find_elements_by_tag_name("tr")
+        self.assertIn(self.lookup_data["name"], [get_col_val(row, 0) for row in rows])
+
+        # user looks at details of newly created object:
         new_object_row = False
         for row in rows:
             name_cell = row.find_elements_by_tag_name("td")[0]
             if name_cell.text == self.lookup_data["name"]:
                 new_object_row = row
-        self.assertTrue(new_object_row, "New object not displayed")
-
-        # user looks at details of newly created object:
         new_object_row.find_element_by_xpath("//a[@class='btn btn-primary btn-sm my-1' and contains(text(), "
                                              "'Details')]").click()
         description_detail_element = self.browser.find_element_by_xpath("//span[@class='font-weight-bold'and "
@@ -167,23 +198,15 @@ class InstcTestSimpleLookup(CommonFunctionalTest):
         self.browser.find_element_by_xpath("//a[@class='btn btn-primary' and@title='Update']").click()
         name_field = self.browser.find_element_by_xpath("//input[@name='name']")
         name_field.clear()
-        name_field.send_keys("updated name")
+        new_name = "updated name"
+        name_field.send_keys(new_name)
         submit_btn = self.browser.find_element_by_xpath("//button[@class='btn btn-success']")
         scroll_n_click(self.browser, submit_btn)
 
         details_table = self.browser.find_element_by_xpath("//table[@id='details_table']/tbody")
         rows = details_table.find_elements_by_tag_name("tr")  # get all of the rows in the table
-        new_object_row = False
-        old_object_row = False
-        for row in rows:
-            # Get the columns (all the column 2)
-            name_cell = row.find_elements_by_tag_name("td")[0]  # note: index start from 0, 1 is col 2
-            if name_cell.text == self.lookup_data["name"]:
-                old_object_row = row
-            if name_cell.text == "updated name":
-                new_object_row = row
-        self.assertTrue(new_object_row, "updated object not in details list")
-        self.assertFalse(old_object_row, "old object still in details list")
+        self.assertNotIn(self.lookup_data["name"], [get_col_val(row, 0) for row in rows])
+        self.assertIn(new_name, [get_col_val(row, 0) for row in rows])
 
 
 @tag("Functional", "Instdc")
@@ -206,24 +229,20 @@ class InstdcTestSimpleLookup(CommonFunctionalTest):
 
         # user creates a new instance of the lookup
         self.browser.find_element_by_xpath("//a[@class='btn btn-primary' and contains(text(), '+')]").click()
-        name_field = self.browser.find_element_by_xpath("//input[@name='name']")
-        description_field = self.browser.find_element_by_xpath("//textarea[@name='description_en']")
-        name_field.send_keys(self.lookup_data["name"])
-        description_field.send_keys(self.lookup_data["description_en"])
-        submit_btn = self.browser.find_element_by_xpath("//button[@class='btn btn-success']")
-        scroll_n_click(self.browser, submit_btn)
+        fill_n_submit_form(self.browser, self.lookup_data)
 
         # user checks to make sure that the instance is created
         details_table = self.browser.find_element_by_xpath("//table[@id='details_table']/tbody")
         rows = details_table.find_elements_by_tag_name("tr")
+        self.assertIn(self.lookup_data["name"], [get_col_val(row, 0) for row in rows])
+
+        # user looks at details of newly created object:
         new_object_row = False
         for row in rows:
             name_cell = row.find_elements_by_tag_name("td")[0]
             if name_cell.text == self.lookup_data["name"]:
                 new_object_row = row
-        self.assertTrue(new_object_row, "New object not displayed")
 
-        # user looks at details of newly created object:
         new_object_row.find_element_by_xpath("//a[@class='btn btn-primary btn-sm my-1' and contains(text(), "
                                              "'Details')]").click()
         description_detail_element = self.browser.find_element_by_xpath("//span[@class='font-weight-bold'and "
@@ -236,20 +255,12 @@ class InstdcTestSimpleLookup(CommonFunctionalTest):
         self.browser.find_element_by_xpath("//a[@class='btn btn-primary' and@title='Update']").click()
         name_field = self.browser.find_element_by_xpath("//input[@name='name']")
         name_field.clear()
-        name_field.send_keys("updated name")
+        new_name = "updated name"
+        name_field.send_keys(new_name)
         submit_btn = self.browser.find_element_by_xpath("//button[@class='btn btn-success']")
         scroll_n_click(self.browser, submit_btn)
 
         details_table = self.browser.find_element_by_xpath("//table[@id='details_table']/tbody")
         rows = details_table.find_elements_by_tag_name("tr")  # get all of the rows in the table
-        new_object_row = False
-        old_object_row = False
-        for row in rows:
-            # Get the columns (all the column 2)
-            name_cell = row.find_elements_by_tag_name("td")[0]  # note: index start from 0, 1 is col 2
-            if name_cell.text == self.lookup_data["name"]:
-                old_object_row = row
-            if name_cell.text == "updated name":
-                new_object_row = row
-        self.assertTrue(new_object_row, "updated object not in details list")
-        self.assertFalse(old_object_row, "old object still in details list")
+        self.assertNotIn(self.lookup_data["name"], [get_col_val(row, 0) for row in rows])
+        self.assertIn(new_name, [get_col_val(row, 0) for row in rows])
