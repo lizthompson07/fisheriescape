587e1e0e1a49b9b359c397697f7319e161b67a77
David Fishman <davjfish@gmail.com>
2021-01-05 09:25:33 -0400

    translations

--- scuba/utils.py
+++ scuba/utils.py
@@ -1,10 +1,14 @@
+from django.contrib.auth.models import Group
+
 # open basic access up to anybody who is logged in
-
 def in_scuba_admin_group(user):
+    # make sure the following group exist:
+    admin_group, created = Group.objects.get_or_create(name="scuba_admin")
     if user:
-        return user.groups.filter(name='scuba_admin').exists()
-
+        return admin_group in user.groups.all()
 
 def in_scuba_crud_group(user):
+    # make sure the following group exist:
+    crud_group, created = Group.objects.get_or_create(name="scuba_crud")
     if user:
-        return in_scuba_admin_group(user) or user.groups.filter(name='scuba_crud').exists()
+        return in_scuba_admin_group(user) or crud_group in user.groups.all()
