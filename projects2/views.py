f0a238b3da1fbc962ed33f7773ed442762efd7d1
David Fishman <davjfish@gmail.com>
2021-01-12 10:52:40 -0400

    #492

--- projects2/views.py
+++ projects2/views.py
@@ -814,7 +814,7 @@ class AdminStaffListView(ManagerOrAdminRequiredMixin, CommonFilterView):
 
     filterset_class = filters.StaffFilter
     home_url_name = "projects2:index"
-    h1 = gettext_lazy("Non-registered Staff List")
+    h1 = gettext_lazy("Non-DMApps Staff List")
     h2 = gettext_lazy(
         "The purpose of this list view is to check if any DFO staff were listed on projects, but were not connected to their dmapps user accounts")
     field_list = [
@@ -829,7 +829,8 @@ class AdminStaffListView(ManagerOrAdminRequiredMixin, CommonFilterView):
         qs = models.Staff.objects.filter(user__isnull=True, name__isnull=False).filter(~Q(name__icontains="unknown")).filter(~Q(name__icontains="tbd")).filter(
             ~Q(name__icontains="BI-")).filter(~Q(name__icontains="PC-")).filter(~Q(name__icontains="EG-")).filter(~Q(name__icontains="determined")).filter(
             ~Q(name__icontains="to be")).filter(~Q(name__icontains="student")).filter(~Q(name__icontains="casual")).filter(
-            ~Q(name__icontains="technician")).filter(~Q(name__icontains="crew")).filter(~Q(name__icontains="hired"))
+            ~Q(name__icontains="technician")).filter(~Q(name__icontains="crew")).filter(~Q(name__icontains="hired")).filter(
+            ~Q(name__icontains="support")).filter(~Q(name__icontains="volunteer"))
 
         qs = qs.order_by('-project_year__fiscal_year', 'project_year__project__section__division', 'project_year__project__section',
                          'project_year__project__title')
@@ -841,7 +842,7 @@ class AdminStaffUpdateView(ManagerOrAdminRequiredMixin, CommonUpdateView):
     model = models.Staff
     template_name = 'projects2/admin_staff_form.html'
     form_class = forms.AdminStaffForm
-    h1 = gettext_lazy("Edit Non-registered Staff Member")
+    h1 = gettext_lazy("Edit Non-DMApps Staff Member")
     parent_crumb = {"title": _("Non-registered Staff List"), "url": reverse_lazy("projects2:admin_staff_list")}
     container_class = "container bg-light curvy"
 
