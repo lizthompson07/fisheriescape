4a19f8a072ab61291a5b23d02b49835cae84c234
David Fishman <davjfish@gmail.com>
2021-01-13 12:36:58 -0400

    #492

--- inventory/models.py
+++ inventory/models.py
@@ -381,7 +381,7 @@ class Resource(models.Model):
     open_data_notes = models.TextField(blank=True, null=True,
                                        verbose_name="Open data notes")
     notes = models.TextField(blank=True, null=True, verbose_name="General notes")
-    # citations = models.ManyToManyField(Citation, related_name='resources', blank=True)
+    citations = models.ManyToManyField(Citation, related_name='resources', blank=True)
     citations2 = models.ManyToManyField(shared_models.Citation, related_name='resources', blank=True)
     keywords = models.ManyToManyField(Keyword, related_name='resources', blank=True)
     people = models.ManyToManyField(Person, through='ResourcePerson')
