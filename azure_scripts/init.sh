6c39abbe450ac3c240c9ace3fad99f211fb96916
Fishman <David.Fishman@dfo-mpo.gc.ca>
2021-01-20 13:54:00 -0400

    pipelines

--- azure_scripts/init.sh
+++ azure_scripts/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 service ssh start
-python manage.py compilemessages
 python create_env_file_from_json.py "$ENVIRONMENT_NAME"
+python manage.py compilemessages
+python manage.py collectstatic --no-input
+python manage.py migrate
 gunicorn -b 0.0.0.0:8000 dm_apps.wsgi:application
