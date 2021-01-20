f77ea189c919981036220fa7a1795c782c22e7f7
Fishman <David.Fishman@dfo-mpo.gc.ca>
2021-01-20 14:03:54 -0400

    pipelines

--- azure_scripts/init.sh
+++ azure_scripts/init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 service ssh start
+git rev-parse --short HEAD
 python create_env_file_from_json.py "$ENVIRONMENT_NAME"
 python manage.py compilemessages
 python manage.py collectstatic --no-input
