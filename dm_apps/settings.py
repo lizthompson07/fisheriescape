b2c6f7016728ba027b87317100ce8b0468a25dda
David Fishman <davjfish@gmail.com>
2021-01-21 11:41:11 -0400

    travel

--- dm_apps/settings.py
+++ dm_apps/settings.py
@@ -48,7 +48,8 @@ GITHUB_API_KEY = config("GITHUB_API_KEY", cast=str, default="")
 # Should the ticketing app be displayed on the main index page?
 SHOW_TICKETING_APP = config("SHOW_TICKETING_APP", cast=bool, default=True)
 # get the git commit number from the ENV to display on index.html
-GIT_VERSION = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode()
+# GIT_VERSION = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode()
+GIT_VERSION = ""
 # Azure Instrumentation KEy for application insights
 USE_AZURE_APPLICATION_INSIGHT = config("USE_AZURE_APPLICATION_INSIGHT", cast=bool, default=False)
 AZURE_INSTRUMENTATION_KEY = config("AZURE_INSTRUMENTATION_KEY", cast=str, default="")
