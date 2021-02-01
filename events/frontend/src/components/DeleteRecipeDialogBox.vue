f7caac86098bfc197842eda83be997d888a1db90
David Fishman <davjfish@gmail.com>
2021-02-01 16:20:19 -0400

    projects2 quick fix

---
+++ events/frontend/src/components/DeleteRecipeDialogBox.vue
@@ -0,0 +1,67 @@
+<template>
+    <v-dialog
+      v-model="dialog"
+      width="500"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          class="mx-1"
+          color="error"
+          dark
+          v-bind="attrs"
+          v-on="on"
+        >
+          Delete
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title
+          class="headline grey lighten-2"
+          primary-title
+        >
+          Delete Confirmation
+        </v-card-title>
+
+        <v-card-text>
+          Are you certain you want to delete this recipe?
+        </v-card-text>
+
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="triggerDelete"
+          >
+            Yes
+          </v-btn>
+          <v-btn
+            color="primary"
+            text
+            @click="dialog = false"
+          >
+            No
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: "DeleteRecipeDialogBox",
+    data () {
+      return {
+        dialog: false,
+      }
+    },
+    methods: {
+      triggerDelete(){
+        this.$emit("delete-confirmation");
+        this.dialog = false;
+      }
+    }
+  }
+</script>
