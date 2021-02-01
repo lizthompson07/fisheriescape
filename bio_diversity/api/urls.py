3822a403f0f1431478bdd0b348133a723f062d8e
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2021-02-01 11:46:26 -0400

    rename api to avoid url conflicts

--- bio_diversity/api/urls.py
+++ bio_diversity/api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("bio_diversity/user/", views.CurrentUserAPIView.as_view(), name="current-user"),  # tested
+    path("bio_diversity/user/", views.BioCurrentUserAPIView.as_view(), name="bio-current-user"),  # tested
 
     path("bio_diversity/indv/", views.IndividualAPIView.as_view(), name="api-indv"),  # tested
 
