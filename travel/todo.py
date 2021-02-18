52265255cdd47dfe85e94cd68aa5225a05991c7a
David Fishman <davjfish@gmail.com>
2021-02-18 00:54:43 -0400

    #600

--- travel/todo.py
+++ travel/todo.py
@@ -2,20 +2,16 @@
 # make sure emails are sent when requests are denied
 # email for when ppl are deleted from a request
 
-# TODO: go back to show attachments and reviewers at end
-
 # TODO: traveller costs
 # TODO: traveller clone
 
-# TODO: file edit
-# TODO: file add
-# TODO: file delete
+# todo: TR Cost SHOULD BE RENAMED TO TRAVELLER COST
+# TODO: TR Cost help bubble not working
 
 # At a later date
 # TODO: revamp verification form
 # TODO: help bubble on traveller form
 
-# todo: TR Cost SHOULD BE RENAMED TO TRAVELLER COST
 # bring back admin warnings
 
 # reviewer page not working
