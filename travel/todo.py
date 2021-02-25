364b711043046ca9c0365e7623e034f3c7277d39
David Fishman <davjfish@gmail.com>
2021-02-25 00:32:08 -0400

    #600

--- travel/todo.py
+++ travel/todo.py
@@ -1,17 +1,15 @@
-# TODO: trip reassign and delete
-# todo: api view validation
-# TODO: traveller filter not working on request -- use vue js dropdown
-# TODO: api for tracking view
+# TODO: update DFO org chart
+# todo:  email for when ppl are deleted from a request
 
+# TODO: trip reassign and delete
+# TODO: revamp verification form
+
+# TODO: sort all methods and attrs on serializers
 
 # At a later date
-# todo:  email for when ppl are deleted from a request
-# todo: make sure emails are sent when requests are denied
-# TODO: revamp verification form
 
 # TODO: fix up unit tests
     # TODO: test new model choices!!
-# TODO: sort all methods and attrs on serializers
 
 # todo: it would be nice for the trips to have the same flow as requests. right now they are opening in new widows all the time!
 # TODO: seek and destroy print statements
@@ -22,7 +20,6 @@
 # todo: TR1 SHOULD BE RENAMED TO TR
 # todo: rename conference to something trip
 
-# TODO: phone number dashes
 
 # TODO: clean up model props
 # TODO: should not be able to submit a trip without any recommenders or reviewers
