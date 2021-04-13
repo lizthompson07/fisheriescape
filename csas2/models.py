10685dc099cd42647eba6b15a460225aff2c6d8a
David Fishman <davjfish@gmail.com>
2021-04-13 15:47:30 -0300

    scetching out some models:

--- csas2/models.py
+++ csas2/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.template.defaultfilters import date
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _, gettext
 
 from shared_models.models import SimpleLookup, UnilingualSimpleLookup, UnilingualLookup, FiscalYear, Region, MetadataFields, Language
 
@@ -114,7 +114,7 @@ class Attendance(models.Model):
         unique_together = (("invitee", "date"),)
 
 
-class Note(models.Model):
+class MeetingNote(MetadataFields):
     # Choices for type
     type_choices = (
         (1, 'To Do'),
@@ -122,27 +122,10 @@ class Note(models.Model):
         (3, 'General comment'),
     )
 
-    event = models.ForeignKey(Event, related_name='notes', on_delete=models.CASCADE)
+    meeting = models.ForeignKey(Meeting, related_name='notes', on_delete=models.CASCADE)
     type = models.IntegerField(choices=type_choices, verbose_name=_("type"))
     note = models.TextField(verbose_name=_("note"))
     is_complete = models.BooleanField(default=False, verbose_name=_("complete?"))
-
-    # meta
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("date last modified"), editable=False)
-    updated_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, verbose_name=_("last modified by"), editable=False,
-                                   related_name="user_event_notes_updated_by")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("date created"), editable=False)
-    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, verbose_name=_("created by"), editable=False,
-                                   related_name="user_event_notes_created_by")
-
-    @property
-    def metadata(self):
-        return get_metadata_string(
-            self.created_at,
-            self.created_by,
-            self.updated_at,
-            self.updated_by,
-        )
 
     class Meta:
         ordering = ["is_complete", "-updated_at", ]
@@ -152,30 +135,11 @@ def resource_directory_path(instance, filename):
     return 'events/{0}/{1}'.format(instance.event.id, filename)
 
 
-class Resource(SimpleLookup):
-    event = models.ForeignKey(Event, related_name='resources', on_delete=models.CASCADE)
+class MeetingResource(SimpleLookup, MetadataFields):
+    event = models.ForeignKey(Meeting, related_name='resources', on_delete=models.CASCADE)
 
     url_en = models.URLField(verbose_name=_("url (English)"), blank=True, null=True)
     url_fr = models.URLField(verbose_name=_("url (French)"), blank=True, null=True)
-    # file_en = models.FileField(upload_to=resource_directory_path, verbose_name=_("file attachment (English)"), blank=True, null=True)
-    # file_fr = models.FileField(upload_to=resource_directory_path, verbose_name=_("file attachment (French)"), blank=True, null=True)
-
-    # meta
-    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("date last modified"), editable=False)
-    updated_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, verbose_name=_("last modified by"), editable=False,
-                                   related_name="user_event_resources_updated_by")
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("date created"), editable=False)
-    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, verbose_name=_("created by"), related_name="user_event_resources_created_by",
-                                   editable=False)
-
-    @property
-    def metadata(self):
-        return get_metadata_string(
-            self.created_at,
-            self.created_by,
-            self.updated_at,
-            self.updated_by,
-        )
 
     @property
     def tfile(self):
