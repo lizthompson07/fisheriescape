79d8a444830aa050c13d5ab129cdafcddb2af915
UpsonP <upsonp@gmail.com>
2021-04-20 11:09:17 -0300

    visual updates, using generic_filter from shared_models now

--- whalesdb/views.py
+++ whalesdb/views.py
@@ -649,12 +649,19 @@ class StnDetails(mixins.StnMixin, CommonDetails):
 
 class CommonList(CommonAuthFilterView):
 
+    template_name = "whalesdb/whales_filter.html"
     nav_menu = 'whalesdb/base/whales_nav_menu.html'
     site_css = 'whalesdb/base/whales_css.css'
     home_url_name = "whalesdb:index"
+    new_btn_text = "+"
 
     # fields to be used as columns to display an object in the filter view table
     fields = []
+
+    field_list = [
+        {"name": 'tname|{}'.format("name"), "class": "", "width": ""},
+        {"name": 'tdescription|{}'.format("description"), "class": "", "width": ""},
+    ]
 
     # URL to use to create a new object to be added to the filter view
     create_url = None
@@ -704,8 +711,8 @@ class CommonList(CommonAuthFilterView):
         # if the details_url = False in the extending view then False will be passed to the context['detials_url']
         # variable and in the template where the variable is used for buttons and links the button and/or links can
         # be left out without causing URL Not Found issues.
-        context['create_url'] = self.get_create_url()
-        context['details_url'] = self.get_details_url()
+        context['new_object_url'] = self.get_create_url()
+        context['row_object_url_name'] = self.get_details_url()
         context['update_url'] = self.get_update_url()
         context['delete_url'] = self.get_delete_url()
 
@@ -727,6 +734,18 @@ class CruList(mixins.CruMixin, CommonList):
     fields = ["mission_number", "description", "chief_scientist", "samplers", "start_date", "end_date", "notes",
               "season", "vessel" ]
 
+    field_list = [
+        {"name": "mission_number"},
+        {"name": "description"},
+        {"name": "chief_scientist"},
+        {"name": "samplers"},
+        {"name": "start_date"},
+        {"name": "end_date"},
+        {"name": "notes"},
+        {"name": "season"},
+        {"name": "vessel"},
+    ]
+
     details_url = "whalesdb:details_cru"
     delete_url = "whalesdb:delete_cru"
 
@@ -745,6 +764,15 @@ class DepList(mixins.DepMixin, CommonList):
     fields = ['dep_name', 'dep_year', 'dep_month', 'stn', 'prj', 'mor']
     creation_form_height = 600
 
+    field_list = [
+        {"name": "dep_name"},
+        {"name": "dep_year"},
+        {"name": "dep_month"},
+        {"name": "stn"},
+        {"name": "prj"},
+        {"name": "mor"},
+    ]
+
     delete_url = "whalesdb:delete_dep"
 
     def get_context_data(self, *args, object_list=None, **kwargs):
@@ -756,42 +784,84 @@ class DepList(mixins.DepMixin, CommonList):
 class EcaList(mixins.EcaMixin, CommonList):
     filterset_class = filters.EcaFilter
     fields = ['eca_date', 'eca_attachment', 'eca_hydrophone']
+    field_list = [
+        {"name": "eca_date"},
+        {"name": "eca_attachment"},
+        {"name": "eca_hydrophone"},
+    ]
+
 
 
 class EmmList(mixins.EmmMixin, CommonList):
     filterset_class = filters.EmmFilter
     fields = ['eqt', 'emm_make', 'emm_model', 'emm_depth_rating']
+    field_list = [
+        {"name": "eqt"},
+        {"name": "emm_make"},
+        {"name": "emm_model"},
+        {"name": "emm_depth_rating"},
+    ]
 
 
 class EqpList(mixins.EqpMixin, CommonList):
     fields = ['emm', 'eqp_serial', 'eqp_date_purchase', 'eqo_owned_by', 'eqp_retired', "eqp_deployed"]
+    field_list = [
+        {"name": "emm"},
+        {"name": "eqp_serial"},
+        {"name": "eqp_date_purchase"},
+        {"name": "eqo_owned_by"},
+        {"name": "eqp_retired"},
+        {"name": "eqp_deployed"},
+    ]
 
 
 class EtrList(mixins.EtrMixin, CommonList):
     filterset_class = filters.EtrFilter
     fields = ['etr_date', 'eqp', 'etr_issue_desc', 'etr_repair_desc', 'etr_repaired_by', 'etr_dep_affe', 'etr_rec_affe']
+    field_list = [
+        {"name": "etr_date"},
+        {"name": "eqp"},
+        {"name": "etr_issue_desc"},
+        {"name": "etr_repair_desc"},
+        {"name": "etr_repaired_by"},
+        {"name": "etr_dep_affe"},
+        {"name": "etr_rec_affe"},
+    ]
 
 
 class MorList(mixins.MorMixin, CommonList):
     filterset_class = filters.MorFilter
     fields = ['mor_name', 'mor_max_depth', 'mor_notes']
-    creation_form_height = 725
+    field_list = [
+        {"name": "mor_name"},
+        {"name": "mor_max_depth"},
+        {"name": "mor_notes"},
+    ]
 
 
 class PrjList(mixins.PrjMixin, CommonList):
     filterset_class = filters.PrjFilter
-    creation_form_height = 400
     fields = ['tname|Name', 'tdescription|Description']
 
 
 class RecList(mixins.RecMixin, CommonList):
     filterset_class = filters.RecFilter
     fields = ['eda_id', 'rsc_id', 'rec_start_date', 'rec_end_date']
+    field_list = [
+        {"name": "eda_id"},
+        {"name": "rsc_id"},
+        {"name": "rec_start_date"},
+        {"name": "rec_end_date"},
+    ]
 
 
 class RetList(mixins.RetMixin, CommonList):
     filterset_class = filters.RetFilter
     fields = ['ret_name', 'ret_desc']
+    field_list = [
+        {"name": "ret_name"},
+        {"name": "ret_desc"},
+    ]
 
     details_url = False
 
@@ -799,16 +869,30 @@ class RetList(mixins.RetMixin, CommonList):
 class RscList(mixins.RscMixin, CommonList):
     filterset_class = filters.RscFilter
     fields = ['rsc_name', 'rsc_period']
+    field_list = [
+        {"name": "rsc_name"},
+        {"name": "rsc_period"},
+    ]
 
 
 class StnList(mixins.StnMixin, CommonList):
     filterset_class = filters.StnFilter
     fields = ['stn_name', 'stn_code', 'stn_revision']
+    field_list = [
+        {"name": "stn_name"},
+        {"name": "stn_code"},
+        {"name": "stn_revision"},
+    ]
 
 
 class TeaList(mixins.TeaMixin, CommonList):
     filterset_class = filters.TeaFilter
     fields = ["tea_abb", "tea_last_name", "tea_first_name"]
+    field_list = [
+        {"name": "tea_abb"},
+        {"name": "tea_last_name"},
+        {"name": "tea_first_name"},
+    ]
 
     details_url = False
 
