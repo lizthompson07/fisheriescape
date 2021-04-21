80c695d9a1cddba2c1a980556baf1e335f6d1f04
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2021-04-21 09:57:06 -0300

    catch and parse errors temperature parser

--- bio_diversity/data_parsers/temperatures.py
+++ bio_diversity/data_parsers/temperatures.py
@@ -16,19 +16,29 @@ def temperature_parser(cleaned_data):
         log_data += "\n File format not valid: {}".format(err.__str__())
         return log_data, False
 
-    contx = utils.enter_trof_contx(cleaned_data["trof_id"].name, cleaned_data, final_flag=None, return_contx=True)
-    qual_id = models.QualCode.objects.filter(name="Good").get()
-    envc_id = models.EnvCode.objects.filter(name="Temperature").get()
+    # prep the data:
+    try:
+        contx = utils.enter_trof_contx(cleaned_data["trof_id"].name, cleaned_data, final_flag=None, return_contx=True)
+        qual_id = models.QualCode.objects.filter(name="Good").get()
+        envc_id = models.EnvCode.objects.filter(name="Temperature").get()
 
-    data["datetime"] = data.apply(lambda row: datetime.strptime(row["Date(yyyy-mm-dd)"] + ", " + row["Time(hh:mm:ss)"],
-                                                                "%Y-%m-%d, %H:%M:%S").replace(tzinfo=pytz.UTC), axis=1)
+        data["datetime"] = data.apply(lambda row: datetime.strptime(row["Date(yyyy-mm-dd)"] + ", " + row["Time(hh:mm:ss)"],
+                                                                    "%Y-%m-%d, %H:%M:%S").replace(tzinfo=pytz.UTC), axis=1)
 
-    data["env"] = data.apply(lambda row: utils.enter_env(row["Temperature (°C)"], row["datetime"].date(), cleaned_data,
-                                                         envc_id, env_start=row["datetime"].time(), contx=contx,
-                                                         save=False, qual_id=qual_id), axis=1)
+        data["env"] = data.apply(lambda row: utils.enter_env(row["Temperature (°C)"], row["datetime"].date(), cleaned_data,
+                                                             envc_id, env_start=row["datetime"].time(), contx=contx,
+                                                             save=False, qual_id=qual_id), axis=1)
+    except Exception as err:
+        log_data += "Error preparing data for entry \n "
+        log_data += "\n Error: {}".format(err.__str__())
+        return log_data, False
 
-    entered_list = models.EnvCondition.objects.bulk_create(list(data["env"].dropna()))
+    # enter the data
+    try:
+        entered_list = models.EnvCondition.objects.bulk_create(list(data["env"].dropna()))
+    except Exception as err:
+        log_data += "Error with bulk data entry \n "
+        log_data += "\n Error: {}".format(err.__str__())
+        return log_data, False
 
-    log_data += "\n\n\n {} of {} rows entered to " \
-                "database".format(len(entered_list), len(data_dict))
     return log_data, True
