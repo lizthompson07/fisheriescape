0db074f042169642ca048b2c19e8b8bf0b1c33ab
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2021-05-07 15:52:37 -0300

    expand api #747

--- bio_diversity/api/serializers.py
+++ bio_diversity/api/serializers.py
@@ -46,3 +46,52 @@ class IndividualDisplaySerializer(serializers.ModelSerializer):
     def get_group(self, instance):
         return instance.grp_id.__str__()
 
+
+class CupDisplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Cup
+        fields = ["id", "name", "nom", "description_en", "description_fr", "drawer", "draw_id", "heath_unit",
+                  "start_date", "end_date"]
+
+    drawer = serializers.SerializerMethodField()
+    heath_unit = serializers.SerializerMethodField()
+
+    def get_drawer(self, instance):
+        return instance.draw_id.__str__()
+
+    def get_heath_unit(self, instance):
+        return instance.draw_id.heat_id.__str__()
+
+
+class AnixDisplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.AniDetailXref
+        fields = "__all__"
+
+
+class ContxDisplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ContainerXRef
+        fields = "__all__"
+
+
+class GroupDisplaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Group
+        fields = "__all__"
+
+    stock = serializers.SerializerMethodField()
+    collection = serializers.SerializerMethodField()
+    species = serializers.SerializerMethodField()
+
+    def get_stock(self, instance):
+        return instance.stok_id.__str__()
+
+    def get_length(self, instance):
+        return instance.individual_detail("Length")
+
+    def get_collection(self, instance):
+        return instance.coll_id.__str__()
+
+    def get_species(self, instance):
+        return instance.spec_id.__str__()
--- bio_diversity/api/urls.py
+++ bio_diversity/api/urls.py
@@ -6,5 +6,9 @@ urlpatterns = [
     path("bio_diversity/user/", views.BioCurrentUserAPIView.as_view(), name="bio-current-user"),  # tested
 
     path("bio_diversity/indv/", views.IndividualAPIView.as_view(), name="api-indv"),  # tested
+    path("bio_diversity/cup/", views.CupAPIView.as_view(), name="api-cup"),
+    path("bio_diversity/anix/", views.AnixAPIView.as_view(), name="api-anix"),
+    path("bio_diversity/contx/", views.ContxAPIView.as_view(), name="api-contx"),
+    path("bio_diversity/grp/", views.GroupAPIView.as_view(), name="api-grp"),
 
 ]
