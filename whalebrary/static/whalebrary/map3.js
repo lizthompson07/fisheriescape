52ff953615d51ccc373427a0615496d76bbae294
Liz <lizthompson07@gmail.com>
2021-05-25 15:23:24 -0300

    change colour of leaflet icon for resights

--- whalebrary/static/whalebrary/map3.js
+++ whalebrary/static/whalebrary/map3.js
@@ -35,9 +35,24 @@ var allPointObject = L.geoJSON(allPointObj, {
         }
 });
 
+// Create icon of different colour for resight (see https://awesomeopensource.com/project/pointhi/leaflet-color-markers)
+
+var myIcon = new L.Icon({
+
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+
 // Create all resight layer and use onEachFeature to show certain info for each feature
 
 var resightObject = L.geoJSON(resightObj, {
+     pointToLayer: function (feature, latlng) {
+                    return L.marker(latlng, {icon: myIcon});
+    },
 
     onEachFeature: function (feature, layer) {
        layer.bindPopup(`Resight #: ${feature.properties.pk}<br>Date: ${feature.properties.date}</br>Comments: ${feature.properties.comments}`);
