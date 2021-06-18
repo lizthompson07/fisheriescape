484e8691088983f8c69f39090c53fa01c92e51e6
David Fishman <davjfish@gmail.com>
2021-06-18 07:32:08 -0300

    fixing broken unit tests

--- scuba/api/urls.py
+++ scuba/api/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("scuba/user/", views.CurrentUserAPIView.as_view(), name="current-user"),  # tested
+    path("scuba/user/", views.CurrentUserAPIView.as_view(), name="scuba-current-user"),  # tested
 
     # observations
-    path("scuba/observations/", views.ObservationListCreateAPIView.as_view(), name="observation-list"),  # tested
-    path("scuba/observations/<int:pk>/", views.ObservationRetrieveUpdateDestroyAPIView.as_view(), name="observation-detail"),  # tested
-    path("scuba/sections/", views.SectionListCreateAPIView.as_view(), name="section-list"),  # tested
-    path("scuba/sections/<int:pk>/", views.SectionRetrieveUpdateDestroyAPIView.as_view(), name="section-detail"),  # tested
+    path("scuba/observations/", views.ObservationListCreateAPIView.as_view(), name="scuba-observation-list"),  # tested
+    path("scuba/observations/<int:pk>/", views.ObservationRetrieveUpdateDestroyAPIView.as_view(), name="scuba-observation-detail"),  # tested
+    path("scuba/sections/", views.SectionListCreateAPIView.as_view(), name="scuba-section-list"),  # tested
+    path("scuba/sections/<int:pk>/", views.SectionRetrieveUpdateDestroyAPIView.as_view(), name="scuba-section-detail"),  # tested
 
 ]
