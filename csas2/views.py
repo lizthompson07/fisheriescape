3533eb58a23aa0ac78f7b09463f90cac5b14dee0
David Fishman <davjfish@gmail.com>
2021-10-07 08:40:15 -0300

    csas dev

--- csas2/views.py
+++ csas2/views.py
@@ -255,8 +255,7 @@ class CSASRequestListView(LoginAccessRequiredMixin, CommonFilterView):
 
     def get_extra_button_dict1(self):
         # qs = self.filterset.qs
-        # ids = listrify([obj.id for obj in qs])
-        ids=""
+        ids = listrify([])
         return {
             "name": _("<span class=' mr-1 mdi mdi-file-excel'></span> {name}").format(name=_("Export")),
             "url": reverse("csas2:request_list_report") + f"?csas_requests={ids}",
