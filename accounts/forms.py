09f40e592c4ad6841cb05d57f9783c25a0d10ec6
David Fishman <davjfish@gmail.com>
2021-11-12 13:40:01 -0400

    user auth revamp

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,14 +1,15 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, PasswordResetForm, UserModel, _unicode_ci_compare
+from django.contrib.auth.forms import PasswordResetForm, UserModel, _unicode_ci_compare
 from django.contrib.auth.models import User
 from django.template import loader
 from django.urls import reverse
 from django.utils.safestring import mark_safe
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext as _, gettext_lazy
 
 from dm_apps.utils import custom_send_mail
 from . import models
+from .tokens import otp_token_generator
 
 chosen_js = {"class": "chosen-select-contains"}
 
@@ -22,6 +23,11 @@ except (ModuleNotFoundError, ImportError):
 
 
 class ProfileForm(forms.ModelForm):
+    first_name = forms.CharField(label=_("First name"))
+    last_name = forms.CharField(label=_("Last name"))
+    email = forms.EmailField(label=_("Email"))
+    field_order = ("first_name", "last_name", "email",)
+
     class Meta:
         model = models.Profile
         fields = ('position_eng', 'position_fre', 'phone', 'language', 'section')
@@ -37,21 +43,38 @@ class ProfileForm(forms.ModelForm):
         self.fields["section"].choices = section_choices
         self.fields["section"].widget.attrs = chosen_js
 
+    def clean_email(self):
+        new_email = self.cleaned_data['email']
+        old_email = self.instance.user.email
+        if new_email != old_email and User.objects.filter(email__iexact=new_email).exists():
+            raise forms.ValidationError(mark_safe(_("There is already a DM Apps account with this email address.")))
+        return new_email
+
 
 class UserAccountForm(forms.ModelForm):
     class Meta:
-        # fields = ('username','first_name','last_name','email','password1','password2')
         model = get_user_model()
-        fields = ('email', 'first_name', 'last_name')
+        fields = ('first_name', 'last_name', "email")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["first_name"].required = True
+        self.fields["last_name"].required = True
 
     def clean_email(self):
         new_email = self.cleaned_data['email']
-        if User.objects.filter(email__iexact=new_email).count() > 0:
-            url_redirect = reverse("accounts:password_reset")
-            raise forms.ValidationError(mark_safe(
-                "This email address already exists in the database. To reset your account credentials click <a href='{}'>HERE</a>".format(
-                    url_redirect)))
-            # raise forms.ValidationError(_(mark_safe('An account already exists for this email address. <a href="#" class="email_error">Log in instead?</a>')))
+        user_qs = User.objects.filter(email__iexact=new_email)
+        if user_qs.exists():
+            user = user_qs.first()
+            if user.is_active:
+                url_redirect = reverse("accounts:login")
+                raise forms.ValidationError(mark_safe(
+                    _("You already have an account on DM Apps. Please sign in <a href='{url}'>HERE</a>").format(url=url_redirect)))
+            else:
+                url_redirect = reverse("accounts:resend_activation_email")
+                raise forms.ValidationError(mark_safe(
+                    _("There is already an account on DM Apps with this email address however it has not been validated. Please validate this account <a href='{url}'>HERE</a>").format(
+                        url=url_redirect)))
 
         if new_email.lower().endswith("@dfo-mpo.gc.ca") == False:
             raise forms.ValidationError(
@@ -69,39 +92,8 @@ class AccountRequestForm(forms.Form):
     reason_for_request = forms.CharField(widget=forms.Textarea)
 
 
-class SignupForm(UserCreationForm):
-    email = forms.EmailField(max_length=200, help_text=_(
-        'Required - a verification email will be sent to you once this form is submitted'))
-    password1 = forms.HiddenInput()
-    password2 = forms.HiddenInput()
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
-
-    def clean_email(self):
-        new_email = self.cleaned_data['email']
-        if User.objects.filter(email__iexact=new_email).count() > 0:
-            url_redirect = reverse("password_reset")
-            raise forms.ValidationError(mark_safe(
-                "This email address already exists in the database. To reset your account credentials click <a href='{}'>HERE</a>".format(
-                    url_redirect)))
-            # raise forms.ValidationError(_(mark_safe('An account already exists for this email address. <a href="#" class="email_error">Log in instead?</a>')))
-
-        if new_email.lower().endswith("@dfo-mpo.gc.ca") == False:
-            raise forms.ValidationError(
-                _("Only DFO employees can register for an account. Please enter an email ending with '@DFO-MPO.GC.CA'"))
-
-        # Always return a value to use as the new cleaned data, even if
-        # this method didn't change it.
-        return new_email
-
-
-class UserForgotPasswordForm(PasswordResetForm):
+class ResendVerificationEmailForm(forms.Form):
     email = forms.EmailField(required=True)
-
-    class Meta:
-        model = User
-        fields = ("email")
 
 
 class RequestAccessForm(forms.Form):
@@ -115,45 +107,6 @@ class RequestAccessForm(forms.Form):
     user_id = forms.CharField(widget=forms.HiddenInput())
     application = forms.ChoiceField(label='Requesting access to which app', choices=APPLICATION_CHOICES)
     optional_comment = forms.CharField(required=False, label="Details")
-
-
-# class SetUserPasswordForm(SetPasswordForm):
-
-# label='Reason for request'
-
-class DMAppsPasswordResetForm(PasswordResetForm):
-    def send_mail(self, subject_template_name, email_template_name,
-                  context, from_email, to_email, html_email_template_name=None):
-        """
-        Send a django.core.mail.EmailMultiAlternatives to `to_email`.
-        """
-        subject = loader.render_to_string(subject_template_name, context)
-        # Email subject *must not* contain newlines
-        subject = ''.join(subject.splitlines())
-        body = loader.render_to_string(email_template_name, context)
-        custom_send_mail(
-            html_message=body,
-            subject=subject,
-            from_email=from_email,
-            recipient_list=[to_email]
-        )
-
-    def get_users(self, email):
-        """Given an email, return matching user(s) who should receive a reset.
-
-        This allows subclasses to more easily customize the default policies
-        that prevent inactive users and users with unusable passwords from
-        resetting their password.
-        """
-        email_field_name = UserModel.get_email_field_name()
-        active_users = UserModel._default_manager.filter(**{
-            '%s__iexact' % email_field_name: email,
-        })
-        return (
-            u for u in active_users
-            if u.has_usable_password() and
-               _unicode_ci_compare(email, getattr(u, email_field_name))
-        )
 
 
 class DMAppsEmailLoginForm(PasswordResetForm):
@@ -194,13 +147,39 @@ class DMAppsEmailLoginForm(PasswordResetForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.fields["email"].label = _("Please enter the DFO e-mail address associated with your account:")
-        self.fields["email"].widget = forms.EmailInput(attrs={'autocomplete': 'email', "placeholder": _("E-mail")})
+        self.fields["email"].widget = forms.EmailInput(attrs={'autocomplete': 'email', "placeholder": _("Email")})
+
+    # def clean_email(self):
+    #     new_email = self.cleaned_data['email']
+    #     if new_email.lower().endswith("@dfo-mpo.gc.ca") == False:
+    #         raise forms.ValidationError(
+    #             _("Please enter an email ending with '@dfo-mpo.gc.ca'"))
+    #     if len([u for u in self.get_users(new_email)]) == 0:
+    #         raise forms.ValidationError(_("This email address is not in the system. Please check the spelling or register for an account "))
+    #     return new_email
 
     def clean_email(self):
         new_email = self.cleaned_data['email']
         if new_email.lower().endswith("@dfo-mpo.gc.ca") == False:
-            raise forms.ValidationError(
-                _("Please enter an email ending with '@dfo-mpo.gc.ca'"))
-        if len([u for u in self.get_users(new_email)]) == 0:
+            raise forms.ValidationError(_("Only DFO employees can register for an account. Please enter an email ending with '@DFO-MPO.GC.CA'"))
+        elif len([u for u in self.get_users(new_email)]) == 0:
             raise forms.ValidationError(_("This email address is not in the system. Please check the spelling or register for an account "))
+        else:
+            for user in self.get_users(new_email):
+                if not user.is_active:
+                    url_redirect = reverse("accounts:resend_activation_email")
+                    raise forms.ValidationError(mark_safe(
+                        _("This has not been activated. Please validate this account <a href='{url}?email={email}'>HERE</a>").format(
+                            url=url_redirect, email=new_email)))
+
+        # Always return a value to use as the new cleaned data, even if
+        # this method didn't change it.
         return new_email
+
+    def save(self, *args, **kwargs):
+        kwargs["token_generator"] = otp_token_generator
+        super().save(*args, **kwargs)
+
+
+class OTPForm(forms.Form):
+    otp = forms.CharField(required=True, label=gettext_lazy("Code"))
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,30 +4,30 @@ from . import views
 
 app_name = 'accounts'
 
+
 urlpatterns = [
-    path('close/', views.CloserTemplateView.as_view(), name="close_me"),
     path('denied/', views.access_denied, name='denied_access'),
     path('denied/<str:message>/', views.access_denied, name='denied_access'),
-    path('login/', views.UserLoginView.as_view(), name='login'),
     path('login_required/', views.UserLoginView.as_view(), name='login_required'),
 
-    path('azure-login/', views.sign_in, name='azure_login'),
-    path('callback/', views.callback, name='callback'),
-    path('regular-callback/<str:uidb64>/<str:token>/', views.CallBack.as_view(), name='regular_callback'),
-    path('signup/', views.signup, name='signup'),
-    path('activate/<str:uidb64>/<str:token>', views.activate, name='activate'),
-    path('resend-verification-email/<str:email>', views.resend_verification_email, name='resend_verification_email'),
-    path('verified/', views.account_verified, name='verified'),
+    # main login/logout pages
+    path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.UserLogoutView.as_view(), name='logout'),
-    path('users/<int:pk>/edit/', views.UserUpdateView.as_view(), name='account'),
-    path('profiles/<int:pk>/edit/', views.ProfileUpdateView.as_view(), name='profile'),
-    path('users/change-password/', views.change_password, name='change_password'),
-    path('request-access/', views.RequestAccessFormView.as_view(), name='request_access'),
 
-    ### NOTE: Password reset views are mapped in the dm_apps urls.py file. Views are still in the Accounts app views.py
-    # Password reset views. Views are part of accounts app #
-    ########################################################
-    path('password-reset/', views.UserPassWordResetView.as_view(), name='password_reset'),
-    path('reset/<str:uidb64>/<str:token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # azure
+    path('azure-login/', views.sign_in, name='azure_login'),  # used to access the MZ Azure AD login page
+    path('callback/', views.callback, name='callback'),  # used to finalize the authentication from MS Azure AD
+    # callback for non-azure authentication
+    path('regular-callback/<str:uidb64>/<str:token>/', views.CallBack.as_view(), name='regular_callback'),
+
+    # sign up pages
+    path('signup/', views.signup, name='signup'),
+    path('activate/<str:uidb64>/<str:token>', views.activate, name='activate'),  # to activate account
+    path('resend-activation-email/', views.resend_activation_email, name='resend_activation_email'),  # to activate account
+
+
+    path('profile/', views.ProfileUpdateView.as_view(), name='profile'),
+
+    path('request-access/', views.RequestAccessFormView.as_view(), name='request_access'),
 
 ]
