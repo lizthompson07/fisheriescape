aba1a5df6c4d679c31f424dfdd3d85903eed4f6a
David Fishman <davjfish@gmail.com>
2021-12-14 14:17:42 -0400

    crushing spot migrations

--- spot/models.py
+++ spot/models.py
@@ -230,7 +230,7 @@ class Person(models.Model):
     section = models.CharField(max_length=100, blank=True, null=True, verbose_name=_("Section"))
     other_membership = models.TextField(max_length=1000, blank=True, null=True, verbose_name=_("Other Membership"))
     date_last_modified = models.DateTimeField(blank=True, null=True, default=timezone.now, verbose_name=_("date last modified"))
-    last_modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True, verbose_name=_("last modified by"))
+    last_modified_by = models.ForeignKey(User, on_delete=models.DO_NOTHING, blank=True, null=True, verbose_name=_("last modified by"), related_name="spot_last_modifications")
 
     def save(self, *args, **kwargs):
         self.date_last_modified = timezone.now()
