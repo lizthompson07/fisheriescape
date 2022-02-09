59c3b22cd43b0deaff690876c051891749e5a093
David Fishman <davjfish@gmail.com>
2022-02-09 10:33:15 -0400

    docker update

--- Procfile.sh
+++ Procfile.sh
@@ -5,4 +5,6 @@ python manage.py compilemessages
 python manage.py collectstatic --no-input
 python manage.py showmigrations
 python manage.py migrate
+celery -A dm_apps beat -l info --detach
+celery -A dm_apps worker -l info --detach
 gunicorn -b 0.0.0.0:8000 -c gunicorn.conf.py dm_apps.wsgi:application
