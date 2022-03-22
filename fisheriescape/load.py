87f92de7d18800d2a95469d14521c5110eea4d3c
lizthompson07 <lizthompson07@gmail.com>
2022-03-22 09:45:57 -0300

    work on load functions

--- fisheriescape/load.py
+++ fisheriescape/load.py
@@ -24,6 +24,21 @@ from .models import FisheryArea, Hexagon, Score
 #     lm = LayerMapping(FisheryArea, nafo_shp, mapping, transform=False)
 #     lm.save(strict=True, verbose=verbose)
 
+# For NAFO_subzones.shp
+mapping = {
+    'layer_id': 'Layer_id',
+    'name': 'ZONE',
+    'polygon': 'MULTIPOLYGON',
+}
+
+nafo_shp = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), 'data', 'NAFO_Atlantic.shp'),
+)
+
+
+def run(verbose=True):
+    lm = LayerMapping(FisheryArea, nafo_shp, mapping, transform=False)
+    lm.save(strict=True, verbose=verbose)
 
 # For SnowCrab_polygons.shp
 # mapping = {
@@ -72,19 +87,19 @@ from .models import FisheryArea, Hexagon, Score
 #     lm = LayerMapping(Hexagon, hexagon_shp, mapping, transform=False)
 #     lm.save(strict=True, verbose=verbose)
 
-## For hexagon scores from a shapefile
-mapping = {
-    'hexagon': {'grid_id': 'grid_id'},
-    'species': {'english_name': 'species'},
-    'week': {'week_number': 'week'},
-    'site_score': 'mean_score',
-}
-
-site_score_shp = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), 'data', 'site_score_test.shp'),
-)
-
-
-def run(verbose=True):
-    lm = LayerMapping(Score, site_score_shp, mapping, transform=False)
-    lm.save(strict=True, verbose=verbose)
+# ## For hexagon scores from a shapefile
+# mapping = {
+#     'hexagon': {'grid_id': 'grid_id'},
+#     'species': {'english_name': 'species'},
+#     'week': {'week_number': 'week'},
+#     'site_score': 'mean_score',
+# }
+#
+# site_score_shp = os.path.abspath(
+#     os.path.join(os.path.dirname(__file__), 'data', 'site_score_test.shp'),
+# )
+#
+#
+# def run(verbose=True):
+#     lm = LayerMapping(Score, site_score_shp, mapping, transform=False)
+#     lm.save(strict=True, verbose=verbose)
