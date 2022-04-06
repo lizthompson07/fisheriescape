cd9edba57bcd66ba06cfddb271388ca5a1819cff
David Fishman <davjfish@gmail.com>
2022-04-06 10:46:29 -0300

    trapnet dev

--- trapnet/mixins.py
+++ trapnet/mixins.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponseRedirect
 
-from trapnet.utils import can_access, is_admin
+from trapnet.utils import can_access, is_admin, is_crud_user
 
 
 class TrapNetBasicMixin(LoginRequiredMixin, UserPassesTestMixin):
@@ -23,8 +23,10 @@ class TrapNetBasicMixin(LoginRequiredMixin, UserPassesTestMixin):
         return context
 
 
-class TrapNetAccessRequiredMixin(TrapNetBasicMixin):
-    pass
+class TrapNetCRUDRequiredMixin(TrapNetBasicMixin):
+    def test_func(self):
+        return is_crud_user(self.request.user)
+
 
 
 class TrapNetAdminRequiredMixin(TrapNetBasicMixin):
