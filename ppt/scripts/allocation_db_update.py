cf3b9df8336c2a66fdc913ae65a569decdabfa79
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2022-08-16 13:35:19 -0300

    add script to migrate allocation amounts from reviews to om allocation objects

---
+++ ppt/scripts/allocation_db_update.py
@@ -0,0 +1,21 @@
+from django.db import transaction
+
+from ppt import models
+
+
+def allocation_mover():
+    with transaction.atomic():
+        for rev in models.Review.objects.all():
+            if rev.allocated_budget:
+                # check if any amount are already allocated:
+                if rev.project_year.allocations:
+                    pass
+                else:
+                    alloc_obj = models.OMAllocation(
+                        project_year=rev.project_year,
+                        amount=rev.allocated_budget,
+                        funding_source=rev.project_year.project.default_funding_source
+                    )
+                    alloc_obj.clean()
+                    alloc_obj.save()
+    return 0
