ff14af02f80bc5322f036acee566288d15787a47
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2022-09-14 11:00:03 -0300

    pk version of api

--- maret/api/serializers.py
+++ maret/api/serializers.py
@@ -28,53 +28,53 @@ class ComitteeSerializer(serializers.ModelSerializer):
     other_dfo_areas = serializers.SerializerMethodField()
 
     def get_main_topic(self, instance):
-        return listrify([str(nz(t)) for t in instance.main_topic.all()])
+        return [t.pk for t in instance.main_topic.all()]
 
     def get_species(self, instance):
-        return listrify([str(nz(t)) for t in instance.species.all()])
+        return [t.pk for t in instance.species.all()]
 
     def get_external_chair(self, instance):
-        return listrify([str(nz(t)) for t in instance.external_chair.all()])
+        return [t.pk for t in instance.external_chair.all()]
 
     def get_dfo_liaison(self, instance):
-        return listrify([str(nz(t)) for t in instance.dfo_liaison.all()])
+        return [t.pk for t in instance.dfo_liaison.all()]
 
     def get_other_dfo_branch(self, instance):
-        return listrify([str(nz(t)) for t in instance.other_dfo_branch.all()])
+        return [t.pk for t in instance.other_dfo_branch.all()]
 
     def get_other_dfo_regions(self, instance):
-        return listrify([str(nz(t)) for t in instance.other_dfo_regions.all()])
+        return [t.pk for t in instance.other_dfo_regions.all()]
 
     def get_other_dfo_areas(self, instance):
-        return listrify([str(nz(t)) for t in instance.other_dfo_areas.all()])
+        return [t.pk for t in instance.other_dfo_areas.all()]
 
     def get_dfo_national_sectors(self, instance):
-        return listrify([str(nz(t)) for t in instance.dfo_national_sectors.all()])
+        return [t.pk for t in instance.dfo_national_sectors.all()]
 
     def get_other_dfo_participants(self, instance):
-        return listrify([str(nz(t)) for t in instance.other_dfo_participants.all()])
+        return [t.pk for t in instance.other_dfo_participants.all()]
 
     def get_external_contact(self, instance):
-        return listrify([str(nz(t)) for t in instance.external_contact.all()])
+        return [t.pk for t in instance.external_contact.all()]
 
     def get_external_organization(self, instance):
-        return listrify([str(nz(t)) for t in instance.external_organization.all()])
+        return [t.pk for t in instance.external_organization.all()]
 
     def get_lead_region(self, instance):
-        return instance.lead_region.__str__()
+        return instance.lead_region.pk if instance.lead_region else None
 
     def get_branch(self, instance):
-        return instance.branch.__str__()
+        return instance.branch.pk if instance.branch else None
 
     def get_division(self, instance):
-        return instance.division.__str__()
+        return instance.division.pk if instance.division else None
 
     def get_area_office(self, instance):
-        return instance.area_office.__str__()
+        return instance.area_office.pk if instance.area_office else None
 
     def get_area_office_program(self, instance):
-        return instance.area_office_program.__str__()
+        return instance.area_office_program.pk if instance.area_office_program else None
 
     def get_lead_national_sector(self, instance):
-        return instance.lead_national_sector.__str__()
+        return instance.lead_national_sector.pk if instance.lead_national_sector else None
 
