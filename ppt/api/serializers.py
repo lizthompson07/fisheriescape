e859a44418cc87ddd0c76e004652185c59b1b989
stoyelq <quentin.stoyel@dfo-mpo.gc.ca>
2022-10-20 08:59:38 -0300

    add email primary project leads to management view

--- ppt/api/serializers.py
+++ ppt/api/serializers.py
@@ -218,6 +218,7 @@ class ProjectYearSerializer(serializers.ModelSerializer):
     review_score_fraction = serializers.SerializerMethodField()
     status_display = serializers.SerializerMethodField()
     status_class = serializers.SerializerMethodField()
+    primary_contact_email = serializers.SerializerMethodField()
     om_costs = serializers.SerializerMethodField()
     salary_costs = serializers.SerializerMethodField()
     capital_costs = serializers.SerializerMethodField()
@@ -261,6 +262,13 @@ class ProjectYearSerializer(serializers.ModelSerializer):
 
     def get_om_allocations(self, instance):
         return instance.om_allocations
+
+    def get_primary_contact_email(self, instance):
+        primary_contact = instance.staff_set.filter(is_primary_lead=True).first()
+        if primary_contact:
+            if primary_contact.user:
+                return primary_contact.user.email
+        return None
 
     def get_status_class(self, instance):
         return slugify(instance.get_status_display())
