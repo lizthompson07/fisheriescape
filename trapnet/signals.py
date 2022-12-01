2ca74ddbb184df3b6d7aaa19cea6c0419f3b049e
David Fishman <davjfish@gmail.com>
2022-12-01 11:37:57 -0400

    trapnet dev

--- trapnet/signals.py
+++ trapnet/signals.py
@@ -68,3 +68,9 @@ def auto_delete_trapnet_sample_file_on_change(sender, instance, **kwargs):
     if not old_file == new_file:
         if os.path.isfile(old_file.path):
             os.remove(old_file.path)
+
+
+# @receiver(post_save, sender=Sample)
+# def auto_save_trapnet_specimens_on_sample_change(sender, instance, **kwargs):
+#     for specimen in instance.specimens.all():
+#         specimen.save()
