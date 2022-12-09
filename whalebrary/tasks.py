15a634791e2946d1f5ad81930d8d5f0be7377d89
lizthompson07 <lizthompson07@gmail.com>
2022-12-09 14:02:10 -0400

    maintenance email reminders start of work

---
+++ whalebrary/tasks.py
@@ -0,0 +1,33 @@
+from celery import shared_task
+from django.utils import timezone
+
+from . import emails
+from .models import Maintenance
+
+
+@shared_task(name="maintenance_reminder_email")
+def maintenance_reminder_email():
+    # get all maintenance items that are overdue
+    print("test")
+    pass
+
+    # lazy_reviewers = ToRReviewer.objects.filter(review_started__isnull=False, review_completed__isnull=True)
+    # for r in lazy_reviewers:
+    #     td = timezone.now() - r.review_started
+    #     if td.days >= 5:
+    #         send_email = False
+    #         # they are in the warning zone...
+    #         # if there was never a warning, we send one right away
+    #         if not r.reminder_sent:
+    #             send_email = True
+    #         # otherwise we compare to when the reminder email was last sent
+    #         else:
+    #             td = timezone.now() - r.reminder_sent
+    #             if td.days >= 5:
+    #                 send_email = True
+    #         if send_email:
+    #             # update the reminder sent date
+    #             email = emails.ToRReviewReminderEmail(r, td)
+    #             email.send()
+    #             r.reminder_sent = timezone.now()
+    #             r.save()
