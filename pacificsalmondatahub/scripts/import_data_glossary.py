941c82bf472d7f28c504b85911cc9f5ec1afb4d3
Yan <110200329+pssi-sdpa@users.noreply.github.com>
2022-12-29 11:00:14 -0800

    Change names

---
+++ pacificsalmondatahub/scripts/import_data_glossary.py
@@ -0,0 +1,46 @@
+import csv
+import os
+import datetime
+
+from django.db import models
+from django.conf import settings
+from pacificsalmondatahub.models import DataGlossary
+from django.db.models import QuerySet
+# @pylance.typecheck(QuerySet)
+
+rootdir = os.path.join(settings.BASE_DIR, 'pacificsalmondatahub')
+
+def clear():
+    toDelete = DataGlossary.objects.all()
+    toDelete.delete()
+
+# Similar to the functions in importCSV.py - use discrete column indices to map glossary values to the right field
+def run():
+
+    pass
+
+    # with open(os.path.join(rootdir, 'CSV/PacificSalmonDataGlossary.csv'), 'r') as csvfile:
+    #     reader = csv.reader(csvfile)
+    #     #skip header row
+    #     next(reader)
+    #     i = 0
+    #     for row in reader:
+    #         row = [entry.strip() for entry in row]
+    #         row = [entry if entry != '' else None for entry in row]
+            
+    #         acronym_ID = i
+    #         i+= 1
+    #         acronym_Letters = row[0]
+    #         acronym_Full_Name = row[1]
+    #         acronym_URL = row[7]
+    #         acronym_Topic = row[4]
+
+    #         # left side: parameters, right side: argument
+    #         model = DataGlossary(
+    #             acronym_ID = acronym_ID,
+    #             acronym_Letters = acronym_Letters,
+    #             acronym_Full_Name = acronym_Full_Name,
+    #             acronym_Topic = acronym_Topic,
+    #             acronym_URL = acronym_URL,
+    #         )
+    #         model.save()
--- pacificsalmondatahub/urls.py
+++ pacificsalmondatahub/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import SearchView, Index, AcronymView, DataGlossaryView,BusinessGlossaryView, DetailView, loadData, loadAcronyms
+from .views import SearchView, Index, AcronymView, DataGlossaryView,BusinessGlossaryView, DetailView, loadData, loadAcronyms, loadBusinessGlossary, loadDataGlossary
 
 
 app_name = "pacificsalmondatahub"
@@ -9,15 +9,20 @@ app_name = "pacificsalmondatahub"
 # as_view() is needed when using class-based views. 
 urlpatterns = [
     path('', Index.as_view(), name = 'Index'),
+
+    # SEARCH LIST AND DETAILS
     path('search/', SearchView.as_view(), name = 'search'),
-    # Currently loading a static details page
-    path('search/details/', DetailView.as_view(), name = 'details'),
-    path('search/<str:uuid>/', DetailView.as_view(), name="search_detail_uuid"),    
+    path('search/<int:pk>/view/', DetailView.as_view(), name="details"),
+    path('search/<str:uuid>/', DetailView.as_view(), name="details_uuid"),
+    path('search/details/', DetailView.as_view(), name = 'details_static'),
+
     path('acronyms/', AcronymView.as_view(), name = 'acronym_list'),
-    path('dataGlossary/', DataGlossaryView.as_view(), name = 'data_glossary_list'),
-    path('businessGlossary/', BusinessGlossaryView.as_view(), name = 'business_glossary_list'),
-    path('importcsv/', loadData, name = 'loadPSSIcsv'),
-    path('getAC/', loadAcronyms, name = "loadAcronyms" )
+    path('dataglossary/', DataGlossaryView.as_view(), name = 'data_glossary_list'),
+    path('businessglossary/', BusinessGlossaryView.as_view(), name = 'business_glossary_list'),
+
+    # IMPORTING CSV
+    path('importcsv/', loadData, name = 'load_pssi_csv'),
+    path('importacronyms/', loadAcronyms, name = "load_acronyms"),
+    path('importdataglossary/', loadDataGlossary, name = "load_data_glossary"),
+    path('importbusinessglossary/', loadBusinessGlossary, name = "load_business_glossary"),
 ]
-
-
