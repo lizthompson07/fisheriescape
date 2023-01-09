3c54b01d56d79f3a4d7ca96649b9f2db87946af2
Yan <110200329+pssi-sdpa@users.noreply.github.com>
2023-01-09 15:08:48 -0800

    Fix import

--- pacificsalmondatahub/scripts/import_business_glossary.py
+++ pacificsalmondatahub/scripts/import_business_glossary.py
@@ -18,29 +18,3 @@ def clear():
 def run():
 
     pass
-
-    # with open(os.path.join(ROOTDIR, "./csv/PacificSalmonBusinessGlossary.csv"), "r") as csvfile:
-    #     reader = csv.reader(csvfile)
-    #     #skip header row
-    #     next(reader)
-    #     i = 0
-    #     for row in reader:
-    #         row = [entry.strip() for entry in row]
-    #         row = [entry if entry != "" else None for entry in row]
-            
-    #         acronym_ID = i
-    #         i+= 1
-    #         acronym_Letters = row[0]
-    #         acronym_Full_Name = row[1]
-    #         acronym_URL = row[7]
-    #         acronym_Topic = row[4]
-
-    #         # left side: parameters, right side: argument
-    #         model = BusinessGlossary(
-    #             acronym_ID = acronym_ID,
-    #             acronym_Letters = acronym_Letters,
-    #             acronym_Full_Name = acronym_Full_Name,
-    #             acronym_Topic = acronym_Topic,
-    #             acronym_URL = acronym_URL,
-    #         )
-    #         model.save()
