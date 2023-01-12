f5d4c3ea120eb2e31590a9e4299a9a3d38ce18ef
Yan <110200329+pssi-sdpa@users.noreply.github.com>
2023-01-11 21:05:20 -0800

    Update import method

--- pssi/scripts/import_acronyms.py
+++ pssi/scripts/import_acronyms.py
@@ -18,6 +18,13 @@ def clear():
 # Also should be converted into pandas or another library that will allow for accessing specific fields
 def run():
 
+    field_list = [
+        "acronym_letters",
+        "acronym_full_name",
+        "acronym_topic",
+        "acronym_url"
+    ]
+
     with open(os.path.join(ROOTDIR, "./csv/Pacific_Salmon_Acronyms.csv"), "r") as csvfile:
         reader = csv.reader(csvfile)
         #skip header row
@@ -26,20 +33,28 @@ def run():
         for row in reader:
             row = [entry.strip() for entry in row]
             row = [entry if entry != "" else None for entry in row]
-            
+
+            # Auto Increment ID
             acronym_id = i
-            i+= 1
-            acronym_letters = row[0]
-            acronym_full_name = row[1]
-            acronym_url = row[7]
-            acronym_topic = row[4]
+            i += 1
+
+            # value list
+            value_list = [
+                row[0],
+                row[1],
+                row[4],
+                row[7]
+            ]
+
+            fields = dict(zip(field_list, value_list))
 
             # left side: parameters, right side: argument
             model = Acronym(
                 acronym_id = acronym_id,
-                acronym_letters = acronym_letters,
-                acronym_full_name = acronym_full_name,
-                acronym_topic = acronym_topic,
-                acronym_url = acronym_url,
+                acronym_letters = fields["acronym_letters"],
+                acronym_full_name = fields["acronym_full_name"],
+                acronym_topic = fields["acronym_topic"],
+                acronym_url = fields["acronym_url"]
             )
             model.save()
+            
--- pssi/scripts/import_csv.py
+++ pssi/scripts/import_csv.py
@@ -19,6 +19,48 @@ def clear_inventory():
 # Goes through the csv row by row and maps the appropriate column to a variable
 # Mapping with discrete column index will not work if structure of CSV is changed at all, try using pandas to search by field name
 def run_csv_to_inventory():
+    
+    field_list = [
+        "inventory_id",
+        "approved_by",
+        "data_asset_name",
+        "data_asset_steward",
+        "data_asset_sorting_category",
+        "operational_or_analytical_data",
+        "data_asset_acronym",
+        "data_asset_description",
+        "apm_id",
+        "non_salmon_data",
+        "data_asset_status",
+        "data_asset_format",
+        "data_type",
+        "data_asset_location",
+        "data_asset_trustee",
+        "data_asset_custodian",
+        "application_data_asset_is_associated_with",
+        "application_description",
+        "technical_documentation",
+        "access_point",
+        "policy_levers_data_asset_supports",
+        "key_decisions",
+        "impact_to_dfo_data_consumers",
+        "decision_supporting_key_products",
+        "impact_on_decision_making",
+        "uniqueness",
+        "cost",
+        "data_asset_size",
+        "update_frequency",
+        "data_standards",
+        "metadata_maturity",
+        "data_quality_rating",
+        "naming_conventions",
+        "security_classification",
+        "inbound_data_linkage",
+        "outbound_data_linkage",
+        "publication_status"
+    ]
+
+    # max_field_len = dict(zip(field_list, list([0]*len(field_list))))
 
     with open(os.path.join(ROOTDIR, "./csv/Pacific_Region_Data_Inventory_main.csv"), "r") as csvfile:
         reader = csv.reader(csvfile)
@@ -28,84 +70,69 @@ def run_csv_to_inventory():
             # Remove whitespace and replace "" with None for cleaning
             row = [entry.strip() for entry in row]
             row = [entry if entry not in ["", "N/A", "n/a"] else None for entry in row]
+            fields = dict(zip(field_list, row))
 
-            Inventory_ID                              = row[0]
-            Approved_By                               = row[1]
-            Data_Asset_Name                           = row[2]
-            Data_Asset_Steward                        = row[3]
-            Data_Asset_Sorting_Category               = row[4]
-            Operational_Or_Analytical_Data            = row[5]
-            Data_Asset_Acronym                        = row[6]
-            Data_Asset_Description                    = row[7]
-            APM_ID                                    = row[8]
-            Non_Salmon_Data                           = row[9]
-            Data_Asset_Status                         = row[10]
-            Data_Asset_Format                         = row[11]
-            Data_Type                                 = row[12]
-            Data_Asset_Location                       = row[13]
-            Data_Asset_Trustee                        = row[14]
-            Data_Asset_Custodian                      = row[15]
-            Application_Data_Asset_is_Associated_With = row[16]
-            Application_Description                   = row[17]
-            Technical_Documentation                   = row[18]
-            Access_Point                              = row[19]
-            Policy_Levers_Data_Asset_Supports         = row[20]
-            Key_Decisions                             = row[21]
-            Impact_to_DFO_Data_Consumers              = row[22]
-            Decision_Supporting_Key_Products          = row[23]
-            Impact_on_Decision_Making                 = row[24]
-            Uniqueness                                = row[25]
-            Cost                                      = row[26]
-            Data_Asset_Size                           = row[27]
-            Update_Frequency                          = row[28]
-            Data_Standards                            = row[29]
-            Metadata_Maturity                         = row[30]
-            Data_Quality_Rating                       = row[31]
-            Naming_Conventions                        = row[32]
-            Security_Classification                   = row[33]
-            Inbound_Data_Linkage                      = row[34]
-            Outbound_Data_Linkage                     = row[35]
-            Publication_Status                        = row[36]
+            # Clean data
+            cleaning = [
+                "data_asset_steward",
+                "data_asset_custodian", 
+            ]
 
+            for field in cleaning:
+                if fields[field] is not None:
+                    fields[field] = fields[field].replace("\n", " ")
+                    fields[field] = " ".join(fields[field].split())
+
+            # Find max len of each field
+            # for field in fields:
+            #     if fields[field] is not None:
+            #         max_len = 0
+            #         if len(fields[field]) > max_len:
+            #             max_len = len(fields[field])
+            #         if max_len > max_field_len[field]:
+            #             max_field_len[field] = max_len
+           
             # Initializing a record for DataAsset -> DataAsset(parameter1=value1, parameter2=value2,...)
             # Initialize a DataAsset object using the variables and values assigned above
             model = DataAsset(
-                inventory_id                              = Inventory_ID,
-                approved_by                               = Approved_By,
-                data_asset_name                           = Data_Asset_Name,
-                data_asset_steward                        = Data_Asset_Steward,
-                data_asset_sorting_category               = Data_Asset_Sorting_Category,
-                operational_or_analytical_data            = Operational_Or_Analytical_Data,
-                data_asset_acronym                        = Data_Asset_Acronym,
-                data_asset_description                    = Data_Asset_Description,
-                apm_id                                    = APM_ID,
-                non_salmon_data                           = Non_Salmon_Data,
-                data_asset_status                         = Data_Asset_Status,
-                data_asset_format                         = Data_Asset_Format,
-                data_type                                 = Data_Type,
-                data_asset_location                       = Data_Asset_Location,
-                data_asset_trustee                        = Data_Asset_Trustee,
-                data_asset_custodian                      = Data_Asset_Custodian,
-                application_data_asset_is_associated_with = Application_Data_Asset_is_Associated_With,
-                application_description                   = Application_Description,
-                technical_documentation                   = Technical_Documentation,
-                access_point                              = Access_Point,
-                policy_levers_data_asset_supports         = Policy_Levers_Data_Asset_Supports,
-                key_decisions                             = Key_Decisions,
-                impact_to_dfo_data_consumers              = Impact_to_DFO_Data_Consumers,
-                decision_supporting_key_products          = Decision_Supporting_Key_Products,
-                impact_on_decision_making                 = Impact_on_Decision_Making,
-                uniqueness                                = Uniqueness,
-                cost                                      = Cost,
-                data_asset_size                           = Data_Asset_Size,
-                update_frequency                          = Update_Frequency,
-                data_standards                            = Data_Standards,
-                metadata_maturity                         = Metadata_Maturity,
-                data_quality_rating                       = Data_Quality_Rating,
-                naming_conventions                        = Naming_Conventions,
-                security_classification                   = Security_Classification,
-                inbound_data_linkage                      = Inbound_Data_Linkage,
-                outbound_data_linkage                     = Outbound_Data_Linkage,
-                publication_status                        = Publication_Status
+                inventory_id                              = fields["inventory_id"],
+                approved_by                               = fields["approved_by"],
+                data_asset_name                           = fields["data_asset_name"],
+                data_asset_steward                        = fields["data_asset_steward"],
+                data_asset_sorting_category               = fields["data_asset_sorting_category"],
+                operational_or_analytical_data            = fields["operational_or_analytical_data"],
+                data_asset_acronym                        = fields["data_asset_acronym"],
+                data_asset_description                    = fields["data_asset_description"],
+                apm_id                                    = fields["apm_id"],
+                non_salmon_data                           = fields["non_salmon_data"],
+                data_asset_status                         = fields["data_asset_status"],
+                data_asset_format                         = fields["data_asset_format"],
+                data_type                                 = fields["data_type"],
+                data_asset_location                       = fields["data_asset_location"],
+                data_asset_trustee                        = fields["data_asset_trustee"],
+                data_asset_custodian                      = fields["data_asset_custodian"],
+                application_data_asset_is_associated_with = fields["application_data_asset_is_associated_with"],
+                application_description                   = fields["application_description"],
+                technical_documentation                   = fields["technical_documentation"],
+                access_point                              = fields["access_point"],
+                policy_levers_data_asset_supports         = fields["policy_levers_data_asset_supports"],
+                key_decisions                             = fields["key_decisions"],
+                impact_to_dfo_data_consumers              = fields["impact_to_dfo_data_consumers"],
+                decision_supporting_key_products          = fields["decision_supporting_key_products"],
+                impact_on_decision_making                 = fields["impact_on_decision_making"],
+                uniqueness                                = fields["uniqueness"],
+                cost                                      = fields["cost"],
+                data_asset_size                           = fields["data_asset_size"],
+                update_frequency                          = fields["update_frequency"],
+                data_standards                            = fields["data_standards"],
+                metadata_maturity                         = fields["metadata_maturity"],
+                data_quality_rating                       = fields["data_quality_rating"],
+                naming_conventions                        = fields["naming_conventions"],
+                security_classification                   = fields["security_classification"],
+                inbound_data_linkage                      = fields["inbound_data_linkage"],
+                outbound_data_linkage                     = fields["outbound_data_linkage"],
+                publication_status                        = fields["publication_status"]
             )
             model.save()
+
+    # print(max_field_len)
