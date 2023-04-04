20357ed9866b98575570d69f993a857cc8b32ad4
lizthompson07 <lizthompson07@gmail.com>
2023-04-04 10:13:45 -0300

    update readme, change templates

--- dm_apps/settings.py
+++ dm_apps/settings.py
@@ -172,7 +172,7 @@ INSTALLED_APPS = [
                      'shared_models',
                      'tickets',
                      "django_browser_reload",
-                     "django_extensions",
+                     # "django_extensions",
                  ] + local_conf.MY_INSTALLED_APPS
 
 
